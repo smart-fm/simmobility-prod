e0486ab7c6abb4d914ed6dbb61bfdcaa5da54948
Seth N. Hetu <seth.hetu@gmail.com>
2011-05-26 16:43:36 +0800

    Moved some files, trying out gitignore

---
+++ dev/Basic/workers/AgentWorker.cpp
@@ -0,0 +1,17 @@
+/*
+ * AgentWorker.cpp
+ *
+ *  Created on: May 26, 2011
+ *      Author: sethhetu
+ */
+
+#include "AgentWorker.h"
+
+AgentWorker::AgentWorker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AgentWorker::~AgentWorker() {
+	// TODO Auto-generated destructor stub
+}
--- dev/Basic/workers/Worker.hpp
+++ dev/Basic/workers/Worker.hpp
@@ -4,7 +4,10 @@
  *    1) Use the default constructor. Call "join" when you need its result. (NOTE: This behavior is currently disabled.)
  *    2) Create it with a non-null barrier. Best used with a "WorkGroup"
  * To customize the Worker, either subclass it and override "main_loop", or
+ *
  *    use a normal Worker and pass in a bindable function in the constructor.
+ *        ---NOTE: Haven't tested using a bindable function.
+ *
  *    In this case, the "main_loop" will wrap "action" in a "while(active)" loop; calling
  *    "join" will set "active" to false and wait for the thread to terminate.
  */

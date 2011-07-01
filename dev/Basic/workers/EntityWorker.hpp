54938b4558502b83a718aaa24b5e6af9c6bd5da3
Seth N. Hetu <seth.hetu@gmail.com>
2011-07-01 11:41:03 +0800

    Fixed warnings regarding non-virtual destructors.

--- dev/Basic/workers/EntityWorker.hpp
+++ dev/Basic/workers/EntityWorker.hpp
@@ -21,6 +21,8 @@ namespace sim_mob
 class EntityWorker : public Worker<Entity> {
 public:
 	EntityWorker(Worker<Entity>::actionFunction* action =NULL, boost::barrier* internal_barr =NULL, boost::barrier* external_barr =NULL, unsigned int endTick=0);
+	virtual ~EntityWorker() {}
+
 	virtual void perform_main(frame_t frameNumber);
 
 };

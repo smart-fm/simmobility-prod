acff47e33ab55b789211039d2e18be5b852b2716
Seth N. Hetu <seth.hetu@gmail.com>
2011-07-07 12:54:53 +0700

    Point2D, Node

--- dev/Basic/geospatial/Node.hpp
+++ dev/Basic/geospatial/Node.hpp
@@ -1,20 +1,30 @@
 #pragma once
 
+#include <vector>
+
+#include "Point2D.hpp"
+#include "Link.hpp"
+#include "LaneConnector.hpp"
+#include "RoadSegment.hpp"
 
 namespace sim_mob
 {
 
 /**
- * A location on a map where other elements interact. Nodes are used to identify
- * the start and end positions of links and segments.
+ * A location on a map where other elements interact. Nodes build off of the Point2D class
+ * to include information on the Lane Connectors and RoadSegments at that particular location.
  */
-class Node {
+class Node : public sim_mob::Point2D {
 public:
+	std::vector<sim_mob::LaneConnector*> getConnectors(const Link* from) { return std::vector<sim_mob::LaneConnector*>;};
+	std::vector<sim_mob::RoadSegment*> getItemsAt() { return std::vector<sim_mob::RoadSegment*>; }
 
 
-private:
-	double xPos;
-	double yPos;
+
+protected:
+	std::vector<sim_mob::LaneConnector*> connectors;
+	std::vector<sim_mob::RoadSegment*> itemsAt;
+
 
 
 };
---
+++ dev/Basic/geospatial/Point2D.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+
+namespace sim_mob
+{
+
+/**
+ * Base class for geospatial items in the road network. In general, everything within a road network
+ * is defined by its Point2D location. Road segments are defined by 2 points; one at the beginning
+ * of the segment and another at the end. Intersections are defined by a point at the intersection
+ * of all roads that join at that point.
+ *
+ * The Point2D location of any item should be accurate enough to generate a reasonable visualization
+ * of that item using its Point(s) alone.
+ *
+ * \note
+ * This is a skeleton class. All functions are defined in this header file.
+ * When this class's full functionality is added, these header-defined functions should
+ * be moved into a separate cpp file.
+ *
+ * \todo
+ * We created the Point2D class partly to allow easier sharing of Nodes. Should we treat 2
+ * points at the same X/Y location as physically identical? Should we then have a function
+ * like Point2D::GetPoint(x, y) which returns a new point ONLY if the point x/y doesn't
+ * exist, and returns the pre-existing point otherwise? Just some thoughts.
+ * \par
+ * ~Seth
+ */
+class Point2D {
+public:
+
+
+protected:
+	int xPos;
+	int yPos;
+
+
+
+};
+
+
+
+
+
+}

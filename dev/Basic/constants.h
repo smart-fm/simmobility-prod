5d7ca52dbb794910f760536604756c23800a1f5f
Seth N. Hetu <seth.hetu@gmail.com>
2011-07-12 16:33:12 +0800

    Moved workgroup size constants into an enum.

--- dev/Basic/constants.h
+++ dev/Basic/constants.h
@@ -13,13 +13,17 @@
 
 //namespace sim_mob {} //This is a temporary file, so it exists outside the namespace
 
-const unsigned int WG_TRIPCHAINS_SIZE = 4;       ///<Number of trip chain workers in group.
-const unsigned int WG_CREATE_AGENT_SIZE = 3;     ///<Number of agent creation workers in group.
-const unsigned int WG_CHOICESET_SIZE = 6;        ///<Number of choice set workers in group.
-const unsigned int WG_VEHICLES_SIZE = 5;         ///<Number of vehicle workers in group.
-const unsigned int WG_AGENTS_SIZE = 5;           ///<Number of agent workers in group.
-const unsigned int WG_SIGNALS_SIZE = 2;          ///<Number of signal workers in group.
-const unsigned int WG_SHORTEST_PATH_SIZE = 10;   ///<Number of shortest path workers in group.
+
+//Sizes of workgroups. (Note that enums are allowed to overlap values)
+enum WORKGROUP_SIZES {
+	WG_TRIPCHAINS_SIZE = 4,       ///<Number of trip chain workers in group.
+	WG_CREATE_AGENT_SIZE = 3,     ///<Number of agent creation workers in group.
+	WG_CHOICESET_SIZE = 6,        ///<Number of choice set workers in group.
+	WG_VEHICLES_SIZE = 5,         ///<Number of vehicle workers in group.
+	WG_AGENTS_SIZE = 5,           ///<Number of agent workers in group.
+	WG_SIGNALS_SIZE = 2,          ///<Number of signal workers in group.
+	WG_SHORTEST_PATH_SIZE = 10,   ///<Number of shortest path workers in group.
+};
 
 
 

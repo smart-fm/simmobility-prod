4ce00632ef9ac9415eacb375a2a3424566b23dfa
LIM Fung Chai <LIM.Fung.Chai@smart.mit.edu>
2011-07-13 17:56:28 +0800

    Added more unit-tests of the Basic/buffering classes.

--- dev/Basic/buffering/unit-tests/BufferedUnitTests.cpp
+++ dev/Basic/buffering/unit-tests/BufferedUnitTests.cpp
@@ -1,4 +1,6 @@
 #include <Buffered.hpp>
+#include <Buffered_uint32.hpp>
+#include <BufferedDataManager.hpp>
 
 #include "BufferedUnitTests.hpp"
 
@@ -7,11 +9,141 @@ namespace unit_tests
 
 CPPUNIT_TEST_SUITE_REGISTRATION(unit_tests::BufferedUnitTests);
 
+void BufferedUnitTests::test_default_Buffered_uint32_constructor()
+{
+    sim_mob::Buffered_uint32 integer;
+    CPPUNIT_ASSERT(0 == integer);
+    CPPUNIT_ASSERT(0 == integer.get());
+}
+
 void BufferedUnitTests::test_default_Buffered_float_constructor()
 {
     sim_mob::Buffered<float> floater;
     CPPUNIT_ASSERT(0.0f == floater);
     CPPUNIT_ASSERT(0.0f == floater.get());
+}
+
+void BufferedUnitTests::test_Buffered_uint32_constructor()
+{
+    sim_mob::Buffered_uint32 integer(42);
+    CPPUNIT_ASSERT(42 == integer);
+    CPPUNIT_ASSERT(42 == integer.get());
+}
+
+void BufferedUnitTests::test_Buffered_float_constructor()
+{
+    sim_mob::Buffered<float> floater(3.14159f);
+    CPPUNIT_ASSERT(3.14159f == floater);
+    CPPUNIT_ASSERT(3.14159f == floater.get());
+}
+
+void BufferedUnitTests::test_flipping_after_single_change_to_Buffered_uint32()
+{
+    sim_mob::Buffered_uint32 integer(42);
+
+    sim_mob::BufferedDataManager mgr;
+    mgr.add(&integer);
+
+    integer.set(1);
+    CPPUNIT_ASSERT(42 == integer);
+
+    mgr.flip();
+    CPPUNIT_ASSERT(1 == integer);
+
+    mgr.rem(&integer);
+}
+
+void BufferedUnitTests::test_flipping_after_single_change_to_Buffered_float()
+{
+    sim_mob::Buffered<float> floater(3.14159f);
+
+    sim_mob::BufferedDataManager mgr;
+    mgr.add(&floater);
+
+    floater.set(2.71828f);
+    CPPUNIT_ASSERT(3.14159f == floater);
+
+    mgr.flip();
+    CPPUNIT_ASSERT(2.71828f == floater);
+
+    mgr.rem(&floater);
+}
+
+void BufferedUnitTests::test_flipping_after_multiple_changes()
+{
+    sim_mob::Buffered_uint32 integer(42);
+
+    sim_mob::BufferedDataManager mgr;
+    mgr.add(&integer);
+
+    integer.set(100);
+    CPPUNIT_ASSERT(42 == integer);
+    ++integer;
+    CPPUNIT_ASSERT(42 == integer);
+    integer++;
+    CPPUNIT_ASSERT(42 == integer);
+    integer -= 3;
+    CPPUNIT_ASSERT(42 == integer);
+    integer--;
+    CPPUNIT_ASSERT(42 == integer);
+    --integer;
+    CPPUNIT_ASSERT(42 == integer);
+    integer += 6;
+    CPPUNIT_ASSERT(42 == integer);
+
+    mgr.flip();
+    CPPUNIT_ASSERT(100 + 1 + 1 - 3 - 1 - 1 + 6 == integer);
+
+    mgr.rem(&integer);
+}
+
+void BufferedUnitTests::test_multiple_changes_back_to_original_value()
+{
+    sim_mob::Buffered_uint32 integer(42);
+
+    sim_mob::BufferedDataManager mgr;
+    mgr.add(&integer);
+
+    ++integer;
+    CPPUNIT_ASSERT(42 == integer);
+    integer.set(42);
+    CPPUNIT_ASSERT(42 == integer);
+
+    mgr.flip();
+    CPPUNIT_ASSERT(42 == integer);
+
+    mgr.rem(&integer);
+}
+
+namespace
+{
+    enum Color { green, amber, red };
+
+    class BufferedColor : public sim_mob::Buffered<Color>
+    {
+    public:
+        BufferedColor(enum Color color)
+          : Buffered<Color>(color)
+        {
+        }
+    };
+}
+
+void BufferedUnitTests::test_flipping_single_change_to_Buffered_enum()
+{
+    BufferedColor color(red);
+
+    sim_mob::BufferedDataManager mgr;
+    mgr.add(&color);
+
+    CPPUNIT_ASSERT(red == color);
+    color.set(green);
+    CPPUNIT_ASSERT(red == color);
+
+    mgr.flip();
+    CPPUNIT_ASSERT(green == color);
+
+    mgr.rem(&color);
 }
 
 }
--- dev/Basic/buffering/unit-tests/BufferedUnitTests.hpp
+++ dev/Basic/buffering/unit-tests/BufferedUnitTests.hpp
@@ -13,17 +13,98 @@ class BufferedUnitTests : public CppUnit::TestFixture
 {
 public:
     /**
-     * Tests the Buffered<float> constructor.
+     * Tests the default Buffered_uint32 constructor.
      *
-     * The Buffered<float> constructor should initialize current_ to 0.0.
-     * This test confirms that the value is indeed 0.0 via get() and
-     * the float conversion operator.
+     * The default Buffered_uint32 constructor should initialize current_ to 0.
+     * This test confirms that the value is indeed 0 via get() and the
+     * convert-to-uint32 operator.
+     */
+    void test_default_Buffered_uint32_constructor();
+
+    /**
+     * Tests the default Buffered<float> constructor.
+     *
+     * The default Buffered<float> constructor should initialize current_ to 0.0.
+     * This test confirms that the value is indeed 0.0 via get() and the
+     * convert-to-float operator.
      */
     void test_default_Buffered_float_constructor();
 
+    /**
+     * Tests the Buffered_uint32 constructor.
+     *
+     * This test confirms that the Buffered_uint32 constructor initializes current_ correctly.
+     */
+    void test_Buffered_uint32_constructor();
+
+    /**
+     * Tests the Buffered<float> constructor.
+     *
+     * This test confirms that the Buffered<float> constructor initializes current_ correctly.
+     */
+    void test_Buffered_float_constructor();
+
+    /**
+     * Tests that the BufferedDataManager is able to flip the Buffered_uint32 class correctly.
+     *
+     * This test confirms that the Buffered_uint32 object retains its current value until
+     * its data-manager calls its flip() method, at which time its current value is
+     * properly updated.
+     */
+    void test_flipping_after_single_change_to_Buffered_uint32();
+
+    /**
+     * Tests that the BufferedDataManager is able to flip the Buffered<float> class.
+     *
+     * This test confirms that the Buffered<float> object retains its current value until
+     * its data-manager calls its flip() method, at which time its current value is
+     * properly updated.
+     */
+    void test_flipping_after_single_change_to_Buffered_float();
+
+    /**
+     * Shows that changing Buffered<T> object multiple times doesn't affect its current value.
+     *
+     * This test confirms that the Buffered<T> object retains its current value, no matter
+     * how many times its next value is changed, until its data-manager calls its flip()
+     * method, at which time its current value is properly updated.
+     *
+     * The test also confirms that the flip() method sets its current value to the latest
+     * next value at the time flip() was called.
+     *
+     * This test tests the pre- and post- increment and decrement operators as well as the
+     * += and -= operators of the Buffered_uint32 class.
+     */
+    void test_flipping_after_multiple_changes();
+
+    /**
+     * Shows that flip() sets the current value to the latest next value.
+     *
+     * This test confirms that the flip() method sets its current value to the latest
+     * next value at the time flip() was called.  Even if the Buffered<T> object was
+     * changed multiple times, even to the point that it was reset to its original
+     * value, the flip() uses the last value of next_ to set the current_ value.
+     */
+    void test_multiple_changes_back_to_original_value();
+
+    /**
+     * Shows that enum can also be double-buffered.
+     *
+     * This test confirms that Buffered<enum> works as well as any Buffered<T> types.
+     */
+    void test_flipping_single_change_to_Buffered_enum();
+
 private:
     CPPUNIT_TEST_SUITE(BufferedUnitTests);
+        CPPUNIT_TEST(test_default_Buffered_uint32_constructor);
         CPPUNIT_TEST(test_default_Buffered_float_constructor);
+        CPPUNIT_TEST(test_Buffered_uint32_constructor);
+        CPPUNIT_TEST(test_Buffered_float_constructor);
+        CPPUNIT_TEST(test_flipping_after_single_change_to_Buffered_uint32);
+        CPPUNIT_TEST(test_flipping_after_single_change_to_Buffered_float);
+        CPPUNIT_TEST(test_flipping_after_multiple_changes);
+        CPPUNIT_TEST(test_multiple_changes_back_to_original_value);
+        CPPUNIT_TEST(test_flipping_single_change_to_Buffered_enum);
     CPPUNIT_TEST_SUITE_END();
 };
 

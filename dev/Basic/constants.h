8e60066804b749e9ccfcf420837efc3080da80db
Seth N. Hetu <seth.hetu@gmail.com>
2011-07-21 10:40:41 +0700

    Equality operator workaround for nullptr

--- dev/Basic/constants.h
+++ dev/Basic/constants.h
@@ -56,9 +56,15 @@ public:
 	//Covnertible to any type of null member pointer.
 	template<class C, class T> operator T C::*() const { return 0; }
 
+	//Workaround for GCC 4.5 equality bug.
+	template<class C, class T> bool operator == (C (T::*p)()) const { return  p == 0; }
+
 private:
 	//Cannot take the address of nullptr.
 	void operator&() const;
+
+	//Side-effect of equality bug: need to make == private
+	bool operator == (long) const;
 
 } nullptr = {};  //Single instance, named "nullptr"
 

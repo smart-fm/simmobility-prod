cb494c68e384592bb9f0ffdc0d1a2f69287065e4
Seth N. Hetu <seth.hetu@gmail.com>
2011-07-25 17:16:02 +0800

    (Forgot two files in the last commit)

---
+++ dev/visualizer/Traffic_Signal_Animation/src/sim_mob/AgentTick.java
@@ -0,0 +1,17 @@
+package sim_mob;
+
+//Simple class for visualizing an agent's state in one time tick.
+public class AgentTick {
+	public int agentID;
+	
+	public double agentX;
+	public double agentY;
+	
+	public int phaseSignal;
+	public double ds;
+	
+	public double cycleLen;
+	public int phaseCount;
+	
+	public double carDir;
+}
---
+++ dev/visualizer/Traffic_Signal_Animation/src/sim_mob/TimeTick.java
@@ -0,0 +1,8 @@
+package sim_mob;
+
+import java.util.Hashtable;
+
+//Simple class for visualizing the simulation's state in one time tick.
+public class TimeTick {
+	public Hashtable<Integer, AgentTick> agentTicks;
+}

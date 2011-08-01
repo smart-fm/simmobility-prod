188a3c59ed0cbb4ca3bc623c63c7ffc3edff2733
Seth N. Hetu <seth.hetu@gmail.com>
2011-08-01 23:13:48 +0700

    Some restructuring.

--- dev/Basic/geospatial/aimsun/Loader.cpp
+++ dev/Basic/geospatial/aimsun/Loader.cpp
@@ -13,20 +13,34 @@ using std::vector;
 
 namespace {
 
-void LoadNodes(std::vector<Node>& nodelist)
+void LoadNodes(soci::session& sql, std::vector<Node>& nodelist)
+{
+	//Our SQL statement
+	soci::rowset<Node> rs = (sql.prepare <<"select * from get_node()");
+
+	//Exectue as a rowset to avoid repeatedly building the query.
+	nodelist.clear();
+	for (soci::rowset<Node>::const_iterator it=rs.begin(); it!=rs.end(); ++it)  {
+		nodelist.push_back(*it);
+	    std::cout <<it->id << '\n';
+	}
+}
+
+
+
+void LoadBasicAimsunObjects(sim_mob::RoadNetwork& rn)
 {
 	//Connect
 	//NOTE: Our git repository is private (SSH-only) for now, so just storing the password to the DB here.
 	soci::session sql(soci::postgresql, "host=localhost port=5432 dbname=SimMobility_DB user=postgres password=S!Mm0bility");
 
-	//Our SQL statement
-	soci::rowset<Node> rs = (sql.prepare <<"select * from get_node()");
+	//Load all nodes
+	std::vector<Node> nodelist;
+	LoadNodes(sql, nodelist);
 
-	//Exectue as a rowset to avoid repeatedly building the query.
-	for (soci::rowset<Node>::const_iterator it=rs.begin(); it!=rs.end(); ++it)  {
-	     std::cout <<it->id << '\n';
-	}
+	//Load all sections
 }
+
 
 
 } //End anon namespace
@@ -35,9 +49,7 @@ void LoadNodes(std::vector<Node>& nodelist)
 bool sim_mob::aimsun::Loader::LoadNetwork(sim_mob::RoadNetwork& rn)
 {
 	//try {
-		//Load all nodes
-		std::vector<Node> nodelist;
-		LoadNodes(nodelist);
+		LoadBasicAimsunObjects(rn);
 
 
 

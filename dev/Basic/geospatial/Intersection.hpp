6f0635fba34a48603ccd039236acb019564ce90a
Seth N. Hetu <seth.hetu@gmail.com>
2011-08-14 20:44:37 +0700

    Intersections, roundabouts, and images in the doxygen output.

--- dev/Basic/geospatial/Intersection.hpp
+++ dev/Basic/geospatial/Intersection.hpp
@@ -23,23 +23,56 @@ class Loader;
  * required to diagram which Lanes lead to other lanes at a given intersection. The
  * Intersection class, however, adds a much-needed layer of context by specifying exactly
  * how the intersection actually looks and behaves.
+ *
+ * \todo
+ * The current image doesn't use the same terminology as the code. Also, I think we can
+ * provide a slightly clearer example (e.g., a T-intersection is probably sufficient).
+ *
+ * \todo
+ * Currently, we have several parallel vectors, where each entry is relevant to a RoadSegment
+ * at that intersection. It might be safer to group these elements (separator, etc.) into
+ * a new class and then have a single vector of items of this class.
+ *
+ * \image html intersection.jpg
+ *
  */
 class Intersection : public sim_mob::MultiNode {
 public:
 
 protected:
-	std::vector<float> separator;
+	///The length of each chunk. As each RoadSegment approaches an Intersection, a "chunk" of
+	///   this distance is taken from the end of that RoadSegment and is used to model details
+	///   specific to this intersection. A chunkLength of zero means that no special processing
+	///   is done for that RoadSegment at this Intersection.
+	std::vector<float> chunkLengths;
 
-	std::vector<float> percentChunks;
+	///List of which RoadSegments approaching an intersection have a separator in the median.
+	///
+	/// \note
+	/// This was originally a float; I changed it to a bool. ~Seth
+	std::vector<bool> separator;
 
+	///Vector of offsets
+	///
+	/// \note
+	/// I'm not clear on how offsets differ from chunks. ~Seth
 	std::vector<float> offsets;
 
+	///Vector of additional lanes on the dominant ("driving") side.
+	/// These lanes begin at a distance of chunkLength from the Intersection.
 	std::vector<sim_mob::Lane> additionalDominantLanes;
 
+	///Vector of additional lanes on the sub-dominant (opposite of "driving") side.
+	/// These lanes begin at a distance of chunkLength from the Intersection.
 	std::vector<sim_mob::Lane> additionalSubdominantLanes;
 
+	///Vector of flags; if true, then there is an island on the dominant ("driving") side
 	std::vector<bool> dominantIslands;
 
+	///Vector of obstacles.
+	///
+	/// \note
+	/// What's this for? Traffic lights? ~Seth
 	std::vector<RoadItem*> obstacles;
 
 friend class aimsun::Loader;
--- dev/Basic/geospatial/Roundabout.hpp
+++ dev/Basic/geospatial/Roundabout.hpp
@@ -14,25 +14,60 @@ namespace sim_mob
  * required to diagram which Lanes lead to other lanes at a given roundabout. The
  * Roundabout class, however, adds a much-needed layer of context by specifying exactly
  * how the roundabout actually looks and behaves.
+ *
+ * \todo
+ * The current image doesn't use the same terminology as the code. Also, I think we can
+ * provide a slightly clearer example (e.g., color-coded segments?).
+ *
+ * \todo
+ * See Intersection's note on having multiple parallel data structures.
+ *
+ * \image html roundabout.jpg
  */
 class Roundabout : public sim_mob::MultiNode {
 public:
 
 protected:
+	///List of which RoadSegments approaching a roundabout have a separator in the median.
 	std::vector<bool> separator;
 
-	std::vector<float> percentChunks;
+	///The length of each chunk. As each RoadSegment approaches a Roundabout, a "chunk" of
+	///   this distance is taken from the end of that RoadSegment and is used to model details
+	///   specific to this intersection. A chunkLength of zero means that no special processing
+	///   is done for that RoadSegment at this Intersection.
+	std::vector<float> chunkLengths;
 
+	///Vector of offsets
+	///
+	/// \note
+	/// I'm not clear on how offsets differ from chunks. ~Seth
 	std::vector<float> offsets;
 
+	///Angle of this RoadSegment's entrance into the Roundabout.
+	///
+	/// \note
+	/// What is this angle measured in reference to? ~Seth
 	std::vector<float> entranceAngle;
 
+	///Number of additional lanes (for each RoadSegment) which are added to the
+	///  dominant ("driving") side to allow for greater throughput on the Roundabout.
+	///Any additional lanes will end as soon as they reach the Roundabout, at which point
+	//   roundaboutNumberOfLanes remain.
 	std::vector<int> addDominantLane;
 
+	///Islands....
+	///
+	/// \note
+	/// Note sure how this is specified, esp. as a float. ~Seth
 	float roundaboutDominantIslands;
 
+	///Numer of lanes within the roundabout.
 	int roundaboutNumberOfLanes;
 
+	///Vector of obstacles.
+	///
+	/// \note
+	/// What's this for? Yield signs? ~Seth
 	std::vector<RoadItem*> obstacles;
 };
 

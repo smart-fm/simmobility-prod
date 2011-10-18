5352fdb3b3c474e113b424fc20d9476c91a4355e
Seth N. Hetu <seth.hetu@gmail.com>
2011-10-18 17:46:49 +0700

    Sensing a delayed perception. Will add some example Agent code.

--- dev/Basic/perception/FixedDelayed.hpp
+++ dev/Basic/perception/FixedDelayed.hpp
@@ -21,6 +21,11 @@ namespace sim_mob
  * \note
  * The FixedDelayed class copies items frequently, so if you are storing a large class or
  * non-copyable item, you should pass a pointer as the template paramter.
+ *
+ * \note
+ * This class should be used for continuous values. It should NOT be used for things like
+ * reactionary signals or threshold values, since a call to sense() may discard values if they
+ * do not represent the latest data.
  */
 template <typename T>
 class FixedDelayed {
@@ -33,28 +38,81 @@ public:
 
 
 	/**
+	 * Remove all delayed perceptions.
+	 */
+	void clear() {
+		history.clear();
+	}
+
+
+	/**
 	 * Delay a value, to be returned after the delay time.
 	 * \param value The value to delay.
-	 * \param currTimeMS The time at which this value physically occurred. It will be sense-able after currTimeMS + delayMS
+	 * \param observedTimeMS The time at which this value physically occurred. It will be sense-able after observedTimeMS + delayMS
 	 *
 	 * \note
-	 * The value of currTimeMS must always increase monotonically, or an exception is thrown.
+	 * The value of observedTimeMS must always increase monotonically, or an exception is thrown.
 	 */
-	void delay(const T& value, unsigned int currTimeMS) {
+	void delay(const T& value, uint32_t observedTimeMS) {
 		//Check consistency
-		if (!history.empty() && history.back().observedTime>currTimeMS) {
-			throw std::runtime_error("Delay called with an out-of-order currTimeMS.");
+		if (!history.empty() && history.back().observedTime>observedTimeMS) {
+			throw std::runtime_error("Delay called with an out-of-order observedTimeMS.");
 		}
 
 		//Save it
-		history.push_back(HistItem(value, currTimeMS));
+		history.push_back(HistItem(value, observedTimeMS));
+	}
+
+
+	/**
+	 * Retrieve the current perceived value of this item.
+	 *
+	 * \param currTimeMS The current time of the simulation.
+	 *
+	 * \note
+	 * currTimeMS must be monotonically increasing; sense() is used to retrieve values at
+	 * the current time tick, not any arbitrary time tick.
+	 */
+	const T& sense(uint32_t currTimeMS) {
+		//Consistency check, done via update.
+		if (!can_sense(currTimeMS)) {
+			throw std::runtime_error("Can't sense: not enough time has passed.");
+		}
+
+		//Return the first value in the list; can_sense() will have already performed the necessary updates.
+		return history.front().item;
+	}
+
+
+	/**
+	 * Return true if the current time is sufficiently advanced for sensing to occur.
+	 * Once this function returns true at least once, it will always return true (until clear() is called).
+	 */
+	bool can_sense(uint32_t currTimeMS) {
+		//Loop while the first value is "sense"-able.
+		while (!history.empty() && history.front().canObserve(currTimeMS)) {
+			//If the second element in the list is non-sensable, we're done.
+			if (history.size()==1 || !history.at(1).canObserve(currTimeMS)) {
+				return true;
+			}
+
+			//Otherwise, remove the first element.
+			history.pop_front();
+		}
+
+		//In this case, nothing can be observed
+		return false;
 	}
 
 
 private:
 	struct HistItem {
 		T item;
-		unsigned int observedTime;
+		uint32_t observedTime;
+
+		bool canObserve(uint32_t currTimeMS){
+			return observedTime + delayMS <= currTimeMS;
+		}
 	};
 
 	std::list<HistItem> history;

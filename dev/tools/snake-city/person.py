2872fc79c44ea00039a1c2ca7b00c63d20f4e64f
LIM Fung Chai <LIM.Fung.Chai@smart.mit.edu>
2011-11-29 09:45:20 +0800

    Added code to generate SQL insert statements into the Trip_chains table.

--- dev/tools/snake-city/person.py
+++ dev/tools/snake-city/person.py
@@ -6,61 +6,111 @@ import random, sys
 from town import Town_layout
 from company import Registrar_of_companies
 
-def generate_school_trips(student):
+class Trip:
+    def __init__(self, from_id, to_id, mode, start_time):
+        self.from_id = from_id
+        self.to_id = to_id
+        self.mode = mode
+        self.start_time = start_time
+
+def generate_school_trips(student, trip_chains, activities):
     name = "student name='%s'" % student.name
     trip =   "from home-address='%s' to school-address='%s'" \
            % (student.address, student.school.address)
-    start_time = "at 7:%02d:%02d" % (random.randint(0, 15), random.randint(0, 59))
+    start_time = "7:%02d:%02d" % (random.randint(0, 15), random.randint(0, 59))
     if "car" == student.mode:
-        print "Person name='%s' drives" % student.driver.name, name, trip, start_time
+        print "Person name='%s' drives" % student.driver.name, name, trip, "at", start_time
     else:
-        print name, "walks", trip, start_time
+        print name, "walks", trip, "at", start_time
+
+    key = "home-%d" % student.address
+    from_id = activities[key].id
+    key = "school-%d" % student.school.address
+    to_id = activities[key].id
+    trip_chains.append(Trip(from_id, to_id, "Walk", start_time))
 
     # catch a movie once in every 100 school days.
     if random.random() < 0.01:
         movie_house = random.choice(Registrar_of_companies.movie_houses)
         trip =   "walks from school-address='%s' to movie-house-address='%s'" \
                % (student.school.address, movie_house.address)
-        start_time = "at 3:%02d:%02d" % (random.randint(0, 45), random.randint(0, 59))
-        print name, trip, start_time
+        start_time = "3:%02d:%02d" % (random.randint(0, 45), random.randint(0, 59))
+        print name, trip, "at", start_time
+
+        from_id = to_id
+        key = "movie-%d" % movie_house.address
+        to_id = activities[key].id
+        trip_chains.append(Trip(from_id, to_id, "Walk", start_time))
 
         trip =   "walks from movie-house-address='%s' to home-address='%s'" \
                % (movie_house.address, student.address)
-        start_time = "at 6:%02d:%02d" % (random.randint(30, 59), random.randint(0, 59))
-        print name, trip, start_time
+        start_time = "6:%02d:%02d" % (random.randint(30, 59), random.randint(0, 59))
+        print name, trip, "at", start_time
+
+        from_id = to_id
+        key = "home-%d" % student.address
+        to_id = activities[key].id
+        trip_chains.append(Trip(from_id, to_id, "Walk", start_time))
     else:
         trip =   "walks from school-address='%s' to home-address='%s'" \
                % (student.school.address, student.address)
-        start_time = "at 3:%02d:%02d" % (random.randint(0, 45), random.randint(0, 59))
-        print name, trip, start_time
+        start_time = "3:%02d:%02d" % (random.randint(0, 45), random.randint(0, 59))
+        print name, trip, "at", start_time
 
-def generate_work_trips(person):
+        from_id = to_id
+        key = "home-%d" % student.address
+        to_id = activities[key].id
+        trip_chains.append(Trip(from_id, to_id, "Walk", start_time))
+
+def generate_work_trips(person, trip_chains, activities):
     name = "Person name='%s'" % person.name
     if "car" == person.mode:
         #if len(person.passengers):
         #    print name, " drives from school-address='%s'"
         trip =   "drives from home-address='%s' to work-address='%s'" \
                % (person.address, person.company.address)
-        start_time = "at 8:%02d:%02d" % (random.randint(0, 30), random.randint(0, 59))
-        print name, trip, start_time
+        start_time = "8:%02d:%02d" % (random.randint(0, 30), random.randint(0, 59))
+        print name, trip, "at", start_time
+
+        key = "home-%d" % person.address
+        from_id = activities[key].id
+        key = "work-%d" % person.company.address
+        to_id = activities[key].id
+        trip_chains.append(Trip(from_id, to_id, "Car", start_time))
     else:
         trip =   "walks from home-address='%s' to work-address='%s'" \
                % (person.address, person.company.address)
-        start_time = "at 8:%02d:%02d" % (random.randint(0, 30), random.randint(0, 59))
-        print name, trip, start_time
+        start_time = "8:%02d:%02d" % (random.randint(0, 30), random.randint(0, 59))
+        print name, trip, "at", start_time
+
+        key = "home-%d" % person.address
+        from_id = activities[key].id
+        key = "work-%d" % person.company.address
+        to_id = activities[key].id
+        trip_chains.append(Trip(from_id, to_id, "Walk", start_time))
 
     # Time for lunch
     cafe = random.choice(Registrar_of_companies.restuarants)
     trip =   "walks from work-address='%s' to restuarant-address='%s' for lunch" \
            % (person.company.address, cafe.address)
-    start_time = "at 12:%02d:%02d" % (random.randint(0, 15), random.randint(0, 59))
-    print name, trip, start_time
+    start_time = "12:%02d:%02d" % (random.randint(0, 15), random.randint(0, 59))
+    print name, trip, "at", start_time
+
+    from_id = to_id
+    key = "lunch-%d" % cafe.address
+    to_id = activities[key].id
+    trip_chains.append(Trip(from_id, to_id, "Walk", start_time))
 
     # Back to the office
     trip =   "walks from restuarant-address='%s' to work-address='%s'" \
            % (cafe.address, person.company.address)
-    start_time = "at 12:%02d:%02d" % (random.randint(45, 59), random.randint(0, 59))
-    print name, trip, start_time
+    start_time = "12:%02d:%02d" % (random.randint(45, 59), random.randint(0, 59))
+    print name, trip, "at", start_time
+
+    from_id = to_id
+    key = "work-%d" % person.company.address
+    to_id = activities[key].id
+    trip_chains.append(Trip(from_id, to_id, "Walk", start_time))
 
     # 20 % chance of having dinner before going home
     if random.random() < 0.2:
@@ -68,26 +118,42 @@ def generate_work_trips(person):
         trip =   "from office-address='%s' to resturant-address='%s'" \
                % (person.company.address, restuarant.address)
         mode = "drives" if "car" == person.mode else "walks"
-        start_time = "at 6:%02d:%02d" % (random.randint(30, 59), random.randint(0, 59))
-        print name, mode, trip, start_time
+        start_time = "6:%02d:%02d" % (random.randint(30, 59), random.randint(0, 59))
+        print name, mode, trip, "for dinner at", start_time
+
+        from_id = to_id
+        key = "dinner-%d" % restuarant.address
+        to_id = activities[key].id
+        trip_chains.append(Trip(from_id, to_id, "Car" if "car" == person.mode else "Walk", start_time))
 
         trip =   "from restuarant-address='%s' to home-address='%s'" \
                % (restuarant.address, person.address)
-        start_time = "at 8:%02d:%02d" % (random.randint(0, 15), random.randint(0, 59))
-        print name, mode, trip, start_time
+        start_time = "8:%02d:%02d" % (random.randint(0, 15), random.randint(0, 59))
+        print name, mode, trip, "at", start_time
+
+        from_id = to_id
+        key = "home-%d" % person.address
+        to_id = activities[key].id
+        trip_chains.append(Trip(from_id, to_id, "Car" if "car" == person.mode else "Walk", start_time))
     else:
         trip = "from office-address='%s' to home-address='%s'" \
                % (person.company.address, person.address)
         mode = "drives" if "car" == person.mode else "walks"
-        start_time = "at 6:%02d:%02d" % (random.randint(30, 59), random.randint(0, 59))
-        print name, mode, trip, start_time
+        start_time = "6:%02d:%02d" % (random.randint(30, 59), random.randint(0, 59))
+        print name, mode, trip, "at", start_time
+
+        from_id = to_id
+        key = "home-%d" % person.address
+        to_id = activities[key].id
+        trip_chains.append(Trip(from_id, to_id, "Car" if "car" == person.mode else "Walk", start_time))
 
 class Activity:
-    def __init__(self, type, primary, flexible, location):
+    def __init__(self, type, primary, flexible, location, id):
         self.type = type
         self.primary = primary
         self.flexible = flexible
         self.location = location
+        self.id = id
 
     def __repr__(self):
         return   "Activity type=%s is%s-primary is%s-flexible location=%d" \
@@ -97,6 +163,7 @@ class City:
     residents = list()
     max_population_size = 2000
     activities = dict()
+    trip_chains = list()
 
     @staticmethod
     def genesis():
@@ -113,7 +180,8 @@ class City:
         for person in City.residents:
             key = "home-%d" % person.address
             if key not in City.activities:
-                City.activities[key] = Activity("Home", True, False, person.address)
+                id = len(City.activities)
+                City.activities[key] = Activity("Home", True, False, person.address, id)
 
     @staticmethod
     def generate_work_place_activities(companies):
@@ -121,42 +189,48 @@ class City:
             if len(company.staff):
                 key = "work-%d" % company.address
                 if key not in City.activities:
-                    City.activities[key] = Activity("Work", True, False, company.address)
+                    id = len(City.activities)
+                    City.activities[key] = Activity("Work", True, False, company.address, id)
 
     @staticmethod
     def generate_shopping_activities():
         for shop in Registrar_of_companies.shops:
             key = "shopping-%d" % shop.address
             if key not in City.activities:
-                City.activities[key] = Activity("Shopping", False, True, shop.address)
+                id = len(City.activities)
+                City.activities[key] = Activity("Shopping", False, True, shop.address, id)
 
     @staticmethod
     def generate_school_activities():
         for school in Registrar_of_companies.schools:
             key = "school-%d" % school.address
             if key not in City.activities:
-                City.activities[key] = Activity("School", True, False, school.address)
+                id = len(City.activities)
+                City.activities[key] = Activity("School", True, False, school.address, id)
 
     @staticmethod
     def generate_lunch_activities():
         for restuarant in Registrar_of_companies.restuarants:
             key = "lunch-%d" % restuarant.address
             if key not in City.activities:
-                City.activities[key] = Activity("Lunch", False, True, restuarant.address)
+                id = len(City.activities)
+                City.activities[key] = Activity("Lunch", False, True, restuarant.address, id)
 
     @staticmethod
     def generate_dinner_activities():
         for restuarant in Registrar_of_companies.restuarants:
             key = "dinner-%d" % restuarant.address
             if key not in City.activities:
-                City.activities[key] = Activity("Dinner", False, True, restuarant.address)
+                id = len(City.activities)
+                City.activities[key] = Activity("Dinner", False, True, restuarant.address, id)
 
     @staticmethod
     def generate_movie_activities():
         for movie_house in Registrar_of_companies.movie_houses:
             key = "movie-%d" % movie_house.address
             if key not in City.activities:
-                City.activities[key] = Activity("Movie", False, True, movie_house.address)
+                id = len(City.activities)
+                City.activities[key] = Activity("Movie", False, True, movie_house.address, id)
 
     @staticmethod
     def generate_activities():
@@ -180,21 +254,41 @@ class City:
                 child = person
                 # a child is either in school or is at home; a toddler never gets to go anywhere.
                 if child.is_student:
-                    generate_school_trips(child)
+                    generate_school_trips(child, City.trip_chains, City.activities)
             elif person.company:
-                generate_work_trips(person)
+                generate_work_trips(person, City.trip_chains, City.activities)
 
     @staticmethod
     def dump_sql_statements():
+        City.dump_activity_spec_insert_statements()
+        print >>sys.stderr
+        City.dump_trip_chains_insert_statements()
+
+    @staticmethod
+    def dump_activity_spec_insert_statements():
         activity_types = {"Home" : 1, "Work" : 2, "Shopping" : 3,
                           "School" : 4, "Lunch" : 5, "Dinner" : 6, "Movie" : 8 }
-        sql = 'INSERT INTO "Activity_Spec"("Activity_Type", "Primary_Activity", '
+        sql = 'INSERT INTO "Activity_Spec"("Activity_Id", "Activity_Type", "Primary_Activity", '
         sql += '"Flexible_Activity", "Location_Id")'
-        for key, activity in City.activities.iteritems():
-            print >>sys.stderr, sql, 'VALUES(%d, %s, %s, %d);' % (activity_types[activity.type],
+        activities = City.activities.values()
+        activities.sort(key = lambda activity : activity.id) 
+        for activity in activities:
+            print >>sys.stderr, sql, 'VALUES(%d, %d, %s, %s, %d);' % (100 + activity.id,
+                                                           activity_types[activity.type],
                                                            "TRUE" if activity.primary else "FALSE",
                                                            "TRUE" if activity.flexible else "FALSE",
                                                            activity.location)
+
+    @staticmethod
+    def dump_trip_chains_insert_statements():
+        mode_choices = {"Car" : 1, "Walk" : 2, "Bus" : 3, "Taxi" : 4,
+                        "MRT" : 5, "Car Sharing" : 6, "Cycle" : 7, "Bike" : 8}
+        sql = 'INSERT INTO "Trip_Chains"("From_Activity_Id", "To_Activity_Id", '
+        sql += '"Mode_Of_Travel_Id", "Start_Time")'
+        for trip in City.trip_chains:
+            print >>sys.stderr, sql, 'VALUES(%d, %d, %d, "%s");' % (trip.from_id, trip.to_id,
+                                                                    mode_choices[trip.mode],
+                                                                    trip.start_time)
 
 class Person:
     def __init__(self, attrs=None):

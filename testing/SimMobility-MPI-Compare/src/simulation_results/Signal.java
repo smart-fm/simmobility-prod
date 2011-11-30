7c9c002fd2c9e16eb6b5d7d23cc4f04737650f08
xuyan <xuyan@comp.nus.edu.sg>
2011-11-30 17:08:08 +0800

    One Java program to compare results of [1]SimMobility V.S. SimMobility [2]SimMobility-MPI V.S. SimMobility-MPI [3]SimMobility V.S. SimMobility-MPI Read the readme firstly before use.

---
+++ testing/SimMobility-MPI-Compare/src/simulation_results/Signal.java
@@ -0,0 +1,131 @@
+package simulation_results;
+
+public class Signal
+{
+
+	private int id;
+
+	private String va;
+	private String vb;
+	private String vc;
+	private String vd;
+
+	private String pa;
+	private String pb;
+	private String pc;
+	private String pd;
+
+	private long xPos;
+	private long yPos;
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public String getVa()
+	{
+		return va;
+	}
+
+	public void setVa(String va)
+	{
+		this.va = va;
+	}
+
+	public String getVb()
+	{
+		return vb;
+	}
+
+	public void setVb(String vb)
+	{
+		this.vb = vb;
+	}
+
+	public String getVc()
+	{
+		return vc;
+	}
+
+	public void setVc(String vc)
+	{
+		this.vc = vc;
+	}
+
+	public String getVd()
+	{
+		return vd;
+	}
+
+	public void setVd(String vd)
+	{
+		this.vd = vd;
+	}
+
+	public String getPa()
+	{
+		return pa;
+	}
+
+	public void setPa(String pa)
+	{
+		this.pa = pa;
+	}
+
+	public String getPb()
+	{
+		return pb;
+	}
+
+	public void setPb(String pb)
+	{
+		this.pb = pb;
+	}
+
+	public String getPc()
+	{
+		return pc;
+	}
+
+	public void setPc(String pc)
+	{
+		this.pc = pc;
+	}
+
+	public String getPd()
+	{
+		return pd;
+	}
+
+	public void setPd(String pd)
+	{
+		this.pd = pd;
+	}
+
+	public long getxPos()
+	{
+		return xPos;
+	}
+
+	public void setxPos(long xPos)
+	{
+		this.xPos = xPos;
+	}
+
+	public long getyPos()
+	{
+		return yPos;
+	}
+
+	public void setyPos(long yPos)
+	{
+		this.yPos = yPos;
+	}
+
+}

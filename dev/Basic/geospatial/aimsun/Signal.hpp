6f6754b7e2ff1815d4f79d8c2f6ec0beaa68f142
xuyan <xuyan@comp.nus.edu.sg>
2011-12-06 16:24:43 +0800

    [1] Update the new Road network.     currntly, the GUI can see the latest road network with fake agents.

---
+++ dev/Basic/geospatial/aimsun/Signal.hpp
@@ -0,0 +1,24 @@
+/* Copyright Singapore-MIT Alliance for Research and Technology */
+
+#pragma once
+
+#include "Base.hpp"
+
+namespace sim_mob
+{
+
+namespace aimsun   // This namespace should be called "database" to be more generic.
+{
+    struct Signal : public Base
+    {
+        int id;
+        int nodeId;
+        double xPos;
+        double yPos;
+        double bearing;
+        std::string typeCode;
+    };
+
+}
+
+}

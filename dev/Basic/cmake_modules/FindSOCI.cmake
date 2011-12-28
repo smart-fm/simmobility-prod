cddb522eb0ba435c40fc4cfe4e4b4d4981b078d5
Seth N. Hetu <seth.hetu@gmail.com>
2011-12-28 14:14:11 +0800

    Added CMake's find_package syntax, which allows for better cross-platform library detection (without this, we're pretty much tied to Ubuntu's library naming scheme). Added module files into their own folder, so that we have better control over these. Created a PostgreSQL finder.

---
+++ dev/Basic/cmake_modules/FindSOCI.cmake
@@ -0,0 +1,58 @@
+## Find SOCI
+##
+## Copyright (c) 2010 Jamie Jones <jamie_jones_au@yahoo.com.au>
+## This file is licensed under the GNU GPLv2 or any later versions,
+##
+## This module defines
+## SOCI_LIBRARIES
+## SOCI_FOUND, if false, do not try to link to SOCI
+## SOCI_INCLUDE_DIRS, where to find the headers
+##
+
+FIND_PATH(SOCI_INCLUDE_DIRS soci.h
+    ${SOCI_DIR}/include
+    ${SOCI_DIR}/include/soci
+    $ENV{SOCI_DIR}/include
+    $ENV{SOCI_DIR}/include/soci
+    $ENV{SOCI_DIR}
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local/include
+    /usr/local/include/soci
+    /usr/include
+    /usr/include/soci
+    /sw/include # Fink
+    /opt/local/include # DarwinPorts
+    /opt/csw/include # Blastwave
+    /opt/include
+)
+
+FIND_LIBRARY(SOCI_LIBRARIES
+    NAMES soci_core  soci_core-gcc-3_0
+    PATHS
+    ${SOCI_DIR}/lib
+    $ENV{SOCI_DIR}/lib
+    $ENV{SOCI_DIR}
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local/lib
+    /usr/lib
+    /sw/lib
+    /opt/local/lib
+    /opt/csw/lib
+    /opt/lib
+)
+
+IF(SOCI_LIBRARIES)
+    IF (NOT SOCI_FIND_QUIETLY)
+    MESSAGE(STATUS "Found the SOCI library at ${SOCI_LIBRARIES}")
+    MESSAGE(STATUS "Found the SOCI headers at ${SOCI_INCLUDE_DIRS}")
+    ENDIF (NOT SOCI_FIND_QUIETLY)
+ENDIF(SOCI_LIBRARIES)
+
+IF(NOT SOCI_LIBRARIES)
+    IF (SOCI_FIND_REQUIRED)
+    MESSAGE(FATAL_ERROR "SOCI Not Found.")
+    ENDIF (SOCI_FIND_REQUIRED)
+ENDIF(NOT SOCI_LIBRARIES)
+
---
+++ dev/Basic/cmake_modules/FindSOCIPostgreSQL.cmake
@@ -0,0 +1,60 @@
+## Find SOCI - PostgreSQL
+##
+## Copyright (c) 2010 Jamie Jones <jamie_jones_au@yahoo.com.au>
+## This file is licensed under the GNU GPLv2 or any later versions,
+##
+## Copyright (c) 2011 Seth Hetu <seth.hetu@gmail.com>
+##
+## This module defines
+## SOCIPOSTGRESQL_LIBRARIES
+## SOCIPOSTGRESQL_FOUND, if false, do not try to link to SOCI-postgresql
+## SOCIPOSTGRESQL_INCLUDE_DIRS, where to find the headers
+##
+
+FIND_PATH(SOCIPOSTGRESQL_INCLUDE_DIRS soci-postgresql.h
+    ${SOCI_DIR}/include
+    ${SOCI_DIR}/include/soci
+    $ENV{SOCI_DIR}/include
+    $ENV{SOCI_DIR}/include/soci
+    $ENV{SOCI_DIR}
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local/include
+    /usr/local/include/soci/postgresql
+    /usr/include
+    /usr/include/soci/postgresql
+    /sw/include # Fink
+    /opt/local/include # DarwinPorts
+    /opt/csw/include # Blastwave
+    /opt/include
+)
+
+FIND_LIBRARY(SOCIPOSTGRESQL_LIBRARIES
+    NAMES soci_postgresql  soci_postgresql-gcc-3_0
+    PATHS
+    ${SOCI_DIR}/lib
+    $ENV{SOCI_DIR}/lib
+    $ENV{SOCI_DIR}
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local/lib
+    /usr/lib
+    /sw/lib
+    /opt/local/lib
+    /opt/csw/lib
+    /opt/lib
+)
+
+IF(SOCIPOSTGRESQL_LIBRARIES)
+    IF (NOT SOCIPOSTGRESQL_FIND_QUIETLY)
+    MESSAGE(STATUS "Found the SOCI PostgreSQL library at ${SOCIPOSTGRESQL_LIBRARIES}")
+    MESSAGE(STATUS "Found the SOCI PostgreSQL headers at ${SOCIPOSTGRESQL_INCLUDE_DIRS}")
+    ENDIF (NOT SOCIPOSTGRESQL_FIND_QUIETLY)
+ENDIF(SOCIPOSTGRESQL_LIBRARIES)
+
+IF(NOT SOCIPOSTGRESQL_LIBRARIES)
+    IF (SOCIPOSTGRESQL_FIND_REQUIRED)
+    MESSAGE(FATAL_ERROR "SOCI PostgreSQL Not Found.")
+    ENDIF (SOCIPOSTGRESQL_FIND_REQUIRED)
+ENDIF(NOT SOCIPOSTGRESQL_LIBRARIES)
+

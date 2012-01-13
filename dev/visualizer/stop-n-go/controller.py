0ea99eecb9da64a7b94acb19dfb14abdfc73634d
LIM Fung Chai <LIM.Fung.Chai@smart.mit.edu>
2012-01-13 10:01:14 +0800

    Moved visualizer.load to controller since it is more logical to be there.

--- dev/visualizer/stop-n-go/controller.py
+++ dev/visualizer/stop-n-go/controller.py
@@ -3,16 +3,25 @@
 # Copyright Singapore-MIT Alliance for Research and Technology
 
 from PyQt4 import QtGui, QtCore
+from gzip import GzipFile
+from bz2 import BZ2File
 
 import ui_controller
+from session import Session
+from static.network import Network
+from dynamic.city import City
+from animator import Animator
+from scene import Graphics_scene
 
 class Controller(QtGui.QDockWidget,
                  ui_controller.Ui_controller):
-    def __init__(self, parent=None):
+    def __init__(self, parent, visualizer_view, status_bar):
         super(Controller, self).__init__(parent)
+        self.parent = parent
         self.setupUi(self)
 
-        self.visualizer = parent
+        self.visualizer_view = visualizer_view
+        self.status_bar = status_bar
         self.interval = 100
 
     @QtCore.pyqtSignature("int")
@@ -21,10 +30,38 @@ class Controller(QtGui.QDockWidget,
 
     @QtCore.pyqtSignature("")
     def on_load_file_push_button_clicked(self):
-        file_name = QtGui.QFileDialog.getOpenFileName(self.visualizer, "Choose file to load", ".")
+        file_name = QtGui.QFileDialog.getOpenFileName(self.parent, "Choose file to load", ".")
         if len(file_name):
-            self.log_file_name_line_edit.setText(file_name)
-            self.visualizer.load(str(file_name))
+            self.load_log_file(str(file_name))
+
+    def load_log_file(self, file_name):
+        self.scene = Graphics_scene(self)
+        self.visualizer_view.setScene(self.scene)
+
+        self.session = Session()
+        self.road_network = Network()
+        self.animator = Animator(self.scene)
+        self.city = City(self.animator)
+
+        if file_name.endswith(".gz"):
+            input_file = GzipFile(file_name)
+        elif file_name.endswith(".bz2"):
+            input_file = BZ2File(file_name)
+        else:
+            input_file = open(file_name, 'r')
+        for line in input_file:
+            if self.session.parse(line):
+                continue
+            if self.road_network.parse(line):
+                continue
+            if self.city.parse(line):
+                continue
+        input_file.close()
+
+        self.road_network.resolve()
+        self.scene.draw_road_network(self.road_network)
+
+        self.log_file_name_line_edit.setText(file_name)
 
     @QtCore.pyqtSignature("bool")
     def on_stop_or_go_push_button_toggled(self, state):
@@ -43,7 +80,7 @@ class Controller(QtGui.QDockWidget,
 
     def run(self):
         frame_number = self.frame_number_spin_box.value()
-        self.visualizer.animator.go_to_frame_number(frame_number)
+        self.animator.go_to_frame_number(frame_number)
 
         if self.is_running:
             self.frame_number_spin_box.stepUp()
--- dev/visualizer/stop-n-go/visualizer.py
+++ dev/visualizer/stop-n-go/visualizer.py
@@ -3,51 +3,18 @@
 # Copyright Singapore-MIT Alliance for Research and Technology
 
 from PyQt4 import QtGui, QtCore
-from gzip import GzipFile
-from bz2 import BZ2File
 
-from session import Session
-from static.network import Network
-from dynamic.city import City
-from controller import Controller
-from animator import Animator
-from scene import Graphics_scene
 from view import Graphics_view
+from controller import Controller
 
 class Visualizer(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(Visualizer, self).__init__(parent)
         self.setWindowTitle("Stop and Go -- SimMobility Visualizer in PyQt4")
 
-        self.controller = Controller(self)
+        self.graphics_view = Graphics_view(self)
+        self.graphics_view.setMinimumSize(640, 480)
+        self.setCentralWidget(self.graphics_view)
+
+        self.controller = Controller(self, self.graphics_view, self.statusBar())
         self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self.controller)
-
-        self.view = Graphics_view(self)
-        self.setCentralWidget(self.view)
-
-    def load(self, file_name):
-        self.scene = Graphics_scene(self)
-        self.view.setScene(self.scene)
-
-        self.session = Session()
-        self.road_network = Network()
-        self.animator = Animator(self.scene)
-        self.city = City(self.animator)
-
-        if file_name.endswith(".gz"):
-            input_file = GzipFile(file_name)
-        elif file_name.endswith(".bz2"):
-            input_file = BZ2File(file_name)
-        else:
-            input_file = open(file_name, 'r')
-        for line in input_file:
-            if self.session.parse(line):
-                continue
-            if self.road_network.parse(line):
-                continue
-            if self.city.parse(line):
-                continue
-        input_file.close()
-
-        self.road_network.resolve()
-        self.scene.draw_road_network(self.road_network)

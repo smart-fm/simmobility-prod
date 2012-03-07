0c82560a782cb39964181fa856f46e6679e978e1
Seth N. Hetu <seth.hetu@gmail.com>
2012-03-07 17:45:03 +0800

    Moved FixedDelayed implementation to the bottom of the file; it's cleaner this way.

--- dev/Basic/perception/FixedDelayed.hpp
+++ dev/Basic/perception/FixedDelayed.hpp
@@ -44,15 +44,13 @@ public:
 	 * \param delayMS The time to delay each sensation.
 	 * \param reclaimPtrs If true, any item discarded by this history list is deleted. Does nothing if the template type is not a pointer.
 	 */
-	explicit FixedDelayed(size_t delayMS, bool reclaimPtrs=true) : maxDelayMS(delayMS), reclaimPtrs(reclaimPtrs) {}
+	explicit FixedDelayed(size_t delayMS, bool reclaimPtrs=true);
 
 
 	/**
 	 * Remove all delayed perceptions.
 	 */
-	void clear() {
-		history.clear();
-	}
+	void clear();
 
 
 	/**
@@ -63,15 +61,7 @@ public:
 	 * \note
 	 * The value of observedTimeMS must always increase monotonically, or an exception is thrown.
 	 */
-	void delay(const T& value, uint32_t observedTimeMS) {
-		//Check consistency
-		if (!history.empty() && history.back().observedTime>observedTimeMS) {
-			throw std::runtime_error("Delay called with an out-of-order observedTimeMS.");
-		}
-
-		//Save it
-		history.push_back(HistItem(value, observedTimeMS));
-	}
+	void delay(const T& value, uint32_t observedTimeMS);
 
 
 	/**
@@ -83,56 +73,14 @@ public:
 	 * currTimeMS must be monotonically increasing; sense() is used to retrieve values at
 	 * the current time tick, not any arbitrary time tick.
 	 */
-	const T& sense(uint32_t currTimeMS, uint32_t delayMS)
-	{
-		//Consistency check, done via update.
-		if (!can_sense(currTimeMS)) {
-			throw std::runtime_error("Can't sense: not enough time has passed.");
-		}
-
-		//Return the first value in the list; can_sense() will have already performed the necessary updates.
-		//const T& test = history.front().item;
-
-
-		typename std::list<HistItem>::iterator iter = history.begin();
-		while(iter!=history.end())
-		{
-			HistItem& histItem = *iter;
-			if((++iter)==history.end())
-				return histItem.item;
-			HistItem& histItemNext = *iter;
-			if(histItem.canObserve(currTimeMS,delayMS)&&(!histItemNext.canObserve(currTimeMS,delayMS)))
-				return histItem.item;
-		}
-		//if for loop doesn't return
-		return history.front().item; //Debugging....
-		//return history.front().item;
-	}
+	const T& sense(uint32_t currTimeMS, uint32_t delayMS);
 
 
 	/**
 	 * Return true if the current time is sufficiently advanced for sensing to occur.
 	 * Once this function returns true at least once, it will always return true (until clear() is called).
 	 */
-	bool can_sense(uint32_t currTimeMS) {
-		//Loop while the first value is "sense"-able.
-		while (!history.empty() && history.front().canObserve(currTimeMS, maxDelayMS)) {
-			//If the second element in the list is non-sensable, we're done.
-			if (history.size()==1 || !(++history.begin())->canObserve(currTimeMS, maxDelayMS)) {
-				return true;
-			}
-
-			//Otherwise, remove the first element.
-			if (reclaimPtrs) {
-				safe_delete_item(history.front().item);
-				history.pop_front();
-			}
-		}
-
-		//In this case, nothing can be observed
-		return false;
-	}
-
+	bool can_sense(uint32_t currTimeMS);
 
 
 
@@ -170,4 +118,82 @@ public:
 
 };
 
+}
+
+
+
+///////////////////////////////////////////////////////////
+// Template implementation
+///////////////////////////////////////////////////////////
+
+template <typename T>
+sim_mob::FixedDelayed<T>::FixedDelayed(size_t delayMS, bool reclaimPtrs)
+	: maxDelayMS(delayMS), reclaimPtrs(reclaimPtrs)
+{}
+
+
+template <typename T>
+void sim_mob::FixedDelayed<T>::clear()
+{
+	history.clear();
+}
+
+
+template <typename T>
+void sim_mob::FixedDelayed<T>::delay(const T& value, uint32_t observedTimeMS)
+{
+	//Check consistency
+	if (!history.empty() && history.back().observedTime>observedTimeMS) {
+		throw std::runtime_error("Delay called with an out-of-order observedTimeMS.");
+	}
+
+	//Save it
+	history.push_back(HistItem(value, observedTimeMS));
+}
+
+template <typename T>
+const T& sim_mob::FixedDelayed<T>::sense(uint32_t currTimeMS, uint32_t delayMS)
+{
+	//Consistency check, done via update.
+	if (!can_sense(currTimeMS)) {
+		throw std::runtime_error("Can't sense: not enough time has passed.");
+	}
+
+	//Return the first value in the list; can_sense() will have already performed the necessary updates.
+	//const T& test = history.front().item;
+
+
+	typename std::list<HistItem>::iterator iter = history.begin();
+	while(iter!=history.end())
+	{
+		HistItem& histItem = *iter;
+		if((++iter)==history.end())
+			return histItem.item;
+		HistItem& histItemNext = *iter;
+		if(histItem.canObserve(currTimeMS,delayMS)&&(!histItemNext.canObserve(currTimeMS,delayMS)))
+			return histItem.item;
+	}
+	//if for loop doesn't return
+	return history.front().item; //Debugging....
+	//return history.front().item;
+}
+
+template <typename T>
+bool sim_mob::FixedDelayed<T>::can_sense(uint32_t currTimeMS) {
+	//Loop while the first value is "sense"-able.
+	while (!history.empty() && history.front().canObserve(currTimeMS, maxDelayMS)) {
+		//If the second element in the list is non-sensable, we're done.
+		if (history.size()==1 || !(++history.begin())->canObserve(currTimeMS, maxDelayMS)) {
+			return true;
+		}
+
+		//Otherwise, remove the first element.
+		if (reclaimPtrs) {
+			safe_delete_item(history.front().item);
+			history.pop_front();
+		}
+	}
+
+	//In this case, nothing can be observed
+	return false;
 }

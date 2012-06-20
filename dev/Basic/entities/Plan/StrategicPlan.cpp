46ddd049cd1677ba816a275ce5f0a7f791cd7923
Seth N. Hetu <seth.hetu@gmail.com>
2012-06-20 21:25:41 +0800

    Manually adding files missing from the previous commit.

---
+++ dev/Basic/entities/Plan/StrategicPlan.cpp
@@ -0,0 +1,20 @@
+/*
+ * StrategicPlan.cpp
+ *
+ *  Created on: Jun 6, 2012
+ *      Author: vuvinhan
+ */
+
+#include "StrategicPlan.hpp"
+
+using namespace sim_mob;
+
+StrategicPlan::StrategicPlan() {
+	// TODO Auto-generated constructor stub
+
+}
+
+StrategicPlan::~StrategicPlan() {
+	// TODO Auto-generated destructor stub
+}
+

5f9224229c22019ef89c13a6d287cf704cb352a7
Paul Averilla <paulaverilla@gmail.com>
2012-08-21 14:07:25 +0800

    HelloQt v2 - simple hello qt window application with HTML-style formatting

--- testing/Qt/HelloQt/main.cpp
+++ testing/Qt/HelloQt/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication application(argc, argv);
-    QLabel *label = new QLabel("Hello Qt!");
+    QLabel *label = new QLabel("<h2><i>Hello</i> <font color=red>Qt!</font></h2>");
     label->show();
     return application.exec();
 }

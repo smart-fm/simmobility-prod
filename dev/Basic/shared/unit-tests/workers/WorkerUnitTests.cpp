a850f876f84a7910312522dbbbb6ba4d37e7651e
Seth N. Hetu <seth.hetu@gmail.com>
2012-09-01 13:41:40 +0800

    New unit test for oddly-spaced granularities. (Currently fails.)

--- dev/Basic/shared/unit-tests/workers/WorkerUnitTests.cpp
+++ dev/Basic/shared/unit-tests/workers/WorkerUnitTests.cpp
@@ -12,6 +12,7 @@
 #include "workers/WorkGroup.hpp"
 #include "workers/Worker.hpp"
 #include "entities/Agent.hpp"
+#include "entities/AuraManager.hpp"
 
 #include "WorkerUnitTests.hpp"
 
@@ -52,13 +53,17 @@ private:
 };
 
 
-//An Agent which sets a flag to 1 in its first time tick.
+//An Agent which sets a flag to 1 in its first time tick, and alternates between 0 and 1 after that.
 class FlagAgent : public NullAgent {
 public:
 	FlagAgent() : flag(0) {}
 
 	virtual Entity::UpdateStatus update(frame_t frameNumber) {
-		flag.set(1);
+		if (flag.get()==0) {
+			flag.set(1);
+		} else {
+			flag.set(0);
+		}
 		return Entity::UpdateStatus::Continue;
 	}
 
@@ -123,9 +128,6 @@ void unit_tests::WorkerUnitTests::test_SimpleWorkers()
 		WorkGroup::WaitAllGroups();
 	}
 
-	//Confirm that our WorkGroups are actually done.
-	//TODO: Perhaps with joinable?
-
 	//Now do the math  manually
 	for (int id=0; id<resInts.size(); id++) {
 		for (int i=0; i<5; i++) {
@@ -186,36 +188,6 @@ void unit_tests::WorkerUnitTests::test_MultipleGranularities()
 
 	//Agent update cycle
 	for (int i=0; i<3; i++) {
-		///////////////////////////////////////////////////////
-		//
-		//NOTE: This demonstrates one of our longer-running glitches:
-		//  1) If left this way, the agents will count to "1", since the counter agents will
-		//     update before the flag agents flip their values.
-		//  2) If switched, the correct value of "2" is obtained.
-		//  3) If you, say, add a println() statement to the Agents' update loops, this may skew the
-		//     results to "2".
-		//
-		//  This can *very* rarely lead to crashes, since agents' udpate cycles will overlap
-		//    just barely. Hence our random crashing when running lots of agents.
-		//
-		//  To fix this, we will need 1 barrier which is shared among all WorkGroups (2 in this case),
-		//    and a lock at each time tick. The real problem here is that the *Workers* need to lock,
-		//    but we want the *WorkGroups* to control the locking.
-		//
-		//  Another solution might involve splitting up the various *phases* of each WorkGroup's wait()
-		//    method into their own functions, and then calling, e.g., wg1.waitMain(); wg2.waitMain()...
-		//    etc., before calling wg1.waitFlip(); wg2.waitFlip(); etc.
-		//
-		//  We probably also want to give a unique barrier per wait stage, even though only 1 is needed
-		//    in practice. This reduces the risk of invalid math leading to hard-to-detect errors.
-		//
-		//  UPDATE: Simply splitting update() into stages doesn't seem 100% accurate, because now
-		//          one WorkGroup can be in flip() while a similar-granularity WorkGroup may be
-		//          in frame_tick(). Seems like we will still need some kind of lock sharing, but
-		//          this is complicated by our macro ticks. I wonder if boost has a more flexible
-		//          version of barrier, or if something like wait/notifyAll() makes some kind of sense.
-		//
-		///////////////////////////////////////////////////////
 		WorkGroup::WaitAllGroups();
 	}
 
@@ -227,6 +199,52 @@ void unit_tests::WorkerUnitTests::test_MultipleGranularities()
 	CPPUNIT_ASSERT_MESSAGE("Agent count failed", sumAg2->getCount()==2);
 	CPPUNIT_ASSERT_MESSAGE("Agent count failed", sumAg3->getCount()==2);
 	CPPUNIT_ASSERT_MESSAGE("Agent count failed", sumAg4->getCount()==2);
+
+	//Finally, clean up all the Work Groups and reset (for the next test)
+	WorkGroup::FinalizeAllWorkGroups();
+}
+
+
+void unit_tests::WorkerUnitTests::test_OddGranularities()
+{
+	//Add an "AuraManager" stage, just for testing purposes.
+	AuraManager::instance().init();
+
+	//Start with the same agent counters.
+	WorkGroup* countWG = WorkGroup::NewWorkGroup(2, 5);
+
+	//Have a flag counter @3ms, which toggles the flag between on and off.
+	WorkGroup* flagWG = WorkGroup::NewWorkGroup(1, 5, 3, &AuraManager::instance());
+
+	//Init all
+	WorkGroup::InitAllGroups();
+	countWG->initWorkers(nullptr);
+	flagWG->initWorkers(nullptr);
+
+	//Add the Flag agent
+	FlagAgent* flagAg = new FlagAgent();
+	flagAg->setStartTime(0);
+	flagWG->assignAWorker(flagAg);
+
+	//Add the counter agents.
+	CondSumAgent* sumAg1 = new CondSumAgent(*flagAg);
+	CondSumAgent* sumAg2 = new CondSumAgent(*flagAg);
+	sumAg1->setStartTime(0);
+	sumAg2->setStartTime(0);
+	countWG->assignAWorker(sumAg1);
+	countWG->assignAWorker(sumAg2);
+
+	//Agent update cycle
+	for (int i=0; i<5; i++) {
+		WorkGroup::WaitAllGroups();
+	}
+
+	//Confirm that the flag was removed (update in the first micro-tick).
+	CPPUNIT_ASSERT_MESSAGE("Agent flag is still set", flagAg->flag.get()==0);
+
+	//Confirm that each Agent has the correct total
+	CPPUNIT_ASSERT_MESSAGE("Agent count failed", sumAg1->getCount()==3);
+	CPPUNIT_ASSERT_MESSAGE("Agent count failed", sumAg2->getCount()==3);
 
 	//Finally, clean up all the Work Groups and reset (for the next test)
 	WorkGroup::FinalizeAllWorkGroups();
--- dev/Basic/shared/unit-tests/workers/WorkerUnitTests.hpp
+++ dev/Basic/shared/unit-tests/workers/WorkerUnitTests.hpp
@@ -21,17 +21,21 @@ public:
 	///Test what happens when workers have different, interacting granularities.
 	void test_MultipleGranularities();
 
+	///Test agents scheduled for 3ms ticks where the simulation ends at 5ms (not 6)
+	void test_OddGranularities();
+
 	//TODO: Test agents with different start times.
 
 	//TODO: Test each agent's update after each time tick.
 
-	//TODO: Test deleting workers/work groups
+
 
 
 private:
     CPPUNIT_TEST_SUITE(WorkerUnitTests);
         CPPUNIT_TEST(test_SimpleWorkers);
         CPPUNIT_TEST(test_MultipleGranularities);
+        CPPUNIT_TEST(test_OddGranularities);
     CPPUNIT_TEST_SUITE_END();
 };
 

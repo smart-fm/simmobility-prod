10f505456257723292bf469ed9ee2b5594c212e9
Seth N. Hetu <seth.hetu@gmail.com>
2012-09-05 17:47:01 +0800

    Sample structure for impl/skel files. The generated skel files will remain as one big hpp+cpp file. The implementation will be broken up into individual classes (since these are what we will be developing). The geo9.hpp file can be used to include "everything" you might need to load files from XML (boost does something like this).

---
+++ dev/Basic/shared/geospatial/xmlLoader/geo9.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+//You should #include this file to get access to all of the implementation files required to load
+//  our geospatial items from XML.
+
+//TODO: Split up various implementation files.
+//#include "implementation/geo9-pimpl.hpp"
+
+

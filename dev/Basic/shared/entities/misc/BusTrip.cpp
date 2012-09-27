485f3f1755b28a7aad2c5f33ed4c674174645b74
ForesterYao <yaojin@smart.mit.edu>
2012-09-27 17:49:59 +0800

    1. Temporary New DataStructure definition

--- dev/Basic/shared/entities/misc/BusTrip.cpp
+++ dev/Basic/shared/entities/misc/BusTrip.cpp
@@ -9,10 +9,14 @@
 
 using namespace sim_mob;
 
-sim_mob::BusStop_Times::BusStop_Times(std::string scheduled_ArrivalTime, std::string scheduled_DepartureTime,
-		std::string real_ArrivalTime, std::string real_DepartureTime)
-: scheduled_ArrivalTime(DailyTime(scheduled_ArrivalTime)), scheduled_DepartureTime(DailyTime(scheduled_DepartureTime)),
-  real_ArrivalTime(DailyTime(real_ArrivalTime)), real_DepartureTime(DailyTime(real_DepartureTime))
+sim_mob::BusStop_ScheduledTimes::BusStop_ScheduledTimes(std::string scheduled_ArrivalTime, std::string scheduled_DepartureTime)
+: scheduled_ArrivalTime(DailyTime(scheduled_ArrivalTime)), scheduled_DepartureTime(DailyTime(scheduled_DepartureTime))
+{
+
+}
+
+sim_mob::BusStop_RealTimes::BusStop_RealTimes(std::string real_ArrivalTime, std::string real_DepartureTime)
+: real_ArrivalTime(DailyTime(real_ArrivalTime)), real_DepartureTime(DailyTime(real_DepartureTime))
 {
 
 }
@@ -51,7 +55,37 @@ sim_mob::BusTrip::BusTrip(int entId, std::string type, unsigned int seqNumber,
 		unsigned int busRoute_id, Node* from, std::string fromLocType, Node* to,
 		std::string toLocType)
 : Trip(entId, type, seqNumber, start, end, busTrip_id,from, fromLocType, to, toLocType),
-busLine_id(busLine_id), vehicle_id(vehicle_id), bus_RouteInfo(&BusRouteInfo(busRoute_id))
+busLine_id(busLine_id), busTrip_id(busTrip_id), vehicle_id(vehicle_id), bus_RouteInfo(&BusRouteInfo(busRoute_id))
+{
+
+}
+
+sim_mob::Busline::Busline(int busline_id)
+{
+
+}
+
+sim_mob::Busline::~Busline()
+{
+
+}
+
+void sim_mob::Busline::addFwdBusTrip(const sim_mob::BusTrip& aFwdBusTrip)
+{
+	fwdbustrip_vec.push_back(aFwdBusTrip);
+}
+
+void sim_mob::Busline::addRevBusTrip(const sim_mob::BusTrip& aRevBusTrip)
+{
+	revbustrip_vec.push_back(aRevBusTrip);
+}
+
+sim_mob::PT_Schedule::PT_Schedule()
+{
+
+}
+
+sim_mob::PT_Schedule::~PT_Schedule()
 {
 
 }
--- dev/Basic/shared/entities/misc/BusTrip.hpp
+++ dev/Basic/shared/entities/misc/BusTrip.hpp
@@ -19,13 +19,19 @@
 #include "partitions/UnPackageUtils.hpp"
 #endif
 
+using std::vector;
+using std::string;
+
 namespace sim_mob {
 
-struct BusStop_Times{
-	explicit BusStop_Times(std::string scheduled_ArrivalTime, std::string scheduled_DepartureTime,
-			std::string real_ArrivalTime = "00:00:00", std::string real_DepartureTime = "00:00:00");
+struct BusStop_ScheduledTimes{
+	explicit BusStop_ScheduledTimes(std::string scheduled_ArrivalTime, std::string scheduled_DepartureTime);
 	sim_mob::DailyTime scheduled_ArrivalTime;
 	sim_mob::DailyTime scheduled_DepartureTime;
+};
+
+struct BusStop_RealTimes{
+	explicit BusStop_RealTimes(std::string real_ArrivalTime = "00:00:00", std::string real_DepartureTime = "00:00:00");
 	sim_mob::DailyTime real_ArrivalTime;// real Arrival Time
 	sim_mob::DailyTime real_DepartureTime;// real Departure Time
 };
@@ -36,9 +42,10 @@ public:
 	virtual ~BusStopInfo() {}
 private:
 	int stop_id;
-	std::string stop_name;
+	string stop_name;
 	int roadsegment_id;
-	BusStop_Times busStop_times;// for each particular BusTrip with this stop_id
+	BusStop_ScheduledTimes busStop_ScheduledTimes;// for each particular BusTrip with this stop_id
+	BusStop_RealTimes busStop_realTimes;
 };
 
 class BusRouteInfo { // need copy constructor since BusTrip copy the BusRoute, or may need assign constructor
@@ -46,10 +53,11 @@ public:
 	BusRouteInfo(unsigned int busRoute_id=0);
 	BusRouteInfo(const BusRouteInfo& copyFrom); ///<Copy constructor
 	virtual ~BusRouteInfo() {}
-	const std::vector<const sim_mob::RoadSegment*>& getRoadSegments() const {
+
+	const vector<const sim_mob::RoadSegment*>& getRoadSegments() const {
 		return roadSegment_vec;
 	}
-	const std::vector<const sim_mob::BusStopInfo*>& getBusStopsInfo() const {
+	const vector<const sim_mob::BusStopInfo*>& getBusStopsInfo() const {
 		return busStopInfo_vec;
 	}
 	void addBusStop(const sim_mob::BusStop* aBusStop);
@@ -57,9 +65,9 @@ public:
 	void addBusStopInfo(const sim_mob::BusStopInfo* aBusStopInfo);
 private:
 	unsigned int busRoute_id;
-	std::vector<const sim_mob::RoadSegment*> roadSegment_vec;
-	std::vector<const sim_mob::BusStopInfo*> busStopInfo_vec;
-	std::vector<const sim_mob::BusStop*> busStop_vec;
+	vector<const sim_mob::RoadSegment*> roadSegment_vec;
+	vector<const sim_mob::BusStopInfo*> busStopInfo_vec;
+	vector<const sim_mob::BusStop*> busStop_vec;
 };
 
 class BusTrip: public sim_mob::Trip {
@@ -70,11 +78,52 @@ public:
 			Node* from=nullptr, std::string fromLocType="node", Node* to=nullptr,
 			std::string toLocType="node");
 	virtual ~BusTrip() {}
+
+	const int getBusTripID() const {
+		return busTrip_id;
+	}
+	const int getBusLineID() const {
+		return busLine_id;
+	}
 private:
 	int busLine_id;
+	int busTrip_id;
 	int vehicle_id;
 	bool direction_flag; // indicate the direction of this BusTrip( two directions only)
 	BusRouteInfo* bus_RouteInfo;// route inside this BusTrip
+};
+
+
+
+class Busline { // busSchedule later inside PT_Schedule
+public:
+	Busline(int busline_id=0);
+	virtual ~Busline();
+
+	void addFwdBusTrip(const sim_mob::BusTrip& aFwdBusTrip);
+	void addRevBusTrip(const sim_mob::BusTrip& aRevBusTrip);
+	const vector<sim_mob::BusTrip>& getFwdBusTrips() const {
+		return fwdbustrip_vec;
+	}
+	const vector<sim_mob::BusTrip>& getRevBusTrips() const {
+		return revbustrip_vec;
+	}
+private:
+	int busline_id;
+	vector<sim_mob::BusTrip> fwdbustrip_vec;
+	vector<sim_mob::BusTrip> revbustrip_vec;
+};
+
+class PT_Schedule { // stored in BusController, Schedule Time Points and Real Time Points should be put separatedly
+public:
+	PT_Schedule();
+	virtual ~PT_Schedule();
+
+	const vector<sim_mob::Busline>& getBuslines() const {
+		return busline_vec;
+	}
+private:
+	vector<sim_mob::Busline> busline_vec;
 };
 
 }

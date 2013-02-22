babd77fbbe3f5808642037bdea6b74a804f3c4ea
Seth N. Hetu <seth.hetu@gmail.com>
2013-02-22 15:17:41 +0800

    Edges and Lanes parse.

--- dev/tools/sumo-convert/convert.py
+++ dev/tools/sumo-convert/convert.py
@@ -7,17 +7,47 @@ from lxml import etree
 #This program converts a SUMO traffic network to Sim Mobility format (this includes 
 # flipping the network for driving on the left).
 
-#Simple classes
+#Simple classes. IDs are always strings
 class Edge:
   def __init__(self, edgeId, fromNode, toNode):
-    self.edgeId = int(edgeId)
-    self.fromNode = int(fromNode)
-    self.toNode = int(toNode)
+    if not (edgeId and fromNode and toNode):
+      raise Exception('Null parameters in Edge constructor')
+    self.edgeId = str(edgeId)
+    self.fromNode = str(fromNode)
+    self.toNode = str(toNode)
+    self.lanes = []
+
+class Lane:
+  def __init__(self, laneId, shape):
+    if not (laneId and shape):
+      raise Exception('Null parameters in Lane constructor')
+    self.laneId = str(laneId)
+    self.shape = Shape(shape)
+
+
+#NOTE on Shapes, from the SUMO user's guide
+#The start and end node are omitted from the shape definition; an example: 
+#    <edge id="e1" from="0" to="1" shape="0,0 0,100"/> 
+#    describes an edge that after starting at node 0, first visits position 0,0 
+#    than goes one hundred meters to the right before finally reaching the position of node 1
+class Shape:
+  def __init__(self, pts):
+    pts = pts.split(' ')
+
+    self.points = []
+    for pair in pts:
+      pair = pair.split(',')
+      self.points.append(Point(float(pair[0]), float(pair[1])))
+
+
+class Point:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
 
 
 
-
-def parse_edge(e, edges):
+def parse_edge(e, edges, lanes):
     #Sanity check
     if e.get('function')=='internal':
       raise Exception('Node with from/to should not be internal')
@@ -26,11 +56,19 @@ def parse_edge(e, edges):
     res = Edge(e.get('id'), e.get('from'), e.get('to'))
     edges[res.edgeId] = res
 
+    #Add child Lanes
+    laneTags = e.xpath("lane")
+    for l in laneTags:
+      newLane = Lane(l.get('id'), l.get('shape'))
+      res.lanes.append(newLane)
+      lanes[newLane.laneId] = newLane
+
 
 
 def run_main(inFile):
   #Result containers
   edges = {}
+  lanes = {}
 
   #Load, parse
   inFile = open(inFile)
@@ -39,7 +77,7 @@ def run_main(inFile):
   #For each edge; ignore "internal"
   edgeTags = doc.xpath("/net/edge[(@from)and(@to)]")
   for e in edgeTags:
-    parse_edge(e, edges)
+    parse_edge(e, edges, lanes)
 
 
 

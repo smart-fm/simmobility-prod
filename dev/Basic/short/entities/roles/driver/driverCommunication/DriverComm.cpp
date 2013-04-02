4cdb66c60097b478caea26b430cf647941c7a067
vahid <vahid@vahid-ThinkPad-T420>
2013-04-02 15:20:03 +0800

    jus had forgotten to add some files

---
+++ dev/Basic/short/entities/roles/driver/driverCommunication/DriverComm.cpp
@@ -0,0 +1,97 @@
+
+#include "DriverComm.hpp"
+#include "entities/Person.hpp"
+#include "entities/communicator/Communicator.hpp"
+
+namespace sim_mob
+{
+
+sim_mob::DriverComm::DriverComm(Person* parent, sim_mob::MutexStrategy mtxStrat):Driver(parent,mtxStrat){ dataSent = false;}
+sim_mob::DriverComm::~DriverComm(){
+}
+
+Role* sim_mob::DriverComm::clone(Person* parent) const
+{
+	Role* role = 0;
+	role = new DriverComm(parent, parent->getMutexStrategy());
+
+	return role;
+}
+void sim_mob::DriverComm::receiveModule(timeslice now)
+{
+	//check if you have received anything in the incoming buffer
+	if(!isIncomingDirty()) return;
+	std::cout << "tick " << now.frame() << " [" << this->parent << "] icoming is dirty";
+	if(getIncoming().get().size() < 0) { std::cout << " Bu there are no data\n"; return; }
+	for(std::vector<DATA_MSG_PTR>::iterator it = getIncoming().get().begin(); it != getIncoming().get().end(); it++)
+	{
+		std::cout  << "tick " << now.frame() << " [" << this->parent << "]" << " Received[" << (*it)->str << std::endl;
+	}
+
+
+}
+void sim_mob::DriverComm::sendModule(timeslice now)
+{
+
+	sim_mob::dataMessage *data = new sim_mob::dataMessage();
+	data->str = "Hi man how are you";
+	std::vector<Entity*> &agents = sim_mob::Agent::all_agents;
+	std::vector<Entity*>::iterator  it , it_end(agents.end());
+	for(it = agents.begin(); it != it_end; it++)
+	{
+		//small filter
+		//1.send only to drivers
+		sim_mob::Person * person = dynamic_cast<sim_mob::Person *>(*it);
+		sim_mob::DriverComm* role = 0;
+		if(person)
+			role = dynamic_cast<sim_mob::DriverComm*>((person)->getRole());
+		if(!role) {
+//			std::cout << "Agent [" << *it << "] is not a driver \n" ;
+			continue;
+		}
+		//2.dont send to yourseld(for some reason, this is bigger than normal pointers
+		if(person == this->parent) {
+//			std::cout << "Agent [" << *it << "]  is the same as me \n" ;
+			continue;
+		}
+		data->receiver = (unsigned long)(*it);
+		addOutgoing(data);
+		std::cout << "\ntick" << now.frame() << " :: Agent [" << *it << "] pushed into outgoing\n";
+		dataSent = true;
+	}
+	//sorry for the hack.
+	//Seriously! you don't wnat to put this in the person/agent 's update method, you dont want to put it in the perform_main function
+	//then where to put this?
+	setAgentUpdateDone(true);
+}
+//Virtual implementations
+void DriverComm::frame_init(UpdateParams& p) {
+	Driver::frame_init(p);
+	subscribe(this->parent, sim_mob::NS3_Communicator::GetInstance());
+}
+;
+void DriverComm::frame_tick(UpdateParams& p) {
+	std::cout << "DriverComm::frame_tick(" << p.now.frame() << ")\n";
+	Driver::frame_tick(p);
+	if(p.now.frame() == 4)
+	{
+		sendModule(p.now);
+	}
+	else if(p.now.frame() > 4)//todo, just to test, just put else without if
+	{
+		receiveModule(p.now);
+	}
+	setAgentUpdateDone(true);
+
+}
+;
+void DriverComm::frame_tick_output(const UpdateParams& p) {
+	Driver::frame_tick_output(p);
+}
+;
+void DriverComm::frame_tick_output_mpi(timeslice now) {
+	Driver::frame_tick_output_mpi(now);
+}
+;
+}//namespace sim_mob
+
---
+++ dev/Basic/short/entities/roles/driver/driverCommunication/DriverComm.hpp
@@ -0,0 +1,25 @@
+#include "entities/roles/driver/Driver.hpp"
+#include "entities/communicator/CommunicationSupport.hpp"
+
+namespace sim_mob
+{
+class DriverComm : public Driver, public CommunicationSupport
+{
+	bool dataSent;
+public:
+
+	DriverComm(Person* parent, sim_mob::MutexStrategy mtxStrat);
+	virtual ~DriverComm();
+
+	virtual sim_mob::Role* clone(sim_mob::Person* parent) const;
+	//Virtual implementations
+	virtual void frame_init(UpdateParams& p);
+	virtual void frame_tick(UpdateParams& p);
+	virtual void frame_tick_output(const UpdateParams& p);
+	virtual void frame_tick_output_mpi(timeslice now);
+
+	void receiveModule(timeslice now);
+	void sendModule(timeslice now);
+};
+
+}//namspace sim_mob

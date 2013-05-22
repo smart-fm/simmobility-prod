ccbda2f03af5a6b5eb5d4597de6099d901283a68
huaipeng <huaipeng@smart.mit.edu>
2013-05-22 11:10:55 +0800

    add new class for FMOD controller

---
+++ dev/Basic/shared/entities/FMODController/FMODController.cpp
@@ -0,0 +1,21 @@
+/*
+ * FMODController.cpp
+ *
+ *  Created on: May 22, 2013
+ *      Author: zhang
+ */
+
+#include "FMODController.hpp"
+
+namespace sim_mob {
+
+FMODController::FMODController() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FMODController::~FMODController() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace sim_mob */
---
+++ dev/Basic/shared/entities/FMODController/FMODController.hpp
@@ -0,0 +1,20 @@
+/*
+ * FMODController.hpp
+ *
+ *  Created on: May 22, 2013
+ *      Author: zhang
+ */
+
+#ifndef FMODCONTROLLER_HPP_
+#define FMODCONTROLLER_HPP_
+
+namespace sim_mob {
+
+class FMODController {
+public:
+	FMODController();
+	virtual ~FMODController();
+};
+
+} /* namespace sim_mob */
+#endif /* FMODCONTROLLER_HPP_ */

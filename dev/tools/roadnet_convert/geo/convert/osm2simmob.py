b7b1b79b49acef6e18dadbcf528b271909c997ec
Seth N. Hetu <seth.hetu@gmail.com>
2013-06-10 13:13:05 -0400

    Saner approach for turning OSM Nodes/Ways into Sim Mobility Nodes/Segments/Links. Should work with overlapping Intersctions.

--- dev/tools/roadnet_convert/geo/convert/osm2simmob.py
+++ dev/tools/roadnet_convert/geo/convert/osm2simmob.py
@@ -5,8 +5,39 @@ from geo.helper import DynVect
 from geo.position import Point
 import geo.helper
 
+
+#Some minimal helper classes to help with conversion.
+class __RoadNetwork:
+  def __init__(self):
+    self.nodes = {}  #osm Node ID->__Node
+    self.ways = []
+
+class __Node:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.inter = False #Definitely an intersection? (start/end)
+    self.count = 0     #WaysAt
+
+  def is_uni(self):
+    return self.count==1 and not self.inter
+
+class __Way:
+  def __init__(self):
+    self.nodes = [] #List of Nodes in order. First and last will be multi; rest will be uni.
+
+
+
 def convert(rn :geo.formats.osm.RoadNetwork) -> geo.formats.simmob.RoadNetwork:
   '''Convert an OSM Road Network to a Sim Mobility network.'''
+  
+  #The first step is to rebuild the osm.RoadNetwork as __RoadNetwork, such that:
+  #  1) Every __Node's (x,y) position is already projected/reversed from lat/lng
+  #  2) Every __Node is tagged with "uni" if it should be a UniNode.
+  #  3) Every __Way maps between two nodes that are not "uni" --any nodes between the first and last are "uni", of course.
+  rn = __preprocess_osm(rn)
+
+  #Now we start to build the actual road network.
   res = geo.formats.simmob.RoadNetwork()
 
   #Simple GUID
@@ -15,15 +46,36 @@ def convert(rn :geo.formats.osm.RoadNetwork) -> geo.formats.simmob.RoadNetwork:
   #Map between old and new Nodes
   ndLookup = {} #OSM Node ID => (generated) Node
 
-  #Every Node can be converted by projecting, then scaling by a factor of 100.
-  #We also mirror the Y-component, since latitude increases going North.
-  #TODO: We *can* preserve osm IDs later, with the "orig-id" tag (or something similar)
-  zone = None
-  for nd in rn.nodes.values():
-    projected,zone = geo.helper.project_wgs84(nd.loc.lat, nd.loc.lng, zone)
-    newNode = geo.formats.simmob.Node(global_id.next(), 100*projected.x, -100*projected.y)
+  #Thanks to our pre-processing step, __Nodes are now trivial.
+  for nodeId,node in rn.nodes:
+    newNode = None
+    if node.is_uni():
+      newNode = geo.formats.simmob.Node(global_id.next(), node.x, node.y)
+    else:
+      newNode = geo.formats.simmob.Intersection(global_id.next(), node.x, node.y)
     res.nodes[newNode.nodeId] = newNode
-    ndLookup[nd.nodeId] = newNode
+    ndLookup[nodeId] = newNode
+
+  #__Ways define Links, with each UniNode representing a Segment (We can't handle polylines that well currently).
+  for wy in rn.ways:
+    #Sanity check.
+    if len(wy.nodes)<2:
+      raise Exception("OSM Ways must have >=2 Nodes listed.")
+
+    #Make the Link first.
+    lnk = geo.formats.simmob.Link(global_id.next(), wy.nodes[0], wy.nodes[-1])
+    res.links[lnk.linkId] = lnk
+
+    #Now, make a Segment for each pair of Nodes
+    prevNode = None
+    for nd in wy.nodes:
+      if prevNode:
+        seg = geo.formats.simmob.Segment(global_id.next(), prevNode, nd)
+        lnk.segments.append(seg)
+      prevNode = nd
+
+
+
 
   #Now add one segment/link per node pair. (For now Sim Mobility does not handle polylines that well).
   #TODO: We might not be handling UniNodes that well here.
@@ -95,6 +147,55 @@ def convert(rn :geo.formats.osm.RoadNetwork) -> geo.formats.simmob.RoadNetwork:
 
   #Some consistency checks
   __check_multi_uni(res, res.links)
+
+  return res
+
+
+
+def __preprocess_osm(rn :osm.formats.osm.RoadNetwork) ->__RoadNetwork:
+  res = __RoadNetwork()
+
+  #Every Node can be converted by projecting, then scaling by a factor of 100.
+  #We also mirror the Y-component, since latitude increases going North.
+  #TODO: We *can* preserve osm IDs later, with the "orig-id" tag (or something similar)
+  zone = None
+  for nd in rn.nodes.values():
+    projected,zone = geo.helper.project_wgs84(nd.loc.lat, nd.loc.lng, zone)
+    res.nodes.[nd.nodeId] = __Node(100*projected.x, -100*projected.y)
+
+  #We now iterate through every Way from the original OSM network, marking start and end nodes
+  #  in addition to counting the number of Ways at each Node.
+  for wy in rn.ways.values():
+    if len(wy.nodes)<2:
+      raise Exception("Ways in an OSM network can't have <2 Nodes")
+
+    #First/last Nodes are always MultiNodes
+    res.nodes[wy.nodes[0].nodeId].inter = True
+    res.nodes[wy.nodes[-1].nodeId].inter = True
+
+    #And count
+    for nd in wy.nodes:
+      res.nodes[nd.nodeId].count += 1
+
+  #We iterate through each Way again, this time splitting it into actual __Ways based on is_uni()'s return value.
+  currWay = None
+  for wy in rn.ways.values():
+    for nd in wy.nodes:
+      #Always count the current Node in the current Way
+      currNode = res.nodes[nd.nodeId]
+      if currWay:
+        currWay.nodes.append(currNode)
+
+      #Done with the current Way?
+      if !node.is_uni():
+        #Finalize the current (old) way.
+        if currWay:
+          res.ways.append(currWay)
+
+        #Start a new (current) way and add the current Node to it.
+        #NOTE: If this is the last Node, then "currWay" will just be useless (this is normal!)
+        currWay = __Way()
+        currWay.nodes.append(currNode)
 
   return res
 

aaee742873516cc8e2959a63a894cd36b5feb18c
Seth N. Hetu <seth.hetu@gmail.com>
2013-06-10 09:35:25 -0400

    Added licensing informatiton to most short-term and geospatial items.

--- dev/Basic/short/entities/roles/pedestrian/Pedestrian2.hpp
+++ dev/Basic/short/entities/roles/pedestrian/Pedestrian2.hpp
@@ -1,4 +1,6 @@
-/* Copyright Singapore-MIT Alliance for Research and Technology */
+//Copyright (c) 2013 Singapore-MIT Alliance for Research and Technology
+//Licensed under the terms of the MIT License, as described in the file:
+//   license.txt   (http://opensource.org/licenses/MIT)
 
 /*
  * \file Pedestrian2.hpp

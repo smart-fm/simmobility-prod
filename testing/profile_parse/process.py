4bccdbaf980f5e9182a14d2d5889f81cb5b1d76f
Seth N. Hetu <seth.hetu@gmail.com>
2013-09-25 14:54:34 +0800

    Cleanup; Worker parse implementation. Regex matches; still debugging.

--- testing/profile_parse/process.py
+++ testing/profile_parse/process.py
@@ -1,23 +1,95 @@
 #!/usr/bin/python2
 
+import sys
+import os
 import re
 
 #Declare our regexes
 LL_Key = '\"([^"]+)\"'
 LL_Value = '\"([^"]+)\"'
-LL_Property = " *#{LL_Key} *: *#{LL_Value},? *"
-LL_Line = "{(?:#{LL_Property})+}"
+LL_Property = " *%s *: *%s,? *" % (LL_Key, LL_Value)
+LL_Line = "{(?:%s)+}" % LL_Property
 LogPropRegex = re.compile(LL_Property)
 LogLineRegex = re.compile(LL_Line)
+
+print(LL_Line)
 
 #For parsing times:
 #(sec,X),(nano,Y)
 LogTimeRegex = re.compile('\( *sec *, *([0-9]+) *\) *, *\( *nano *, *([0-9]+) *\)')
 
+#Some helper classes.
+class WorkerTick:
+  def __init__(self):
+    self.time = None
+    self.id = None
+    self.tick = None
+    self.numAgents = None
+
+
+#Instances of the helper classes
+wrkStart = None
 
 
 
-def dispatch_line(props);
+def parse_work_tick(props, startTick, parseBegin):
+  if parseBegin and startTick:
+    print "Warning: skipping existing WorkTick"
+    return None
+  if not parseBegin and not startTick:
+    print "Warning: missing beginning WorkTick"
+    return None
+
+  if not('real-time' in props and 'worker' in props and 'tick' in props):
+    print "Warning: Skipping Worker; missing required properties."
+    return None
+  if parseBegin and not 'num-agents' in props:
+    print "Warning: Skipping Worker; missing required properties."
+    return None
+
+  res = WorkerTick()
+  res.time = parse_time(props['real-time'])
+  res.id = props['worker']
+  res.tick = props['tick']
+  if parseBegin:
+    res.numAgents = props['num-agents']
+  return res
+
+
+def print_work_tick(out, wStart, wEnd):
+  if not (wStart and wEnd):
+    print "Warning: skipping null start/end WorkTick"
+    return
+  if wStart.id != wEnd.id:
+    print "Warning: skipping start/end WorkTick; IDs don't match."
+    return
+  if wStart.tick != wEnd.tick:
+    print "Warning: skipping start/end WorkTick; time ticks don't match."
+    return
+
+  out.write('\n' % (wStart.id, wStart.tick, wEnd.time-wStart.time, wStart.numAgents))
+
+
+def dispatch_auramgr_update_action(out, act, props):
+  #TODO
+  pass
+
+def dispatch_query_action(out, act, props):
+  #TODO
+  pass
+
+
+def dispatch_worker_update_action(out, act, props):
+  if act=='worker-update-begin':
+    wrkStart = parse_work_tick(props, wrkStart, True)
+  elif act=='worker-update-end':
+    wrkEnd = parse_work_tick(props, wrkStart, False)
+    print_work_tick(out, wrkStart, wrkEnd)
+  else:
+    print ("Unknown worker action: %s" % act)
+
+
+def dispatch_line(out, props):
   #Make sure we have some key properties
   if not 'action' in props:
     raise Exception("Can't parse a line without an 'action'.")
@@ -26,41 +98,49 @@ def dispatch_line(props);
 
   #The rest we dispatch based on the action-type
   act = props['action']
-  if act.startsWith('worker-update-'):
-    dispatch_worker_update_action(act, props)
-  elif act.startsWith('query-'):
+  if act.startswith('worker-update-'):
+    dispatch_worker_update_action(out, act, props)
+  elif act.startswith('query-'):
     dispatch_query_action(act, props)
-  elif act.startsWith('auramgr-update-'):
+  elif act.startswith('auramgr-update-'):
     dispatch_auramgr_update_action(act, props)
   else:
     print ("Unknown action: %s" % act)
 
 
 def run_main(inFilePath):
+  #Prepare an output file.
+  out = open("workers.csv", 'w')
+
   for line in open(inFilePath):
     #Skip empty lines, comments
     line = line.strip()
-    if (len(line)==0) or line.startsWith('#'):
+    if (len(line)==0) or line.startswith('#'):
       continue
 
     #Parse it.
     if LogLineRegex.match(line):
       props = {}
       pos = 0
-      while mr = LogPropRegex.search(line, pos):
-        key = pos.group(1)
-        val = pos.group(2)
+      while True:
+        mr = LogPropRegex.search(line, pos)
+        if not mr:
+          break
+
+        key = mr.group(1)
+        val = mr.group(2)
+        print pos
         if key in props:
           raise Exception('Duplicate property: %s => %s' % (key, val))
         props[key] = val
-        pos = mr.pos+1
+        pos = mr.end(0)
     else:
       print ('Skipped: %s' % line)
       continue
 
     #Dispatch it
-    dispatch_line(props)
-  }
+    dispatch_line(out, props)
+
 
 
 

fc7b04f0114283163f524503b72964346bb56aef
gandola <gandola@dell-smart>
2013-10-10 11:05:37 +0800

    Add LuaBridge library

---
+++ dev/Basic/shared/lua/LuaLibrary.hpp
@@ -0,0 +1,20 @@
+//Copyright (c) 2013 Singapore-MIT Alliance for Research and Technology
+//Licensed under the terms of the MIT License, as described in the file:
+//   license.txt   (http://opensource.org/licenses/MIT)
+
+/* 
+ * Copyright Singapore-MIT Alliance for Research and Technology
+ * 
+ * File:   LuaProxy.hpp
+ * Author: Pedro Gandola <pedrogandola@smart.mit.edu>
+ *
+ * Created on October 4, 2013, 3:17 PM
+ */
+#pragma once
+
+extern "C"
+{
+#include "lua5.1/lua.h"
+#include "lua5.1/lualib.h"
+#include "lua5.1/lauxlib.h"
+}

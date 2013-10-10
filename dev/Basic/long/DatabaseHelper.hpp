44fc86681ddeeb5c4477c36e09e3b92bb5f3af9f
gandola <gandola@dell-smart>
2013-10-10 18:36:23 +0800

    Adapt Household and HouseholdDao  to the new DB schema

--- dev/Basic/long/DatabaseHelper.hpp
+++ dev/Basic/long/DatabaseHelper.hpp
@@ -49,6 +49,9 @@ const std::string DB_FIELD_BUILDING_ID = "building_id";
 const std::string DB_FIELD_ESTABLISMENT_ID = "establishment_id";
 const std::string DB_FIELD_TYPE_ID = "type_id";
 const std::string DB_FIELD_POSTCODE_ID = "postcode_id";
+const std::string DB_FIELD_LIFESTYLE_ID = "lifestyle_id";
+const std::string DB_FIELD_VEHICLE_CATEGORY_ID = "vehicle_category_id";
+const std::string DB_FIELD_ETHNICITY_ID = "ethnicity";
 const std::string DB_FIELD_INCOME = "income";
 const std::string DB_FIELD_FLOOR_AREA = "floor_area";
 const std::string DB_FIELD_YEAR = "year";
@@ -56,7 +59,8 @@ const std::string DB_FIELD_STOREY = "storey";
 const std::string DB_FIELD_RENT = "rent";
 const std::string DB_FIELD_SIZE = "size";
 const std::string DB_FIELD_CHILDREN = "children";
-const std::string DB_FIELD_CAR_OWNERSHIP = "car_ownership";
+const std::string DB_FIELD_WORKERS = "workers";
+const std::string DB_FIELD_AGE_OF_HEAD = "age_of_head";
 const std::string DB_FIELD_HOUSING_DURATION = "housing_duration";
 const std::string DB_FIELD_BUILT_YEAR = "built_year";
 const std::string DB_FIELD_STOREYS = "storeys";
@@ -92,7 +96,6 @@ const std::string DB_INSERT_HOUSEHOLD = "INSERT INTO " + DB_TABLE_HOUSEHOLD + " 
         + DB_FIELD_SIZE + ", "
         + DB_FIELD_CHILDREN + ", "
         + DB_FIELD_INCOME + ", "
-        + DB_FIELD_CAR_OWNERSHIP + ", "
         + DB_FIELD_HOUSING_DURATION + ") VALUES (:v1, :v2, :v3, :v4, :v5, :v6, :v7)";
 
 const std::string DB_INSERT_UNIT_TYPE = ""; // not defined yet...
@@ -109,8 +112,7 @@ const std::string DB_UPDATE_HOUSEHOLD = "UPDATE " + DB_TABLE_HOUSEHOLD + " SET "
         + DB_FIELD_SIZE + "= :v2, "
         + DB_FIELD_CHILDREN + "= :v3, "
         + DB_FIELD_INCOME + "= :v4, "
-        + DB_FIELD_CAR_OWNERSHIP + "= :v5, "
-        + DB_FIELD_HOUSING_DURATION + "= :v6 WHERE " + DB_FIELD_ID + "=:v7";
+        + DB_FIELD_HOUSING_DURATION + "= :v5 WHERE " + DB_FIELD_ID + "=:v6";
 
 const std::string DB_UPDATE_UNIT_TYPE = ""; // not defined yet...
 const std::string DB_UPDATE_BUILDING_TYPE = ""; // not defined yet...

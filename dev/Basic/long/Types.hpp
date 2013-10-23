0a383eff98c6cb47d0d002a84c2321f5be4d09b1
gandola <gandola@dell-smart>
2013-10-23 15:27:11 +0800

    Add types inside of long_term namespace

--- dev/Basic/long/Types.hpp
+++ dev/Basic/long/Types.hpp
@@ -15,76 +15,83 @@
 #include "stddef.h"
 #include "util/LangHelpers.hpp"
 
-typedef long long BigSerial;
-typedef BigSerial UnitId;
+namespace sim_mob {
+    namespace long_term {
+        
+        typedef long long BigSerial;
+        typedef BigSerial UnitId;
 
-enum Sex {
-    UNKNOWN_SEX = 0,
-    MASCULINE = 1,
-    FEMININE = 2
-};
+        const double INVALID_DOUBLE = -1;
 
-enum Race {
-    UNKNOWN_RACE = 0,
-    CHINISE = 1,
-    MALAY = 2,
-    INDIAN = 3,
-    OTHER = 4,
-};
+        enum Sex {
+            UNKNOWN_SEX = 0,
+            MASCULINE = 1,
+            FEMININE = 2
+        };
 
-enum EmploymentStatus {
-    UNKNOWN_STATUS = -1,
-    UNEMPLOYED = 0,
-    EMPLOYED = 1
-};
+        enum Race {
+            UNKNOWN_RACE = 0,
+            CHINISE = 1,
+            MALAY = 2,
+            INDIAN = 3,
+            OTHER = 4,
+        };
 
-enum TimeUnit {
-    DAILY = 0,
-    WEEKLY = 1,
-    MONTHLY = 2,
-    YEARLY = 3
-};
+        enum EmploymentStatus {
+            UNKNOWN_STATUS = -1,
+            UNEMPLOYED = 0,
+            EMPLOYED = 1
+        };
 
-static Race ToRace(int value) {
-    switch (value) {
-        case CHINISE: return CHINISE;
-        case MALAY: return MALAY;
-        case INDIAN: return INDIAN;
-        case OTHER: return OTHER;
-        default: return UNKNOWN_RACE;
+        enum TimeUnit {
+            DAILY = 0,
+            WEEKLY = 1,
+            MONTHLY = 2,
+            YEARLY = 3
+        };
+
+        static Race ToRace(int value) {
+            switch (value) {
+                case CHINISE: return CHINISE;
+                case MALAY: return MALAY;
+                case INDIAN: return INDIAN;
+                case OTHER: return OTHER;
+                default: return UNKNOWN_RACE;
+            }
+        }
+
+        static Sex ToSex(int value) {
+            switch (value) {
+                case MASCULINE: return MASCULINE;
+                case FEMININE: return FEMININE;
+                default: return UNKNOWN_SEX;
+            }
+        }
+
+        static EmploymentStatus ToEmploymentStatus(int value) {
+            switch (value) {
+                case UNEMPLOYED: return UNEMPLOYED;
+                case EMPLOYED: return EMPLOYED;
+                default: return UNKNOWN_STATUS;
+            }
+        }
+
+        static TimeUnit ToTimeUnit(int value) {
+            switch (value) {
+                case DAILY: return DAILY;
+                case WEEKLY: return WEEKLY;
+                case MONTHLY: return MONTHLY;
+                case YEARLY: return YEARLY;
+                default: return DAILY;
+            }
+        }
+
+        /**
+         * Structs
+         */
+        typedef struct ExpectationEntry_ {
+            double price;
+            double expectation;
+        } ExpectationEntry;
     }
 }
-
-static Sex ToSex(int value) {
-    switch (value) {
-        case MASCULINE: return MASCULINE;
-        case FEMININE: return FEMININE;
-        default: return UNKNOWN_SEX;
-    }
-}
-
-static EmploymentStatus ToEmploymentStatus(int value) {
-    switch (value) {
-        case UNEMPLOYED: return UNEMPLOYED;
-        case EMPLOYED: return EMPLOYED;
-        default: return UNKNOWN_STATUS;
-    }
-}
-
-static TimeUnit ToTimeUnit(int value) {
-    switch (value) {
-        case DAILY: return DAILY;
-        case WEEKLY: return WEEKLY;
-        case MONTHLY: return MONTHLY;
-        case YEARLY: return YEARLY;
-        default: return DAILY;
-    }
-}
-
-/**
- * Structs
- */
-typedef struct ExpectationEntry_ {
-    double price;
-    double expectation;
-} ExpectationEntry;

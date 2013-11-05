6ea8be8d3fa28642ce07b5ad6759689a0352fe05
ForesterYao <yaojin@smart.mit.edu>
2013-11-05 14:35:02 +0800

    add one template function in the Util class test it and another method

--- dev/Basic/shared/conf/ExpandAndValidateConfigFile.cpp
+++ dev/Basic/shared/conf/ExpandAndValidateConfigFile.cpp
@@ -26,6 +26,7 @@
 //#include "geospatial/xmlWriter/xmlWriter.hpp"
 #include "partitions/PartitionManager.hpp"
 #include "util/ReactionTimeDistributions.hpp"
+#include "util/Utils.hpp"
 #include "workers/WorkGroup.hpp"
 
 using namespace sim_mob;
@@ -396,11 +397,9 @@ void sim_mob::ExpandAndValidateConfigFile::GenerateXMLAgents(const std::vector<E
 		msg <<it->destPos.getX() <<"," <<it->destPos.getY();
 		props["destPos"] = msg.str();
 		}
-		{
-		std::stringstream msg;
-		msg << it->laneIndex;
-		props["lane"] = msg.str();
-		}
+
+		props["lane"] = Utils::numberToString<unsigned int>(it->laneIndex);
+//		props["lane"] = boost::lexical_cast<std::string>(it->laneIndex);
 		{
 			//Loop through attributes, ensuring that all required attributes are found.
 			//std::map<std::string, bool> propLookup = rf.getRequiredAttributes(roleName);

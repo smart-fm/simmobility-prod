2f6948770391aeebbbd0ca021be4056ea1c95e74
gandola <gandola@dell-smart>
2013-11-12 16:16:30 +0800

    Add ExternalEvent entity

---
+++ dev/Basic/long/database/entity/ExternalEvent.hpp
@@ -0,0 +1,55 @@
+/* 
+ * Copyright Singapore-MIT Alliance for Research and Technology
+ * 
+ * File:   ExternalEvent.hpp
+ * Author: Pedro Gandola <pedrogandola@smart.mit.edu>
+ *
+ * Created on November 12, 2013, 2:51 PM
+ */
+#pragma once
+#include "Types.hpp"
+
+namespace sim_mob {
+
+    namespace long_term {
+
+        class ExternalEvent {
+        public:
+
+            enum Type {
+                NEW_JOB = 0,
+                LOST_JOB,
+                NEW_JOB_LOCATION,
+                NEW_CHILD,
+                NEW_SCHOOL_LOCATION,
+                UNKNOWN,
+            };
+            
+        public:
+            ExternalEvent();
+            ExternalEvent(const ExternalEvent& orig);
+            virtual ~ExternalEvent();
+
+            /**
+             * Getters 
+             */
+            int getDay() const;
+            unsigned int getType() const; //int because lua
+            int getIntType() const;
+            BigSerial getHouseholdId() const;
+            
+            /**
+             * Setters 
+             */
+            void setDay(int day);
+            void setType(unsigned int type);
+            void setHouseholdId(BigSerial id);
+
+        private:
+            int day;
+            Type type;
+            BigSerial householdId;
+        };
+    }
+}
+

c5b8b442faf756e0acd4357835a4043f82e2d13c
gandola <gandola@dell-smart>
2013-11-12 16:18:00 +0800

    Change to use the new ExternalEvent

--- dev/Basic/long/core/EventsInjector.cpp
+++ dev/Basic/long/core/EventsInjector.cpp
@@ -9,9 +9,13 @@
 
 #include "EventsInjector.hpp"
 #include "model/lua/LuaProvider.hpp"
+#include "message/MessageBus.hpp"
 
 using namespace sim_mob;
 using namespace sim_mob::long_term;
+using namespace sim_mob::event;
+using namespace sim_mob::messaging;
+
 using std::vector;
 
 EventsInjector::EventsInjector() : Entity(-1) {
@@ -34,12 +38,15 @@ void EventsInjector::onWorkerExit() {
 }
 
 Entity::UpdateStatus EventsInjector::update(timeslice now) {
-
+    const ExternalEventsModel& model = LuaProvider::getExternalEventsModel();
+    
     vector<ExternalEvent> events;
-    LuaProvider::getExternalEventsModel().getExternalEvents(now.ms(), events);
+    //(now+1) - events for the next day once our events are 1 tick delayed
+    model.getExternalEvents((now.ms() + 1), events);
     vector<ExternalEvent>::iterator it = events.begin();
     for (it; it != events.end(); ++it) {
-        PrintOut("External Event day:[" << it->day << "] hh:[" << it->householdId << "] type:[" << it->type << "]" << std::endl);
+        PrintOut("External Event day:[" << it->getDay() << "] hh:[" << it->getHouseholdId() << "] type:[" << it->getType() << "]" << std::endl);
+        //MessageBus::PublishEvent();
     }
     return Entity::UpdateStatus(Entity::UpdateStatus::RS_CONTINUE);
 }

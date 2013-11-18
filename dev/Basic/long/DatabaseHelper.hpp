490c03732f9fba5a2d298c77c188a85cb125f14e
gandola <gandola@dell-smart>
2013-11-18 17:03:12 +0800

    Add SQL for PSQL functions API.

--- dev/Basic/long/DatabaseHelper.hpp
+++ dev/Basic/long/DatabaseHelper.hpp
@@ -12,10 +12,10 @@
 #pragma once
 
 namespace sim_mob {
-    
+
     namespace long_term {
-        
-        #define APPLY_SCHEMA(schema, field) std::string(schema)+std::string(field)
+
+#define APPLY_SCHEMA(schema, field) std::string(schema)+std::string(field)
 
         /**
          * Schemas
@@ -30,20 +30,26 @@ namespace sim_mob {
         /**
          * Tables
          */
-        const std::string DB_TABLE_GLOBAL_PARAMS = APPLY_SCHEMA(CURRENT_SCHEMA, "global_params");
-        const std::string DB_TABLE_UNIT_TYPE = APPLY_SCHEMA(CURRENT_SCHEMA, "unit_type");
-        const std::string DB_TABLE_HOUSEHOLD = APPLY_SCHEMA(CURRENT_SCHEMA, "household");
-        const std::string DB_TABLE_BUILDING_TYPE = APPLY_SCHEMA(CURRENT_SCHEMA, "building_type");
-        const std::string DB_TABLE_BUILDING = APPLY_SCHEMA(CURRENT_SCHEMA, "building");
-        const std::string DB_TABLE_UNIT = APPLY_SCHEMA(CURRENT_SCHEMA, "unit");
+        const std::string DB_TABLE_HOUSEHOLD = APPLY_SCHEMA(MAIN_SCHEMA, "household");
+        const std::string DB_TABLE_BUILDING = APPLY_SCHEMA(MAIN_SCHEMA, "building");
+        const std::string DB_TABLE_UNIT = APPLY_SCHEMA(MAIN_SCHEMA, "unit");
         /**
          * Views
-         */ 
-        const std::string DB_VIEW_UNIT = APPLY_SCHEMA(CURRENT_SCHEMA, "view_unit");
+         */
+        const std::string DB_VIEW_UNIT = APPLY_SCHEMA(MAIN_SCHEMA, "view_unit");
 
-        // housing market
-        const std::string DB_TABLE_SELLER_PARAMS = APPLY_SCHEMA(CURRENT_SCHEMA, "hm_seller_params");
-        const std::string DB_TABLE_BIDDER_PARAMS = APPLY_SCHEMA(CURRENT_SCHEMA, "hm_bidder_params");
+        /**
+         * Functions API
+         */
+        const std::string DB_FUNC_DEL_HOUSEHOLD_BY_ID = APPLY_SCHEMA(MAIN_SCHEMA, "deleteHouseholdById(:id)");
+        const std::string DB_FUNC_GET_HOUSEHOLDS = APPLY_SCHEMA(MAIN_SCHEMA, "getHouseholds()");
+        const std::string DB_FUNC_GET_HOUSEHOLD_BY_ID = APPLY_SCHEMA(MAIN_SCHEMA, "getHouseholdById(:id)");
+        const std::string DB_FUNC_DEL_UNIT_BY_ID = APPLY_SCHEMA(MAIN_SCHEMA, "deleteUnitById(:id)");
+        const std::string DB_FUNC_GET_UNITS = APPLY_SCHEMA(MAIN_SCHEMA, "getUnits()");
+        const std::string DB_FUNC_GET_UNIT_BY_ID = APPLY_SCHEMA(MAIN_SCHEMA, "getUnitById(:id)");
+        const std::string DB_FUNC_DEL_BUILDING_BY_ID = APPLY_SCHEMA(MAIN_SCHEMA, "deleteBuildingById(:id)");
+        const std::string DB_FUNC_GET_BUILDINGS = APPLY_SCHEMA(MAIN_SCHEMA, "getBuildings()");
+        const std::string DB_FUNC_GET_BUILDING_BY_ID = APPLY_SCHEMA(MAIN_SCHEMA, "getBuildingById(:id)");
 
         /**
          * Fields
@@ -84,86 +90,72 @@ namespace sim_mob {
         const std::string DB_FIELD_SQFT_PER_UNIT = "sqft_per_unit";
         const std::string DB_FIELD_LATITUDE = "latitude";
         const std::string DB_FIELD_LONGITUDE = "longitude";
-
         const std::string DB_FIELD_NAME = "name";
         const std::string DB_FIELD_TYPE = "type";
 
         /**
          * INSERT
          */
-        const std::string DB_INSERT_GLOBAL_PARAMS = "";
-        const std::string DB_INSERT_HOUSEHOLD = "INSERT INTO " + DB_TABLE_HOUSEHOLD + " ("
+        const std::string DB_INSERT_HOUSEHOLD = "INSERT INTO " 
+                + DB_TABLE_HOUSEHOLD + " ("
                 + DB_FIELD_ID + ", "
                 + DB_FIELD_UNIT_ID + ", "
                 + DB_FIELD_SIZE + ", "
                 + DB_FIELD_CHILDREN + ", "
                 + DB_FIELD_INCOME + ", "
-                + DB_FIELD_HOUSING_DURATION + ") VALUES (:v1, :v2, :v3, :v4, :v5, :v6, :v7)";
+                + DB_FIELD_HOUSING_DURATION 
+                + ") VALUES (:v1, :v2, :v3, :v4, :v5, :v6, :v7)";
 
-        const std::string DB_INSERT_UNIT_TYPE = DB_EMPTY_QUERY;
-        const std::string DB_INSERT_BUILDING_TYPE = DB_EMPTY_QUERY;
-        const std::string DB_INSERT_BUILDING = DB_EMPTY_QUERY; 
+        const std::string DB_INSERT_BUILDING = DB_EMPTY_QUERY;
         const std::string DB_INSERT_UNIT = DB_EMPTY_QUERY;
 
         /**
          * UPDATE
          */
-        const std::string DB_UPDATE_GLOBAL_PARAMS = "";
-        const std::string DB_UPDATE_HOUSEHOLD = "UPDATE " + DB_TABLE_HOUSEHOLD + " SET "
+        const std::string DB_UPDATE_HOUSEHOLD = "UPDATE " 
+                + DB_TABLE_HOUSEHOLD + " SET "
                 + DB_FIELD_UNIT_ID + "= :v1, "
                 + DB_FIELD_SIZE + "= :v2, "
                 + DB_FIELD_CHILDREN + "= :v3, "
                 + DB_FIELD_INCOME + "= :v4, "
-                + DB_FIELD_HOUSING_DURATION + "= :v5 WHERE " + DB_FIELD_ID + "=:v6";
+                + DB_FIELD_HOUSING_DURATION 
+                + "= :v5 WHERE " 
+                + DB_FIELD_ID + "=:v6";
 
-        const std::string DB_UPDATE_UNIT_TYPE = DB_EMPTY_QUERY;
-        const std::string DB_UPDATE_BUILDING_TYPE = DB_EMPTY_QUERY;
         const std::string DB_UPDATE_BUILDING = DB_EMPTY_QUERY;
         const std::string DB_UPDATE_UNIT = DB_EMPTY_QUERY;
 
         /**
          * DELETE
          */
-        const std::string DB_DELETE_GLOBAL_PARAMS = "";
-        const std::string DB_DELETE_UNIT_TYPE = "DELETE FROM " + DB_TABLE_UNIT_TYPE + " WHERE " + DB_FIELD_ID + "=:id";
-        const std::string DB_DELETE_HOUSEHOLD = "DELETE FROM " + DB_TABLE_HOUSEHOLD + " WHERE " + DB_FIELD_ID + "=:id";
-        const std::string DB_DELETE_BUILDING_TYPE = "DELETE FROM " + DB_TABLE_BUILDING_TYPE + " WHERE " + DB_FIELD_ID + "=:id";
-        const std::string DB_DELETE_BUILDING = "DELETE FROM " + DB_TABLE_BUILDING + " WHERE " + DB_FIELD_ID + "=:id";
-        const std::string DB_DELETE_UNIT = "DELETE FROM " + DB_TABLE_UNIT + " WHERE " + DB_FIELD_ID + "=:id";
+        const std::string DB_DELETE_HOUSEHOLD = "SELECT * FROM " 
+                + DB_FUNC_DEL_HOUSEHOLD_BY_ID;
+        const std::string DB_DELETE_BUILDING = "SELECT * FROM "
+                + DB_FUNC_DEL_BUILDING_BY_ID;
+        const std::string DB_DELETE_UNIT = "SELECT * FROM " 
+                + DB_FUNC_DEL_UNIT_BY_ID;
 
         /**
          * GET ALL
          */
-        const std::string DB_GETALL_GLOBAL_PARAMS = "SELECT * FROM " + DB_TABLE_GLOBAL_PARAMS + " ORDER BY " + DB_FIELD_ID + LIMIT;
-        const std::string DB_GETALL_UNIT_TYPE = "SELECT * FROM " + DB_TABLE_UNIT_TYPE + " ORDER BY " + DB_FIELD_ID + LIMIT;
-        const std::string DB_GETALL_HOUSEHOLD = "SELECT * FROM " + DB_TABLE_HOUSEHOLD + " ORDER BY " + DB_FIELD_ID + LIMIT;
-        const std::string DB_GETALL_BUILDING_TYPE = "SELECT * FROM " + DB_TABLE_BUILDING_TYPE + " ORDER BY " + DB_FIELD_ID + LIMIT;
-        const std::string DB_GETALL_BUILDING = "SELECT * FROM " + DB_TABLE_BUILDING + " ORDER BY " + DB_FIELD_ID + LIMIT;
-        const std::string DB_GETALL_UNIT = "SELECT * FROM " + DB_VIEW_UNIT + " ORDER BY " + DB_FIELD_ID + LIMIT;
+        const std::string DB_GETALL_HOUSEHOLD = "SELECT * FROM "
+                + DB_FUNC_GET_HOUSEHOLDS
+                + LIMIT;
+        const std::string DB_GETALL_BUILDING = "SELECT * FROM "
+                + DB_FUNC_GET_BUILDINGS
+                + LIMIT;
+        const std::string DB_GETALL_UNIT = "SELECT * FROM "
+                + DB_FUNC_GET_UNITS
+                + LIMIT;
 
         /**
          * GET BY ID
          */
-        const std::string DB_GETBYID_GLOBAL_PARAMS = "SELECT * FROM " + DB_TABLE_GLOBAL_PARAMS + " WHERE " + DB_FIELD_ID + "=:id";
-        const std::string DB_GETBYID_UNIT_TYPE = "SELECT * FROM " + DB_TABLE_UNIT_TYPE + " WHERE " + DB_FIELD_ID + "=:id";
-        const std::string DB_GETBYID_HOUSEHOLD = "SELECT * FROM " + DB_TABLE_HOUSEHOLD + " WHERE " + DB_FIELD_ID + "=:id";
-        const std::string DB_GETBYID_BUILDING_TYPE = "SELECT * FROM " + DB_TABLE_BUILDING_TYPE + " WHERE " + DB_FIELD_ID + "=:id";
-        const std::string DB_GETBYID_BUILDING = "SELECT * FROM " + DB_TABLE_BUILDING + " WHERE " + DB_FIELD_ID + "=:id";
-        const std::string DB_GETBYID_UNIT = "SELECT * FROM " + DB_VIEW_UNIT + " WHERE " + DB_FIELD_ID + "=:id";
-
-
-
-        // HOUSING MARKET
-
-        const std::string DB_INSERT_SELLER_PARAMS = "";
-        const std::string DB_INSERT_BIDDER_PARAMS = "";
-        const std::string DB_UPDATE_SELLER_PARAMS = "";
-        const std::string DB_UPDATE_BIDDER_PARAMS = "";
-        const std::string DB_DELETE_SELLER_PARAMS = "DELETE FROM " + DB_TABLE_SELLER_PARAMS + " WHERE " + DB_FIELD_HOUSEHOLD_ID + "=:id";
-        const std::string DB_DELETE_BIDDER_PARAMS = "DELETE FROM " + DB_TABLE_BIDDER_PARAMS + " WHERE " + DB_FIELD_HOUSEHOLD_ID + "=:id";
-        const std::string DB_GETALL_SELLER_PARAMS = "SELECT * FROM " + DB_TABLE_SELLER_PARAMS;
-        const std::string DB_GETALL_BIDDER_PARAMS = "SELECT * FROM " + DB_TABLE_BIDDER_PARAMS;
-        const std::string DB_GETBYID_SELLER_PARAMS = "SELECT * FROM " + DB_TABLE_SELLER_PARAMS + " WHERE " + DB_FIELD_HOUSEHOLD_ID + "=:id";
-        const std::string DB_GETBYID_BIDDER_PARAMS = "SELECT * FROM " + DB_TABLE_BIDDER_PARAMS + " WHERE " + DB_FIELD_HOUSEHOLD_ID + "=:id";
+        const std::string DB_GETBYID_HOUSEHOLD = "SELECT * FROM "
+                + DB_FUNC_GET_HOUSEHOLD_BY_ID;
+        const std::string DB_GETBYID_BUILDING = "SELECT * FROM " 
+                + DB_FUNC_GET_BUILDING_BY_ID;
+        const std::string DB_GETBYID_UNIT = "SELECT * FROM " +
+                DB_FUNC_GET_UNIT_BY_ID;
     }
 }

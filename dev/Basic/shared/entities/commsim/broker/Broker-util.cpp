8e775592b8d953e5bb4c311ab8c1278180f5939d
Seth N. Hetu <seth.hetu@gmail.com>
2013-11-20 16:04:06 -0500

    Removed commented code; cleaned up const-correctness. Removed a few unused functions/classes.

--- dev/Basic/shared/entities/commsim/broker/Broker-util.cpp
+++ dev/Basic/shared/entities/commsim/broker/Broker-util.cpp
@@ -21,9 +21,6 @@ void sim_mob::AgentsList::insert(Agent * a, AgentCommUtilityBase * b, bool valid
 {
 	Lock lock(mutex);
 	data.insert(std::make_pair(a, AgentInfo(a,b,valid)));
-
-	//Agents &aa = data.get<agent_tag>();
-	//aa.insert(AgentInfo(a,b,valid));
 }
 
 void sim_mob::AgentsList::AgentsList::erase(Agent * agent)
@@ -37,24 +34,8 @@ void sim_mob::AgentsList::AgentsList::erase(Agent * agent)
 	} else {
 		Print() << "AgentsList::erase test:  agent[" << agent << "] NOT found for deletion" << std::endl;
 	}
-
-	/*Agents &agents = data.get<agent_tag>();
-	if(agents.find(agent) != agents.end())
-	{
-		Print() << "AgentsList::erase test:  agent[" << agent << "] found for deletion" << std::endl;
-	} else {
-		Print() << "AgentsList::erase test:  agent[" << agent << "] NOT found for deletion" << std::endl;
-	}
-
-	agents.erase(agent);*/
 }
 
-/*void sim_mob::AgentsList::erase(AgentCommUtilityBase * comm)
-{
-	Lock lock(mutex);
-	AgentCommUtilities &comms = data.get<agent_util>();
-	comms.erase(comm);
-}*/
 
 void sim_mob::AgentsList::eraseInvalids()
 {
@@ -66,16 +47,6 @@ void sim_mob::AgentsList::eraseInvalids()
 			data.erase(it); //FYI: map::erase doesn't invalidate iterators.
 		}
 	}
-
-
-
-/*	Valids & valids = data.get<agent_valid>();
-	Valids::iterator it_begin, it_end;
-	boost::tuples::tie(it_begin, it_end) = valids.equal_range(false);
-	while(it_begin != it_end) {
-		Valids::iterator it_erase = it_begin++;
-		valids.erase(it_erase);
-	}*/
 }
 
 void sim_mob::AgentsList::for_each_agent(boost::function<void(sim_mob::Agent*)> Fn)
@@ -88,14 +59,6 @@ void sim_mob::AgentsList::for_each_agent(boost::function<void(sim_mob::Agent*)> 
 		}
 		Fn(it->second.agent);
 	}
-
-	/*AgentsList::type & agents = data.get<0>();
-	for(AgentsList::iterator it= agents.begin(), it_end(agents.end()); it != it_end; it++ ) {
-		if(it->valid == false){
-			Print() << "Warning, Working with an Invalid Agent" << std::endl;
-		}
-		Fn(it->agent);
-	}*/
 }
 
 /*******************************************************************
@@ -103,25 +66,17 @@ void sim_mob::AgentsList::for_each_agent(boost::function<void(sim_mob::Agent*)> 
  * ******************************************************************
  */
 
-int sim_mob::AgentsList::size( )
+int sim_mob::AgentsList::size() const
 {
 	Lock lock(mutex);
-
 	return data.size();
-
-	/*Agents &aa = data.get<agent_tag>();
-	return aa.size();*/
 }
 
 
-bool sim_mob::AgentsList::empty()
+bool sim_mob::AgentsList::empty() const
 {
 	Lock lock(mutex);
-
 	return data.empty();
-
-	/*Agents &aa = data.get<agent_tag>();
-	return aa.empty();*/
 }
 
 /*******************************************************************
@@ -131,9 +86,7 @@ bool sim_mob::AgentsList::empty()
 AgentsList::type& sim_mob::AgentsList::getAgents()
 {
 	Lock lock(mutex);
-
 	return data;
-	//return data.get<0>();
 }
 
 AgentsList::Mutex* sim_mob::AgentsList::getMutex()
@@ -142,74 +95,6 @@ AgentsList::Mutex* sim_mob::AgentsList::getMutex()
 }
 
 
-/*******************************************************************
- * ********* VALID ************************************************
- * ******************************************************************
- */
-
-int sim_mob::AgentsList::Validate(Agent * agent)
-{
-	Lock lock(mutex);
-
-	type::iterator it = data.find(agent);
-	if (it!=data.end()) {
-		if (it->second.valid) {
-			return 1;
-		} else {
-			return 0;
-		}
-	}
-
-	return -1;
-
-	/*Agents &agents = data.get<agent_tag>();
-	Agents::iterator it = agents.find(agent);
-	if((it = agents.find(agent)) != agents.end()) {
-		if(it->valid) {
-			return 1;
-		} else {
-			return 0;
-		}
-	}
-	return -1;*/
-}
-
-/*int sim_mob::AgentsList::Validate(AgentCommUtilityBase * comm)
-{
-	Lock lock(mutex);
-	AgentCommUtilities &comms = data.get<agent_util>();
-	AgentCommUtilities::iterator it;
-	if((it = comms.find(comm)) != comms.end()) {
-		if(it->valid) {
-			return 1;
-		} else {
-			return 0;
-		}
-	}
-	return -1;
-}*/
-
-
-sim_mob::AgentsList::change_valid::change_valid(bool new_value):new_value(new_value)
-{
-}
-
-void sim_mob::AgentsList::change_valid::operator()(AgentInfo& e)
-{
-	 e.valid=new_value;
-}
-
-/*bool sim_mob::AgentsList::setValid(AgentCommUtilityBase * comm , bool value)
-{
-	Lock lock(mutex);
-	bool success = false;
-	AgentCommUtilities &comms = data.get<agent_util>();
-	AgentCommUtilities::iterator it;
-	if((it = comms.find(comm)) != comms.end()) {
-		comms.modify(it,change_valid(value));
-	}
-	return -1;
-}*/
 
 void sim_mob::AgentsList::setValid(Agent * agent , bool value)
 {
@@ -220,60 +105,23 @@ void sim_mob::AgentsList::setValid(Agent * agent , bool value)
 	if (it!=data.end()) {
 		it->second.valid = value;
 	}
-
-
-	/*Agents &agents = data.get<agent_tag>();
-	Agents::iterator it;
-	if((it = agents.find(agent)) != agents.end()) {
-		agents.modify(it,change_valid(value));
-	}
-	return -1;*/
 }
 
 /*******************************************************************
  * ********* Done ************************************************
  * ******************************************************************
 */
-bool sim_mob::AgentsList::isDone(Agent * agent)
+bool sim_mob::AgentsList::isDone(Agent * agent) const
 {
 	Lock lock(mutex);
 
-	type::iterator it = data.find(agent);
+	type::const_iterator it = data.find(agent);
 	if (it!=data.end()) {
 		return it->second.done;
 	}
 
-	/*Agents &agents = data.get<agent_tag>();
-	Agents::iterator it = agents.find(agent);
-	if((it = agents.find(agent)) != agents.end()) {
-		if(it->done) {
-			return true;
-		}
-	}*/
-
 	return false;
 }
-
-/*bool sim_mob::AgentsList::isDone(AgentCommUtilityBase * comm)
-{
-	Lock lock(mutex);
-	AgentCommUtilities &comms = data.get<agent_util>();
-	AgentCommUtilities::iterator it;
-	if((it = comms.find(comm)) != comms.end()) {
-		if(it->done) {
-			return true;
-		}
-	}
-	return false;
-}*/
-
-/*AgentsList::done_range sim_mob::AgentsList::getNotDone()
-{
-	Lock lock(mutex);
-	Dones &dones = data.get<agent_done>();
-	return dones.equal_range(false);;
-}*/
-
 
 bool sim_mob::AgentsList::hasNotDone() const {
 	Lock lock(mutex);
@@ -286,33 +134,9 @@ bool sim_mob::AgentsList::hasNotDone() const {
 }
 
 
-
-sim_mob::AgentsList::change_done::change_done(bool new_value):new_value(new_value)
-{
-}
-
-void sim_mob::AgentsList::change_done::operator()(AgentInfo& e)
-{
-	e.done=new_value;
-}
-
-/*bool sim_mob::AgentsList::setDone(AgentCommUtilityBase * comm , bool value)
-{
-	Lock lock(mutex);
-	bool success = false;
-	AgentCommUtilities &comms = data.get<agent_util>();
-	AgentCommUtilities::iterator it;
-	if((it = comms.find(comm)) != comms.end()) {
-		comms.modify(it,change_done(value));
-		return true;
-	}
-	return false;
-}*/
-
 bool sim_mob::AgentsList::setDone(const Agent* agent , bool value)
 {
 	Lock lock(mutex);
-	//bool success = false;
 
 	type::iterator it = data.find(agent);
 	if (it!=data.end()) {
@@ -321,14 +145,6 @@ bool sim_mob::AgentsList::setDone(const Agent* agent , bool value)
 	}
 
 	return false;
-
-	/*Agents &agents = data.get<agent_tag>();
-	Agents::iterator it = agents.find(const_cast<Agent*>(agent));
-	if(it != agents.end()) {
-		agents.modify(it,change_done(value));
-		return true;
-	}*/
-	//return false;
 }
 
 
--- dev/Basic/shared/entities/commsim/broker/Broker-util.hpp
+++ dev/Basic/shared/entities/commsim/broker/Broker-util.hpp
@@ -8,17 +8,9 @@
 #include <boost/thread/locks.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/recursive_mutex.hpp>
-#include <boost/unordered_map.hpp>
-#include <boost/multi_index_container.hpp>
-#include <boost/multi_index/hashed_index.hpp>
-#include <boost/multi_index/random_access_index.hpp>
-#include <boost/multi_index/ordered_index.hpp>
-#include <boost/multi_index/member.hpp>
-#include <boost/tuple/tuple.hpp>
 #include <boost/function.hpp>
 #include "util/LangHelpers.hpp"
 
-//structure of each data element in the storage
 
 namespace sim_mob
 {
@@ -36,136 +28,53 @@ public:
 };
 
 
-//helper tags used in multi_index_container
-/*struct agent_tag{};
-struct agent_util{};
-struct agent_valid{};
-struct agent_done{};*/
-
 class AgentsList {
 public:
 	//easy reading only
 	typedef typename std::map<const sim_mob::Agent*, AgentInfo> type;//this one is for public use
-	//typedef typename type::iterator iterator;
 
 private:
-	//main definition of the container
-	/*typedef boost::multi_index_container<
-			AgentInfo, boost::multi_index::indexed_by<
-			boost::multi_index::random_access<>//0
-		,boost::multi_index::hashed_unique<boost::multi_index::tag<agent_tag>,boost::multi_index::member<AgentInfo, Agent *, &AgentInfo::agent> >//1
-		,boost::multi_index::hashed_unique<boost::multi_index::tag<agent_util>,boost::multi_index::member<AgentInfo, AgentCommUtilityBase *, &AgentInfo::comm> >//2
-		,boost::multi_index::hashed_non_unique<boost::multi_index::tag<agent_valid>,boost::multi_index::member<AgentInfo, bool, &AgentInfo::valid> >//3
-		,boost::multi_index::hashed_non_unique<boost::multi_index::tag<agent_done>,boost::multi_index::member<AgentInfo, bool, &AgentInfo::done> >//4
-		>
-	> ContainerType;*/
-
 	//the main storage
 	type data;
 
-	//easy reading only
-	/*typedef typename boost::multi_index::nth_index<ContainerType, 1>::type Agents;
-	typedef typename boost::multi_index::nth_index<ContainerType, 2>::type AgentCommUtilities;
-	typedef typename boost::multi_index::nth_index<ContainerType, 3>::type Valids;
-	typedef typename boost::multi_index::nth_index<ContainerType, 4>::type Dones;*/
-
 public:
-	//typedef std::pair<Valids::iterator, Valids::iterator> valid_range;
-	//typedef std::pair<Dones::iterator, Dones::iterator> done_range;
-	//the mutex used for this class
 	typedef typename  boost::recursive_mutex Mutex;
 	typedef typename boost::unique_lock<Mutex> Lock;
 	mutable Mutex mutex;
 
-
-	//	function to insert into the container (only checkes the unqueness of agent)
+	///function to insert into the container (only checkes the unqueness of agent)
 	void insert(Agent * a, AgentCommUtilityBase * b, bool valid = true);
 
-	//returns the size of the container
-	int size( ) ;
+	///returns the size of the container
+	int size() const;
 
-	//returns if the container is empty
-	bool empty();
+	///returns if the container is empty
+	bool empty() const;
 
-	//	erases an elment from the container given the agent's reference
+	///erases an elment from the container given the agent's reference
 	void erase(Agent * agent);
 
-	//	erases an elment from the container given its communication equipment
-	//void erase(AgentCommUtilityBase * comm);
-
-	//erase all elements whose value of 'valid' is set to false
+	///erase all elements whose value of 'valid' is set to false
 	void eraseInvalids();
 
-	//returns the container . not safe if used/iterated without mutex
+	///returns the container . not safe if used/iterated without mutex
 	AgentsList::type &getAgents();
 
-	//returns the mutex used to operate on the container of this class. use it along with getAgents(()
-	//as it is dangerous to work without locking in a multithreaded environment
+	///returns the mutex used to operate on the container of this class. use it along with getAgents(()
+	///as it is dangerous to work without locking in a multithreaded environment
 	AgentsList::Mutex *getMutex();
 
 	//iterates safely through the valid agents executing the requested Function
 	void for_each_agent(boost::function<void(sim_mob::Agent*)> Fn);
 
-	/*******************************************************************
-	 * ********* VALID ************************************************
-	 * ******************************************************************
-	 */
-
-	//validates an element give an agent
-	//if found and if valid, return 1
-	//if found and not valid, return 0
-	//if not found return -1
-	int Validate(Agent * agent);
-
-	//validates an element give an agent's comm equipment
-	//if found and if valid, return 1
-	//if found and not valid, return 0
-	//if not found return -1
-	//int Validate(AgentCommUtilityBase * comm);
-
-	//internal use only(for setting an element to valid/invalid) yep, it is hectic
-	struct change_valid
-	{
-		change_valid(bool new_value);
-
-	  void operator()(AgentInfo& e);
-
-	private:
-	  bool new_value;
-	};
-
-	//	sets valid value of an element in the container, give an agent's comm equipment
-	//bool setValid(AgentCommUtilityBase * comm , bool value);
-
-	//	sets valid value of an element in the container, give an agent
+	///sets valid value of an element in the container, give an agent
 	void setValid(Agent * agent , bool value);
-	/*******************************************************************
-	 * ********* Done ************************************************
-	 * ******************************************************************
-	 */
-//checks if an agent is done
-	bool isDone(Agent * agent);
 
-		//checks if an agent is done
-	//bool isDone(AgentCommUtilityBase * comm);
+	///checks if an agent is done
+	bool isDone(Agent * agent) const;
 
-	//done_range getNotDone();
-
+	///Check if any agent exists which is not done.
 	bool hasNotDone() const;
-
-	//internal use only(for setting an element to valid/invalid) yep, it is hectic
-	struct change_done
-	{
-		change_done(bool new_value);
-
-	  void operator()(AgentInfo& e);
-
-	private:
-	  bool new_value;
-	};
-
-	//	sets Done value of an element in the container, give an agent's comm equipment
-	//bool setDone(AgentCommUtilityBase * comm , bool value) ;
 
 	//	sets Done value of an element in the container, give an agent
 	bool setDone(const Agent* agent , bool value);

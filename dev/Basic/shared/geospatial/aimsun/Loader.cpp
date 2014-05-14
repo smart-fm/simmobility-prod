f31c53d2ed485aec29f6dae26e126fc5c5c2e526
zhang <zhang.huaipeng@gmail.com>
2014-05-14 11:26:05 +0800

    modify the way about building global list of segmentStats with bus stops

--- dev/Basic/shared/geospatial/aimsun/Loader.cpp
+++ dev/Basic/shared/geospatial/aimsun/Loader.cpp
@@ -2626,7 +2626,6 @@ void sim_mob::aimsun::Loader::CreateSegmentStats(const sim_mob::RoadSegment* rdS
 	double lengthCoveredInSeg = 0, segStatLength;
 	double rdSegmentLength = rdSeg->getLaneZeroLength();
 	uint8_t statsNum = 1;
-	std::set<sim_mob::SegmentStats*>& segmentStats = ConfigManager::GetInstanceRW().FullConfig().getSegmentStatsWithBusStops();
 	// NOTE: std::map implements strict weak ordering which defaults to less<key_type>
 	// This is precisely the order in which we want to iterate the stops to create SegmentStats
 	for(std::map<sim_mob::centimeter_t, const sim_mob::RoadItem*>::const_iterator obsIt = obstacles.begin();
@@ -2674,7 +2673,6 @@ void sim_mob::aimsun::Loader::CreateSegmentStats(const sim_mob::RoadSegment* rdS
 			segStats->addBusStop(busStop);
 			splitSegmentStats.push_back(segStats);
 			statsNum++;
-			segmentStats.insert(segStats);
 		}
 	}
 
@@ -2778,6 +2776,16 @@ void sim_mob::aimsun::Loader::CreateSegmentStats(const sim_mob::RoadSegment* rdS
 				statsIt!=splitSegmentStats.end(); statsIt++){
 			(*statsIt)->positionInRoadSegment = statsNum;
 			statsNum++;
+		}
+	}
+
+	std::set<sim_mob::SegmentStats*>& segmentStats =
+			ConfigManager::GetInstanceRW().FullConfig().getSegmentStatsWithBusStops();
+	for (std::list<sim_mob::SegmentStats*>::iterator statsIt =
+			splitSegmentStats.begin(); statsIt != splitSegmentStats.end();
+			statsIt++) {
+		if ((*statsIt)->getBusStops().size() > 0) {
+			segmentStats.insert(*statsIt);
 		}
 	}
 }

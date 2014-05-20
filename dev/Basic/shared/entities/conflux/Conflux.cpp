ab95c12f75f7e07b26293c84ea2b460345129466
zhang <zhang.huaipeng@gmail.com>
2014-05-20 13:56:45 +0800

    change to instantaneous message

--- dev/Basic/shared/entities/conflux/Conflux.cpp
+++ dev/Basic/shared/entities/conflux/Conflux.cpp
@@ -888,7 +888,7 @@ void sim_mob::Conflux::assignPersonToBusStopAgent(Person* person)
 		const StreetDirectory& strDirectory = StreetDirectory::instance();
 		Agent* busStopAgent = strDirectory.findBusStopAgentByBusStop(stop);
 		if (busStopAgent) {
-			messaging::MessageBus::PostMessage(busStopAgent,
+			messaging::MessageBus::SendInstantaneousMessage(busStopAgent,
 					MSG_WAITINGPERSON_ARRIVALAT_BUSSTOP,
 					messaging::MessageBus::MessagePtr(
 							new ArriavalAtStopMessage(person)));

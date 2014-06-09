cf9440ad2fa7819b3fed4a6a347f0bf63f3c4c9b
Harish <harish@smart.mit.edu>
2014-06-09 15:04:32 +0800

    Added CSV reader to utils

--- dev/Basic/shared/util/CSVReader.cpp
+++ dev/Basic/shared/util/CSVReader.cpp
@@ -4,7 +4,6 @@
 
 #include "CSVReader.hpp"
 
-#include <boost/tokenizer.hpp>
 #include <boost/algorithm/string/trim.hpp>
 
 using namespace sim_mob;
@@ -16,8 +15,8 @@ namespace
 	typedef tokenizer< escaped_list_separator<char> > Tokenizer;
 }
 
-sim_mob::CSV_Reader::CSV_Reader(const std::string& fileName, char delim, bool headerLineIncluded)
-: headerLineIncludedInFile(headerLineIncluded)
+sim_mob::CSV_Reader::CSV_Reader(const std::string& fileName, bool headerLineIncluded, char delimChar, char escapeChar, char quoteChar)
+: headerLineIncludedInFile(headerLineIncluded), escapedListSeparator(escapeChar, delimChar, quoteChar)
 {
 	if(fileName.empty())
 	{
@@ -25,29 +24,19 @@ sim_mob::CSV_Reader::CSV_Reader(const std::string& fileName, char delim, bool he
 	}
 
 	//open the file
-	inputFileStream(fileName.c_str());
+	inputFileStream.open(fileName.c_str());
 	if (!inputFileStream.is_open())
 	{
 		throw std::runtime_error("CSV file cannot be opened");
 	}
 
-	//check delimiter
-	if(!isalnum(delim))
-	{
-		delimiter = delim;
-	}
-	else
-	{
-		throw std::runtime_error("Alphanumeric delimiters are not supported");
-	}
-
 	//load header line if it is included in file
-	if(headerLineIncluded)
+	if(headerLineIncludedInFile)
 	{
 		string headerLine = string();
 		if(getline(inputFileStream,headerLine))
 		{
-			Tokenizer tknzr(headerLine);
+			Tokenizer tknzr(headerLine, escapedListSeparator);
 			for(Tokenizer::iterator tknIt=tknzr.begin(); tknIt!=tknzr.end(); tknIt++)
 			{
 				string header = *tknIt;
@@ -60,13 +49,63 @@ sim_mob::CSV_Reader::CSV_Reader(const std::string& fileName, char delim, bool he
 
 sim_mob::CSV_Reader::~CSV_Reader()
 {
+	inputFileStream.close();
 }
 
-void sim_mob::CSV_Reader::getNextRow(boost::unordered_map<std::string, std::string> rowOut)
+void sim_mob::CSV_Reader::getNextRow(boost::unordered_map<std::string, std::string>& rowOut, bool skipInvalidRows)
 {
-
+	if(!headerLineIncludedInFile)
+	{
+		throw std::runtime_error("Cannot build map out of row. Header line was not included in csv.");
+	}
+	string row = string();
+	bool invalidRow = true;
+	while (invalidRow && getline(inputFileStream,row))
+	{
+		Tokenizer tknzr(row, escapedListSeparator);
+		size_t itNo = 0;
+		invalidRow = false;
+		for(Tokenizer::iterator tknIt=tknzr.begin(); tknIt!=tknzr.end(); tknIt++)
+		{
+			string item = *tknIt;
+			trim(item);
+			if(itNo < headerList.size())
+			{
+				rowOut[headerList[itNo]] = item;
+			}
+			itNo++;
+		}
+		if(itNo != headerList.size()) { invalidRow = true; }
+		if(!invalidRow)
+		{
+			// One row was read successfully.
+			break;
+		}
+		else
+		{
+			//row was not having the correct number of items
+			rowOut.clear();
+			if(!skipInvalidRows)
+			{
+				std::stringstream errStream;
+				errStream << "getNextRow(): row has " << itNo << " fields. " << headerList.size() << " fields were expected.\n" << row << "\n";
+				throw std::runtime_error(errStream.str());
+			}
+		}
+	}
 }
 
-
-
-
+void sim_mob::CSV_Reader::getNextRow(std::vector<std::string>& rowOut)
+{
+	string row = string();
+	if (getline(inputFileStream,row))
+	{
+		Tokenizer tknzr(row, escapedListSeparator);
+		for(Tokenizer::iterator tknIt=tknzr.begin(); tknIt!=tknzr.end(); tknIt++)
+		{
+			string item = *tknIt;
+			trim(item);
+			rowOut.push_back(item);
+		}
+	}
+}

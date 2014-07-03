477f8c77cd5aa077a81b21ba474341b4127573df
vahid <vahid@smart.mit.edu>
2014-07-03 13:50:23 +0800

    add universal id assignment feature to message module

--- dev/Basic/shared/message/MessageHandler.cpp
+++ dev/Basic/shared/message/MessageHandler.cpp
@@ -13,8 +13,14 @@
 #include "MessageHandler.hpp"
 
 using namespace sim_mob::messaging;
-
+std::set<unsigned int> MessageHandler::used = std::set<unsigned int>();
+unsigned int MessageHandler::freeCounter = 1;
 MessageHandler::MessageHandler(unsigned int id) : id(id), context(NULL) {
+	add(id);
+}
+
+MessageHandler::MessageHandler() : id(getNewId()), context(NULL) {
+	add(id);
 }
 
 MessageHandler::~MessageHandler() {
@@ -23,4 +29,21 @@ MessageHandler::~MessageHandler() {
 
 unsigned int MessageHandler::GetId() const {
     return id;
+}
+
+
+void MessageHandler::add(unsigned int i)
+{
+    used.insert(i);
+}
+
+unsigned int MessageHandler::getNewId()
+{
+	std::set<unsigned int>::iterator iter = used.lower_bound(freeCounter);
+    while (iter != used.end() && *iter == freeCounter)
+    {
+        ++iter;
+        ++freeCounter;
+    }
+    return freeCounter++;
 }

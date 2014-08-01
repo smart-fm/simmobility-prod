d3d56edadd979390e827cb2f015b3f8919a6d517
vahid <vahid@smart.mit.edu>
2014-08-01 13:59:31 +0800

    multithreaded profiler

--- dev/Basic/shared/util/Profiler.cpp
+++ dev/Basic/shared/util/Profiler.cpp
@@ -1,16 +1,20 @@
 #include "Profiler.hpp"
 #include "logging/Log.hpp"
-std::map<const std::string, boost::shared_ptr<sim_mob::Profiler> > sim_mob::Profiler::repo = std::map<const std::string, boost::shared_ptr<sim_mob::Profiler> >();
-std::string sim_mob::Profiler::newLine("\n");
-sim_mob::Profiler sim_mob::Profiler::instance;
-sim_mob::Profiler::Profiler(){}
-sim_mob::Profiler::Profiler(std::string id_,bool init){
+
+#include <boost/lockfree/queue.hpp>
+
+boost::lockfree::queue<void* > queue(128);
+
+std::map<const std::string, boost::shared_ptr<sim_mob::BaseProfiler> > sim_mob::BaseProfiler::repo = std::map<const std::string, boost::shared_ptr<sim_mob::BaseProfiler> >();
+std::string sim_mob::BaseProfiler::newLine("\n");
+sim_mob::BaseProfiler sim_mob::BaseProfiler::instance;
+sim_mob::BaseProfiler::BaseProfiler(){}
+sim_mob::BaseProfiler::BaseProfiler(std::string id_,bool init){
 
 	reset();
 	start = stop = totalTime = 0;
 	started = false;
-	output.clear();
-	outputSize = 0;
+//	output.clear();
 	id = id_;
 	std::string path = id_ + ".txt";
 	if(path.size()){
@@ -21,39 +25,40 @@ sim_mob::Profiler::Profiler(std::string id_,bool init){
 	}
 }
 
-sim_mob::Profiler & sim_mob::Profiler::operator[](const std::string &key)
+sim_mob::BaseProfiler & sim_mob::BaseProfiler::operator[](const std::string &key)
 {
 
-	std::map<std::string, boost::shared_ptr<sim_mob::Profiler> >::iterator it = repo.find(key);
+	std::map<std::string, boost::shared_ptr<sim_mob::BaseProfiler> >::iterator it = repo.find(key);
 	if(it == repo.end()){
-		boost::shared_ptr<sim_mob::Profiler> t(new sim_mob::Profiler(key,false));
+		boost::shared_ptr<sim_mob::BaseProfiler> t(new sim_mob::BaseProfiler(key,false));
 		repo.insert(std::make_pair(key,t));
 	}
 	return *repo[key];
 }
 
-sim_mob::Profiler::~Profiler(){
+sim_mob::BaseProfiler::~BaseProfiler(){
 	if(LogFile.is_open()){
 		flushLog();
 		LogFile.close();
 	}
+	for(outIt it(out_.begin()); it != out_.end(); safe_delete_item(it->second),it++);
 }
 
 ///whoami
-std::string sim_mob::Profiler::getId(){
+std::string sim_mob::BaseProfiler::getId(){
 	return id;
 }
 ///whoami
-int sim_mob::Profiler::getIndex(){
+int sim_mob::BaseProfiler::getIndex(){
 	return index;
 }
 
-bool sim_mob::Profiler::isStarted(){
+bool sim_mob::BaseProfiler::isStarted(){
 	return started;
 }
 
 ///like it suggests, store the start time of the profiling
-void sim_mob::Profiler::startProfiling(){
+void sim_mob::BaseProfiler::startProfiling(){
 	started = true;
 
 	struct timeval  tv;
@@ -68,9 +73,9 @@ void sim_mob::Profiler::startProfiling(){
 }
 
 ///save the ending time ...and .. if add==true add the value to the total time;
-uint32_t sim_mob::Profiler::endProfiling(bool addToTotalTime_){
+uint32_t sim_mob::BaseProfiler::endProfiling(bool addToTotalTime_){
 	if(!started){
-		throw std::runtime_error("Profiler Ended before Starting");
+		throw std::runtime_error("BaseProfiler Ended before Starting");
 	}
 
 	struct timeval  tv;
@@ -91,31 +96,41 @@ uint32_t sim_mob::Profiler::endProfiling(bool addToTotalTime_){
 }
 
 ///add the given time to the total time
-void sim_mob::Profiler::addToTotalTime(uint32_t value){
+void sim_mob::BaseProfiler::addToTotalTime(uint32_t value){
 	boost::unique_lock<boost::mutex> lock(mutexTotalTime);
-//		Print() << "Profiler "  << "[" << index << ":" << id << "] Adding " << value << " seconds to total time " << std::endl;
+//		Print() << "BaseProfiler "  << "[" << index << ":" << id << "] Adding " << value << " seconds to total time " << std::endl;
 	totalTime+=value;
 }
-std::stringstream & sim_mob::Profiler::outPut(){
-	boost::unique_lock<boost::mutex> lock(mutexOutput);
-	return output;
-}
 
-void sim_mob::Profiler::flushLog(){
+void sim_mob::BaseProfiler::flushLog(){
 		if ((LogFile.is_open() && LogFile.good())) {
-			//boost::unique_lock<boost::mutex> lock(mutexOutput);
-			LogFile << output.str();
-			LogFile.flush();
-			output.str("");
-			output.clear();
-			outputSize = 0;
+			std::stringstream &out = getOut();
+			{
+				boost::unique_lock<boost::mutex> lock(flushMutex);
+				LogFile << "[" << boost::this_thread::get_id() << "]\n" << out.str();
+				LogFile.flush();
+				out.str(std::string());
+			}
 		}
 		else{
 			Warn() << "pathset profiler log ignored" << std::endl;
 		}
-
 }
-unsigned int & sim_mob::Profiler::getTotalTime(){
+
+std::stringstream & sim_mob::BaseProfiler::getOut(){
+	boost::upgrade_lock<boost::shared_mutex> lock(mutexOutput);
+	outIt it;
+	boost::thread::id id = boost::this_thread::get_id();
+	if((it = out_.find(id)) == out_.end()){
+		boost::upgrade_to_unique_lock<boost::shared_mutex> lock2(lock);
+		std::stringstream* strm(new std::stringstream());
+		out_.insert(std::make_pair(id, strm));
+		return *strm;
+	}
+	return *(it->second);
+}
+
+unsigned int & sim_mob::BaseProfiler::getTotalTime(){
 	boost::unique_lock<boost::mutex> lock(mutexTotalTime);
 	return totalTime;
 }
@@ -124,29 +139,19 @@ void printTime(struct tm *tm, struct timeval & tv, std::string id){
 	sim_mob::Print() << "TIMESTAMP:\t  " << tm->tm_hour << std::setw(2) << ":" <<tm->tm_min << ":" << ":" <<  tm->tm_sec << ":" << tv.tv_usec << std::endl;
 }
 
-void sim_mob::Profiler::reset(){
+void sim_mob::BaseProfiler::reset(){
 	start = stop = totalTime = 0;
 	started = false;
-	output.clear();
 	id = "";
 }
 
-void  sim_mob::Profiler::InitLogFile(const std::string& path)
+void  sim_mob::BaseProfiler::InitLogFile(const std::string& path)
 {
-	//1. first type of implementation
 	LogFile.open(path.c_str());
-//	if (LogFile.fail()) {
-//		log_handle =  &std::cout;
-//	}
-//	log_handle = (!LogFile.fail() ? &LogFile : &std::cout);
 }
 
-////Type of cout.
-//typedef std::basic_ostream<char, std::char_traits<char> > CoutType;
-////Type of std::endl and some other manipulators.
-//typedef CoutType& (*StandardEndLine)(CoutType&);
-
-sim_mob::Profiler&  sim_mob::Profiler::operator<<(StandardEndLine manip) {
-		manip(output);
+sim_mob::BaseProfiler&  sim_mob::BaseProfiler::operator<<(StandardEndLine manip) {
+	// call the function, but we cannot return it's value
+		manip(getOut());
 	return *this;
 }
--- dev/Basic/shared/util/Profiler.hpp
+++ dev/Basic/shared/util/Profiler.hpp
@@ -14,16 +14,16 @@ namespace sim_mob {
  * same object can be used multiple times (without resetting) to accumulate profiling time of several operations.
  * multiple instances of profiler objects can form a hierarchy and report to the higher level and thereby generate accumulated time from diesired parts of various methods
  * the amount of buffering to the output file can be configured.
- * Although it is mainly a time profiler, it can be used to profile other parameters with some other tool and dump the result as string stream to the object of this Profiler
+ * Although it is mainly a time profiler, it can be used to profile other parameters with some other tool and dump the result as string stream to the object of this BaseProfiler
  *
  * Note: example use case :
  * 	std::string prof("PathSetManagerProfiler");
-	sim_mob::Profiler::instance[prof] << "My First" << " test" << std::endl;;
-	sim_mob::Profiler::instance[prof] << sim_mob::Profiler::instance["hey"].outPut();
+	sim_mob::BaseProfiler::instance[prof] << "My First" << " test" << std::endl;;
+	sim_mob::BaseProfiler::instance[prof] << sim_mob::BaseProfiler::instance["hey"].outPut();
 	std::stringstream  s;
-	sim_mob::Profiler::instance[prof] << s.str();
+	sim_mob::BaseProfiler::instance[prof] << s.str();
  */
-class Profiler {
+class BaseProfiler {
 
 	///total time measured by all profilers
 	uint32_t totalTime;
@@ -31,13 +31,13 @@ class Profiler {
 	static int totalProfilers;
 
 	///	used for index
-	boost::mutex mutex_;
+	boost::mutex flushMutex;
 
 	///	used for total time
 	boost::mutex mutexTotalTime;
 
 	///	used for output stream
-	boost::mutex mutexOutput;
+	boost::shared_mutex mutexOutput;
 
 	///stores start and end of profiling
 	uint32_t start, stop;
@@ -51,26 +51,27 @@ class Profiler {
 	bool started;
 
 	///print output
-	std::stringstream output;
-
-	///for efficiency
-	int outputSize;
+//	std::stringstream output;
+  std::map<boost::thread::id, std::stringstream* > out_;
+  typedef std::map<boost::thread::id, std::stringstream* >::iterator outIt;
 
 	///logger
 	std::ofstream LogFile;
 
 	///flush the log streams into the file
 
-	static std::map<const std::string, boost::shared_ptr<sim_mob::Profiler> > repo;
+	static std::map<const std::string, boost::shared_ptr<sim_mob::BaseProfiler> > repo;
 	void flushLog();
+	///	return the buffer corresponding to the calling thread.If the buffer doesn't exist, this method will create, register and returns a new buffer.
+	std::stringstream & getOut();
 
 	/**
 	 * @param init Start profiling if this is set to true
 	 * @param id arbitrary identification for this object
 	 * @param logger file name where the output stream is written
 	 */
-	Profiler(std::string id_,bool init = false);
-	Profiler();
+	BaseProfiler(std::string id_,bool init = false);
+	BaseProfiler();
 	//needs improvement
 	static void printTime(struct tm *tm, struct timeval & tv, std::string id);
 
@@ -85,23 +86,21 @@ class Profiler {
 	///whoami
 	int getIndex();
 
-	///is this Profiler started
+	///is this BaseProfiler started
 	bool isStarted();
-	///getoutput stream
-	std::stringstream & outPut();
 
-	//Type of cout.
+	//This is the type of std::cout
 	typedef std::basic_ostream<char, std::char_traits<char> > CoutType;
-	//Type of std::endl and some other manipulators.
+	//This is the function signature of std::endl and some other manipulators
 	typedef CoutType& (*StandardEndLine)(CoutType&);
 
 public:
 	static std::string newLine;
-	static sim_mob::Profiler instance;
-	//copy constructor is required by static std::map<std::string, sim_mob::Profiler> repo;
-	Profiler(const sim_mob::Profiler& value);
-	sim_mob::Profiler & operator[](const std::string &key);
-	~Profiler();
+	static sim_mob::BaseProfiler instance;
+	//copy constructor is required by static std::map<std::string, sim_mob::BaseProfiler> repo;
+	BaseProfiler(const sim_mob::BaseProfiler& value);
+	sim_mob::BaseProfiler & operator[](const std::string &key);
+	~BaseProfiler();
 
 	///like it suggests, store the start time of the profiling
 	void startProfiling();
@@ -120,19 +119,19 @@ public:
 	void addToTotalTime(uint32_t value);
 
 	unsigned int & getTotalTime();
-
-	Profiler& operator<<(StandardEndLine manip);
+	/// This method defines an operator<< to take in std::endl
+	BaseProfiler& operator<<(StandardEndLine manip);
 
 	template <typename T>
-	sim_mob::Profiler & operator<< (const T& val)
+	sim_mob::BaseProfiler & operator<< (const T& val)
 	{
-		boost::unique_lock<boost::mutex> lock(mutexOutput);
-		output << val;
-		if(output.tellp() > 512000/*500KB*/){
+		std::stringstream &out = sim_mob::BaseProfiler::getOut();
+		out << val;
+		if(out.tellp() > 512000/*500KB*/){
 			flushLog();
 		}
 		return *this;
 	}
-
 };
+typedef BaseProfiler Profiler;
 }//namespace

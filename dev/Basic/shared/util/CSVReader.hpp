47490a4be6aaa02708628d325a6326f5f7a71fee
Harish <harish@smart.mit.edu>
2014-09-30 11:29:54 +0800

    refactoring

--- dev/Basic/shared/util/CSVReader.hpp
+++ dev/Basic/shared/util/CSVReader.hpp
@@ -65,7 +65,7 @@ public:
 	 * A row is considered valid only if the number of fields in the row is equal to the number of fields in the header line.
 	 * If the current row is invalid, this function skips the row and reads the next row if skipInvalidRows is set to true; an error is thrown otherwise.
 	 * After this function is executed, the caller must check if unordered_map is empty.
-	 * The map will not be populated if there are no more valid rows to be read in the file.
+	 * The map will not be populated if there are no valid rows to be read in the file.
 	 * @param rowOut unordered_map to be populated with the row's values. When this function is executed, for each <k,v> element in the map - v is the value
 	 * 			of a field in the row that was read from the csv and k is the value of the field in the header line corresponding to that field.
 	 * @param skipInvalidRows flag to indicate whether the algorithm can skip invalid rows.

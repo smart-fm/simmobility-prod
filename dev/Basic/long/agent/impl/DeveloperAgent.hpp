b407e3373e7ecb05a2355ff81d58f51153215c04
gishara <gishara@smart.mit.edu>
2015-01-05 14:35:16 +0800

    add code for potential units creation, profit calcualtion, fm_project, building and units creation

--- dev/Basic/long/agent/impl/DeveloperAgent.hpp
+++ dev/Basic/long/agent/impl/DeveloperAgent.hpp
@@ -5,6 +5,7 @@
 /* 
  * File:   DeveloperAgent.hpp
  * Author: Pedro Gandola <pedrogandola@smart.mit.edu>
+ *       : Gishara Premarathne <gishara@smart.mit.edu>
  *
  * Created on Mar 5, 2014, 6:36 PM
  */
@@ -13,6 +14,9 @@
 #include "database/entity/Developer.hpp"
 #include "database/entity/Parcel.hpp"
 #include "database/entity/PotentialProject.hpp"
+#include "database/entity/Building.hpp"
+#include "database/entity/Unit.hpp"
+#include "database/entity/Project.hpp"
 
 namespace sim_mob {
 
@@ -31,6 +35,22 @@ namespace sim_mob {
              * @param parcelId parcel to process.
              */
             void assignParcel(BigSerial parcelId);
+
+            /**
+             * Tells if the agent is active or not.
+             * @return true if the agent is active, false otherwise.
+             */
+            bool isActive() const {
+            	return active;
+            }
+
+            /**
+             * Disable or enable the agent.
+             * @param active
+             */
+            void setActive(bool active) {
+            	this->active = active;
+            }
        
         protected:
             /**
@@ -47,7 +67,10 @@ namespace sim_mob {
             void onWorkerExit();
             virtual void HandleMessage(messaging::Message::MessageType type, const messaging::Message& message);
         private:
-
+            enum UnitSaleStatus {
+            	NOT_LAUNCHED, LAUNCHED_BUT_UNSOLD, LAUNCHED_AND_SOLD};
+            enum UnitPhysicalStatus {
+                        	NOT_READY_FOR_OCCUPANCY, READY_FOR_OCCUPANCY_AND_VACANT, READY_FOR_OCCUPANCY_AND_OCCUPIED};
             /**
              * Events callbacks.
              */
@@ -62,9 +85,32 @@ namespace sim_mob {
             void processEvent(event::EventId eventId, event::Context ctxId,
                                           const event::EventArgs& args);
         
+            /*
+             * create new units and buildings, do the initial status encoding of parcel,unit and buildings
+             * @param project : most profitable project selected for this parcel
+             * @param projectId
+             */
+            void createUnitsAndBuildings(PotentialProject &project, BigSerial projectId);
+            /*
+             * create fm_project from potential most profitable project
+             * param project : most profitable project selected for this parcel
+             */
+            void createProject(PotentialProject &project, BigSerial projectId);
+
+            /*
+             * update the status and date params of buildings and units
+             */
+            void processExistingProjects();
+
         private:
             DeveloperModel* model;
+            Parcel *parcel;
             IdVector parcelsToProcess;
+            bool active;
+            std::vector<Building> newBuildings;
+            std::vector<Unit> newUnits;
+            Project *fmProject;
+
         };
     }
 }

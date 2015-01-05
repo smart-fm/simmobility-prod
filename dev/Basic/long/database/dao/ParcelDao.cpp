cc3e188a5cffcb26261a8971b6e04fe018857ede
gishara <gishara@smart.mit.edu>
2015-01-05 14:50:32 +0800

    changed according to changed db fields, add code to call getEmptyParcels stored procedure

--- dev/Basic/long/database/dao/ParcelDao.cpp
+++ dev/Basic/long/database/dao/ParcelDao.cpp
@@ -23,10 +23,9 @@ ParcelDao::~ParcelDao() {}
 void ParcelDao::fromRow(Row& result, Parcel& outObj)
 {
     outObj.id = result.get<BigSerial>( "fm_parcel_id", INVALID_ID );
-    outObj.taz_id = result.get<BigSerial>("taz_id", INVALID_ID);
     outObj.lot_size = result.get<double>( "lot_size", .0 );
-    outObj.gpr = result.get<double>("gpr", .0 );
-    outObj.land_use = result.get<std::string>( "land_use", EMPTY_STR );
+    outObj.gpr = result.get<std::string>("gpr", EMPTY_STR );
+    outObj.land_use_type_id = result.get<int>( "land_use_type_id", 0 );
     outObj.owner_name = result.get<std::string>( "owner_name", EMPTY_STR );
     outObj.owner_category = result.get<int>( "owner_category", 0 );
     outObj.last_transaction_date = result.get<std::tm>( "last_transaction_date", std::tm() );
@@ -39,12 +38,22 @@ void ParcelDao::fromRow(Row& result, Parcel& outObj)
     outObj.award_date = result.get<std::tm>( "award_date", std::tm() );
     outObj.award_status = result.get<int>( "award_status", false );
     outObj.use_restriction = result.get<std::string>( "use_restriction", EMPTY_STR );
-    outObj.development_type_allowed = result.get<std::string>("development_type_allowed", EMPTY_STR );
-    outObj.development_type_code = result.get<std::string>( "development_type_code", EMPTY_STR );
+    outObj.development_type_code = result.get<int>( "development_type_code", 0 );
     outObj.successful_tender_id = result.get<int>( "successful_tender_id", 0 );
     outObj.successful_tender_price = result.get<double>( "successful_tender_price", INVALID_ID);
     outObj.tender_closing_date = result.get<std::tm>( "tender_closing_date", std::tm() );
     outObj.lease = result.get<int>( "lease", 0 );
+    outObj.status = result.get<int>("status",0);
+    outObj.developmentAllowed = result.get<std::string>("development_allowed",EMPTY_STR);
+    outObj.nextAvailableDate = result.get<std::tm>("next_available_date", std::tm());
 }
 
 void ParcelDao::toRow(Parcel& data, Parameters& outParams, bool update) {}
+
+std::vector<Parcel*>  ParcelDao::getEmptyParcels()
+{
+	const std::string queryStr = DB_GETALL_EMPTY_PARCELS;
+	std::vector<Parcel*> emptyParcelList;
+	getByQuery(queryStr,emptyParcelList);
+	return emptyParcelList;
+}

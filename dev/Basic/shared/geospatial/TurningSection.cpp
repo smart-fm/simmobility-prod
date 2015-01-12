aa108d0cb5ad0cfa5c788047d515441be1e32eec
Max <zhiyong@smart.mit.edu>
2015-01-12 10:52:01 +0800

    add class TurningSection

---
+++ dev/Basic/shared/geospatial/TurningSection.cpp
@@ -0,0 +1,16 @@
+//Copyright (c) 2013 Singapore-MIT Alliance for Research and Technology
+//Licensed under the terms of the MIT License, as described in the file:
+//   license.txt   (http://opensource.org/licenses/MIT)
+
+using namespace sim_mob;
+
+#include "TurningSection.hpp"
+
+TurningSection::TurningSection():
+	laneFrom(nullptr),laneTo(nullptr){
+
+}
+
+
+
+
---
+++ dev/Basic/shared/geospatial/TurningSection.hpp
@@ -0,0 +1,28 @@
+//Copyright (c) 2013 Singapore-MIT Alliance for Research and Technology
+//Licensed under the terms of the MIT License, as described in the file:
+//   license.txt   (http://opensource.org/licenses/MIT)
+
+#pragma once
+
+#include "geospatial/Lane.hpp"
+
+namespace sim_mob
+{
+
+class TurningSection {
+public:
+	TurningSection();
+
+public:
+	std::string sectiongId;
+	// phases
+	std::vector<TurningSection*> confilicts;
+
+	/// polyline points
+	std::vector<Point2D> polylinePoints;
+
+	sim_mob::Lane* laneFrom;
+		sim_mob::Lane* laneTo;
+};
+
+};// end namespace

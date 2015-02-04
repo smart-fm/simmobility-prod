75d3980f9e55f97086213bac0cd6838105dd7910
Max <zhiyong@smart.mit.edu>
2015-02-04 13:36:20 +0800

    filter out conflict point same with start point of turning

--- dev/tools/generate_conflicts/gen_conflicts2.py
+++ dev/tools/generate_conflicts/gen_conflicts2.py
@@ -2,11 +2,57 @@ import psycopg2
 import psycopg2.extras
 import sys
 import pprint
+import math
 
-conflicts = [ {'first_turning':3,'second_turning':2} ]
+conflicts = [  ]
 
-# check point in the bound of t1,t2
+# calculate turning from point distance to conflict point
+def calculate_conflict_distance(turning,x,y) :
+    # 1.0 calculate turning polyline distance of from point to cross point
+    t_dx = turning['from_xpos'] - x;
+    t_dy = turning['from_ypos'] - y;
+    t_distance = math.sqrt(t_dx*t_dx + t_dy*t_dy)
+    return t_distance
+  
+# check point in the bound of t12
+# x,y cross point position
+# return False when not valid
 def validate_cross_point(t1,t2,x,y):
+    # 1.0 calculate line t1 distance of from/to points
+    t1_dx = t1['from_xpos'] - t1['to_xpos'];
+    t1_dy = t1['from_ypos'] - t1['to_ypos'];
+    t1_distance = math.sqrt(t1_dx*t1_dx + t1_dy*t1_dy)
+    
+    # t2 distance of from/to points
+    t2_dx = t2['from_xpos'] - t2['to_xpos'];
+    t2_dy = t2['from_ypos'] - t2['to_ypos'];
+    t2_distance = math.sqrt(t2_dx*t2_dx + t2_dy*t2_dy)
+    
+    # distance t1 from point to cross point
+    t1_cross_dx = t1['from_xpos'] - x;
+    t1_cross_dy = t1['from_ypos'] - y;
+    t1_cross_distance = math.sqrt(t1_cross_dx*t1_cross_dx + t1_cross_dy*t1_cross_dy)
+    
+    offset = 1.0
+    # cross point same with from point of turning,no valid
+    if t1_cross_distance < offset :
+	return False
+    
+
+    if t1_cross_distance > t1_distance + offset :
+	# cross point outside of the t1
+	return False
+      
+    # distance t2 from point to cross point
+    t2_cross_dx = t2['from_xpos'] - x;
+    t2_cross_dy = t2['from_ypos'] - y;
+    t2_cross_distance = math.sqrt(t2_cross_dx*t2_cross_dx + t2_cross_dy*t2_cross_dy)
+    
+    if t2_cross_distance > t1_distance + offset:
+	# cross point outside of the t1
+	return False
+    
+    return True
   
 def already_has_conflict(turning1,turning2) :
     # Generator Expressions
@@ -58,7 +104,19 @@ def calculate_conflict_point(turning1,turning2) :
     print ('x',x)
     print ('y',y)
     
-    validate_cross_point(turning1,turning2,x,y)
+    valid = validate_cross_point(turning1,turning2,x,y)
+    if valid == True :
+      # the cross point is valid,so create conflict
+      first_cd = calculate_conflict_distance(turning1,x,y)
+      second_cd = calculate_conflict_distance(turning2,x,y)
+      cnf = {}
+      cnf['first_turning'] = turning1['id']
+      cnf['second_turning'] = turning2['id']
+      cnf['first_cd'] = first_cd
+      cnf['second_cd'] = second_cd
+      
+      # append to all 
+      conflicts.append(cnf)
 
 
     
@@ -109,6 +167,9 @@ def main():
 	    
 	for turning_section in records :
 	    generate_conflict(turning_section,records)
+	
+	print ('conflict:')
+	pprint.pprint(conflicts)
 	    
 if __name__ == "__main__":
 	main()

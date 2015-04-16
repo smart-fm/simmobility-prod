3ab3d3ec7f3257c82c52131210587a83b9163482
Harish <harish@smart.mit.edu>
2015-04-16 11:25:49 +0800

    updated md luas to 1169 zones

--- dev/Basic/scripts/lua/mid/behavior-models/stmd.lua
+++ dev/Basic/scripts/lua/mid/behavior-models/stmd.lua
@@ -83,7 +83,7 @@ local beta_mode_work_walk= 0
 
 --choice set
 local choice = {}
-for i = 1, 1092*9 do 
+for i = 1, 1169*9 do 
 	choice[i] = i
 end
 
@@ -137,7 +137,7 @@ local function computeUtilities(params,dbparams)
 	local d2 = {}
 
 	--for each area
-	for i =1,1092 do
+	for i =1,1169 do
 		cost_bus[i] = dbparams:cost_public_first(i) + dbparams:cost_public_second(i)
 		cost_mrt[i] = cost_bus[i]
 		cost_private_bus[i] = cost_bus[i]
@@ -180,56 +180,56 @@ local function computeUtilities(params,dbparams)
 
 	local V_counter = 0
 
-	--utility function for bus 1-1092
-	for i =1,1092 do
+	--utility function for bus 1-1169
+	for i =1,1169 do
 		V_counter = V_counter + 1
 		utility[V_counter] = beta_cons_bus + cost_bus[i] * beta_cost_bus_mrt_1 + tt_bus[i] * beta_tt_bus_mrt + beta_central_bus_mrt * central_dummy[i] + beta_log * log(shop[i]+exp(beta_employment)*employment[i]) + (d1[i]+d2[i]) * beta_distance_bus_mrt + beta_female_bus * female_dummy + beta_mode_work_bus * mode_work_bus
 	end
 
-	--utility function for mrt 1-1092
-	for i=1,1092 do
+	--utility function for mrt 1-1169
+	for i=1,1169 do
 		V_counter = V_counter +1
 		utility[V_counter] = beta_cons_mrt + cost_mrt[i] * beta_cost_bus_mrt_1 + tt_mrt[i] * beta_tt_bus_mrt + beta_central_bus_mrt * central_dummy[i] + beta_log * log(shop[i]+exp(beta_employment)*employment[i]) + (d1[i]+d2[i]) * beta_distance_bus_mrt + beta_female_mrt * female_dummy + beta_mode_work_bus * mode_work_mrt
 	end
 
-	--utility function for private bus 1-1092
-	for i=1,1092 do
+	--utility function for private bus 1-1169
+	for i=1,1169 do
 		V_counter = V_counter +1
 		utility[V_counter] = beta_cons_private_bus + cost_private_bus[i] * beta_cost_private_bus_1 + tt_private_bus[i] * beta_tt_bus_mrt + beta_central_private_bus * central_dummy[i] + beta_log * log(shop[i]+exp(beta_employment)*employment[i]) + (d1[i]+d2[i]) * beta_distance_private_bus + beta_female_private_bus * female_dummy + beta_mode_work_bus * mode_work_private_bus
 	end
 
-	--utility function for drive1 1-1092
-	for i=1,1092 do
+	--utility function for drive1 1-1169
+	for i=1,1169 do
 		V_counter = V_counter +1
 		utility[V_counter] = beta_cons_drive1 + cost_drive1[i] * beta_cost_drive1_1 + tt_drive1[i] * beta_tt_drive1 + beta_central_drive1 * central_dummy[i] + beta_log * log(shop[i]+exp(beta_employment)*employment[i]) + (d1[i]+d2[i]) * beta_distance_drive1 + beta_female_drive1 * female_dummy + beta_mode_work_drive1 * mode_work_drive1 
 	end
 
-	--utility function for share2 1-1092
-	for i=1,1092 do
+	--utility function for share2 1-1169
+	for i=1,1169 do
 		V_counter = V_counter +1
 		utility[V_counter] = beta_cons_share2 + cost_share2[i] *beta_cost_drive1_1 + tt_share2[i] * beta_tt_share2 + beta_central_share2 * central_dummy[i] + beta_log * log(shop[i]+exp(beta_employment)*employment[i]) + (d1[i]+d2[i]) * beta_distance_share2 + beta_female_share2 * female_dummy + beta_mode_work_share2 * mode_work_share2
 	end
 
-	--utility function for share3 1-1092
-	for i=1,1092 do
+	--utility function for share3 1-1169
+	for i=1,1169 do
 		V_counter = V_counter +1
 		utility[V_counter] = beta_cons_share3 + cost_share3[i] * beta_cost_drive1_1 + tt_share3[i] * beta_tt_share3 + beta_central_share3 * central_dummy[i] + beta_log * log(shop[i]+exp(beta_employment)*employment[i]) + (d1[i]+d2[i]) * beta_distance_share3 + beta_female_share3 * female_dummy + beta_mode_work_share2 * mode_work_share3
 	end
 
-	--utility function for motor 1-1092
-	for i=1,1092 do
+	--utility function for motor 1-1169
+	for i=1,1169 do
 		V_counter = V_counter +1
 		utility[V_counter] = beta_cons_motor + cost_motor[i] * beta_cost_drive1_1 + tt_motor[i] * beta_tt_motor + beta_central_motor * central_dummy[i] + beta_log * log(shop[i]+exp(beta_employment)*employment[i]) + (d1[i]+d2[i]) * beta_distance_motor + beta_female_motor * female_dummy + beta_mode_work_drive1 * mode_work_motor
 	end
 
-	--utility function for walk 1-1092
-	for i=1,1092 do
+	--utility function for walk 1-1169
+	for i=1,1169 do
 		V_counter = V_counter +1
 		utility[V_counter] = beta_cons_walk + tt_walk[i] * beta_tt_walk + beta_central_walk * central_dummy[i] + beta_log * log(shop[i]+exp(beta_employment)*employment[i]) + (d1[i]+d2[i]) * beta_distance_walk + beta_female_walk * female_dummy + beta_mode_work_walk * mode_work_walk
 	end
 
-	--utility function for taxi 1-1092
-	for i=1,1092 do
+	--utility function for taxi 1-1169
+	for i=1,1169 do
 		V_counter = V_counter +1
 		utility[V_counter] = beta_cons_taxi + cost_taxi[i] * beta_cost_drive1_1 + tt_taxi[i] * beta_tt_taxi + beta_central_taxi * central_dummy[i] + beta_log * log(shop[i]+exp(beta_employment)*employment[i]) + (d1[i]+d2[i]) * beta_distance_taxi + beta_female_taxi * female_dummy
 	end
@@ -240,7 +240,7 @@ end
 --the logic to determine availability is the same with current implementation
 local availability = {}
 local function computeAvailabilities(params,dbparams)
-	for i = 1, 1092*9 do 
+	for i = 1, 1169*9 do 
 		availability[i] = dbparams:availability(i)
 	end
 end

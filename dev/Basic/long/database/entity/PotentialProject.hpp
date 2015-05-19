1a87f55a1b765275e2cf8cd5e182a85276729a9f
gishara <gishara@smart.mit.edu>
2015-05-19 17:09:48 +0800

    improved the copy constructor, added floor are and profitPerUnit parameters

--- dev/Basic/long/database/entity/PotentialProject.hpp
+++ dev/Basic/long/database/entity/PotentialProject.hpp
@@ -24,7 +24,7 @@ namespace sim_mob {
          */
         class PotentialUnit {
         public:
-            PotentialUnit(BigSerial unitTypeId = INVALID_ID,int numUnits = 0,double floorArea = 0, int freehold = 0);
+            PotentialUnit(BigSerial unitTypeId = INVALID_ID,int numUnits = 0,double floorArea = 0, int freehold = 0, double profitPerUnit = 0.0);
             virtual ~PotentialUnit();
 
             PotentialUnit( const PotentialUnit &source);
@@ -33,17 +33,22 @@ namespace sim_mob {
             //Getters
             BigSerial getUnitTypeId() const;
             double getFloorArea() const;
+            void setFloorArea(double area);
             int isFreehold() const;
             int getNumUnits() const;
             void setNumUnits(int units);
             int getNumUnits();
             void setUnitTypeId(int typeId);
+            void setUnitProfit(double unitProfit);
+            double getUnitProfit() const;
             friend std::ostream& operator<<(std::ostream& strm,  const PotentialUnit& data);
+
         private:
             BigSerial unitTypeId;
             double floorArea;
             int freehold;
             int numUnits;
+            double profitPerUnit;
         };
 
         /**
@@ -52,7 +57,7 @@ namespace sim_mob {
          */
         class PotentialProject {
         public:
-            PotentialProject(const DevelopmentTypeTemplate* devTemplate = nullptr,const Parcel* parcel = nullptr,double constructionCost = 0,double grossArea = 0,double tempSelectProbability = 0,double investmentReturnRatio=0,double demolitionCost = 0, double expRatio=0);
+            PotentialProject(const DevelopmentTypeTemplate* devTemplate = nullptr,const Parcel* parcel = nullptr,double constructionCost = 0,double grossArea = 0,double tempSelectProbability = 0,double investmentReturnRatio=0,double demolitionCost = 0, double expRatio=0,int totalUnits = 0);
             virtual ~PotentialProject();
             
             PotentialProject( const PotentialProject &source);
@@ -81,7 +86,7 @@ namespace sim_mob {
             //Getters
             const DevelopmentTypeTemplate* getDevTemplate() const;
             const Parcel* getParcel() const;
-            const std::vector<PotentialUnit>& getUnits() const;
+            std::vector<PotentialUnit>& getUnits();
             double getProfit() const;
             double getConstructionCost() const;
             double getRevenue() const;
@@ -90,6 +95,7 @@ namespace sim_mob {
             double getExpRatio() const;
             double getTempSelectProbability() const;
             double getDemolitionCost() const;
+            int getTotalUnits();
 
             //Setters
             void setProfit(const double profit);
@@ -100,6 +106,7 @@ namespace sim_mob {
             void setExpRatio(double exRatio);
             void setTempSelectProbability(double probability);
             void setDemolitionCost(double demCost);
+            void setTotalUnits (int totUnits);
 
             std::vector<TemplateUnitType*> templateUnitTypes;
 
@@ -119,6 +126,7 @@ namespace sim_mob {
             double investmentReturnRatio;
             double expRatio;
             double tempSelectProbability;
+            int totalUnits;
 
         };
     }

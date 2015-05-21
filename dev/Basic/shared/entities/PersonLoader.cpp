fbf1da7433c421a90e804625d00166552dda065a
prabhurajreddy <prabhurajreddy2@gmail.com>
2015-05-21 19:25:37 +0800

    Compilation error removed

--- dev/Basic/shared/entities/PersonLoader.cpp
+++ dev/Basic/shared/entities/PersonLoader.cpp
@@ -412,6 +412,7 @@ void sim_mob::PeriodicPersonLoader::loadActivitySchedules()
 	ConfigParams& cfg = ConfigManager::GetInstanceRW().FullConfig();
 	unsigned actCtr = 0;
 	map<string, vector<TripChainItem*> > tripchains;
+	map<int, vector<TripChainItem*> > trips;
 	for (soci::rowset<soci::row>::const_iterator it=rs.begin(); it!=rs.end(); ++it)
 	{
 		const soci::row& r = (*it);

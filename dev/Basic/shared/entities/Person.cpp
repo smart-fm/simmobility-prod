a6bcce4efab7cb43becc01246b8288f2e37bc6c6
huaipeng <zhang.huaipeng@gmail.com>
2015-06-04 18:14:03 +0800

    implement car sharing

--- dev/Basic/shared/entities/Person.cpp
+++ dev/Basic/shared/entities/Person.cpp
@@ -858,6 +858,23 @@ void sim_mob::Person::convertODsToTrips() {
 					itSubTrip->startLocationType="NODE";
 					itSubTrip->endLocationType="NODE";
 				}
+				else if(itSubTrip->fromLocation.type_ == WayPoint::NODE
+						&& itSubTrip->toLocation.type_ == WayPoint::NODE){
+					if(itSubTrip->mode.find("Car Sharing")!=std::string::npos){
+
+						std::vector<sim_mob::OD_Trip> odTrips;
+						std::string originId = boost::lexical_cast<std::string>(
+								itSubTrip->fromLocation.node_->getID());
+						std::string destId = boost::lexical_cast<std::string>(
+								itSubTrip->toLocation.node_->getID());
+
+						itSubTrip->startLocationId=originId;
+						itSubTrip->endLocationId=destId;
+						itSubTrip->startLocationType="NODE";
+						itSubTrip->endLocationType="NODE";
+						itSubTrip->mode = "Sharing";
+					}
+				}
 				++itSubTrip;
 			}
 

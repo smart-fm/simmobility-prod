0283f20a129fa118bd512896e9e6e73f6d8160a4
Neeraj <neeraj@smart.mit.edu>
2015-09-07 14:19:11 +0800

    Updated TurningPath to TurningGroup

--- dev/Basic/shared/geospatial/simmobility_network/WayPoint.hpp
+++ dev/Basic/shared/geospatial/simmobility_network/WayPoint.hpp
@@ -4,132 +4,136 @@
 
 #pragma once
 
+#include "Link.hpp"
 #include "Node.hpp"
 #include "RoadSegment.hpp"
-#include "Link.hpp"
+#include "TurningGroup.hpp"
 
 namespace simmobility_network
 {
-  struct WayPoint
-  {
 
-    enum
-    {
-      //The way point is invalid. None of the pointers are valid
-      INVALID,
+struct WayPoint
+{
 
-      //The way point is a road-segment. roadSegment points to a RoadSegment object
-      NODE,
+	enum
+	{
+		//The way point is invalid. None of the pointers are valid
+		INVALID,
 
-      //The way point is a road-segment. roadSegment points to a RoadSegment object
-      ROAD_SEGMENT,
-      
-      //The way point is a link. link points to a Link object
-      LINK,
-      
-      //The way point is a turning. turningPath points to a TurningPath object
-      TURNING_PATH
-    } type;
+		//The way point is a road-segment. roadSegment points to a RoadSegment object
+		NODE,
 
-    union
-    {
-      const Node *node;
-      const RoadSegment *roadSegment;
-      const Link* link;
-      const TurningPath* turningPath;
-    } ;
-    
-    WayPoint() :
-    type(INVALID), roadSegment(NULL)
-    {
-    }
-    
-    explicit WayPoint(const Node *node) :
-    type(NODE), node(node)
-    {
-    }
-    
-    explicit WayPoint(const RoadSegment *segment) :
-    type(ROAD_SEGMENT), roadSegment(segment)
-    {
-    }
-    
-    explicit WayPoint(const Link *link) :
+		//The way point is a road-segment. roadSegment points to a RoadSegment object
+		ROAD_SEGMENT,
+
+		//The way point is a link. link points to a Link object
+		LINK,
+
+		//The way point is a turning group. turningGroup points to a TurningGroup object
+		TURNING_GROUP
+	} type;
+
+	union
+	{
+		const Node *node;
+		const RoadSegment *roadSegment;
+		const Link* link;
+		const TurningGroup* turningGroup;
+	};
+
+	WayPoint() :
+	type(INVALID), roadSegment(NULL)
+	{
+	}
+
+	explicit WayPoint(const Node *node) :
+	type(NODE), node(node)
+	{
+	}
+
+	explicit WayPoint(const RoadSegment *segment) :
+	type(ROAD_SEGMENT), roadSegment(segment)
+	{
+	}
+
+	explicit WayPoint(const Link *link) :
 	type(LINK), link(link)
 	{
 	}
 
-    explicit WayPoint(const TurningPath *turningPath) : type(TURNING_PATH), turningPath(turningPath) {}
+	explicit WayPoint(const TurningGroup *turningGroup) : type(TURNING_GROUP), turningGroup(turningGroup)
+	{
+	}
 
-    WayPoint(const WayPoint& orig)
-    {
-      type = orig.type;
+	WayPoint(const WayPoint& orig)
+	{
+		type = orig.type;
 
-      switch (type) 
-      {
-      case INVALID:
-        roadSegment = NULL;
-        break;
-        
-      case NODE:
-        node = orig.node;        
-        break;
-      
-      case ROAD_SEGMENT:
-        roadSegment = orig.roadSegment;
-        break;
+		switch (type)
+		{
+		case INVALID:
+			roadSegment = NULL;
+			break;
 
-      case LINK:
-		  link = orig.link;
-		  break;
+		case NODE:
+			node = orig.node;
+			break;
 
-      case TURNING_PATH:
-		  turningPath = orig.turningPath;
-		  break;
-      }
-    }
-    
-    bool operator==(const WayPoint& rhs) const
-    {
-      return (type == rhs.type && node == rhs.node && roadSegment == rhs.roadSegment && link == rhs.link && turningPath == rhs.turningPath);
-    }
-    
-    bool operator!=(const WayPoint& rhs) const
-    {
-      return !(*this == rhs);
-    }
+		case ROAD_SEGMENT:
+			roadSegment = orig.roadSegment;
+			break;
 
-    WayPoint& operator=(const WayPoint& rhs)
-    {
-      type = rhs.type;
+		case LINK:
+			link = orig.link;
+			break;
 
-      switch (type) 
-      {
-      case INVALID:
-        node = NULL;
-        roadSegment = NULL;
-        break;
-        
-      case NODE:
-        node = rhs.node;
-        break;
-      
-      case ROAD_SEGMENT:
-        roadSegment = rhs.roadSegment;
-        break;
+		case TURNING_GROUP:
+			turningGroup = orig.turningGroup;
+			break;
+		}
+	}
 
-      case LINK:
-      		  link = rhs.link;
-      		  break;
+	bool operator==(const WayPoint& rhs) const
+	{
+		return (type == rhs.type && node == rhs.node && roadSegment == rhs.roadSegment && link == rhs.link && turningGroup == rhs.turningGroup);
+	}
 
-      case TURNING_PATH:
-      		  turningPath = rhs.turningPath;
-      		  break;
-      }
+	bool operator!=(const WayPoint& rhs) const
+	{
+		return !(*this == rhs);
+	}
 
-      return *this;
-    }
-    
-  } ;
+	WayPoint& operator=(const WayPoint& rhs)
+	{
+		type = rhs.type;
+
+		switch (type)
+		{
+		case INVALID:
+			node = NULL;
+			roadSegment = NULL;
+			break;
+
+		case NODE:
+			node = rhs.node;
+			break;
+
+		case ROAD_SEGMENT:
+			roadSegment = rhs.roadSegment;
+			break;
+
+		case LINK:
+			link = rhs.link;
+			break;
+
+		case TURNING_GROUP:
+			turningGroup = rhs.turningGroup;
+			break;
+		}
+
+		return *this;
+	}
+
+};
 }
 

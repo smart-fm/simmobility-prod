1664c7ec9e427205942b13a23d386bfa98c5e3d6
Harish <harish@smart.mit.edu>
2015-10-08 15:17:16 +0800

    formatting dao classes

--- dev/Basic/medium/database/DatabaseHelper.hpp
+++ dev/Basic/medium/database/DatabaseHelper.hpp
@@ -2,191 +2,185 @@
 //Licensed under the terms of the MIT License";" as described in the file:
 //   license.txt   (http://opensource.org/licenses/MIT)
 
-/*
- * DatabaseHelper.hpp
- *
- *  Created on: Nov 18, 2013
- *      Author: Harish Loganathan
- */
-
 #pragma once
 
-namespace sim_mob {
-namespace medium {
-	#define APPLY_SCHEMA(schema, field) std::string(schema)+std::string(field)
+namespace sim_mob
+{
+namespace medium
+{
+#define APPLY_SCHEMA(schema, field) std::string(schema)+std::string(field)
 
-	/**
-	 * Useful string constants
-	 */
-	const std::string EMPTY_STRING = "";
+/**
+ * Useful string constants
+ */
+const std::string EMPTY_STRING = "";
 
-    /**
-     * Schemas
-     */
-    const std::string MAIN_SCHEMA = "main2012.";
-    const std::string PUBLIC_SCHEMA = "public.";
-    const std::string DEMAND_SCHEMA = "demand.";
+/**
+ * Schemas
+ */
+const std::string MAIN_SCHEMA = "main2012.";
+const std::string PUBLIC_SCHEMA = "public.";
+const std::string DEMAND_SCHEMA = "demand.";
 
-    /**
-     * Tables
-     */
-    const std::string DB_TABLE_INCOME_CATEGORIES = APPLY_SCHEMA(MAIN_SCHEMA, "income_category");
-    const std::string DB_TABLE_VEHICLE_CATEGORIES = APPLY_SCHEMA(MAIN_SCHEMA, "vehicle_category");
-    const std::string DB_TABLE_LOGSUMS = APPLY_SCHEMA(DEMAND_SCHEMA, "preday_logsum");
+/**
+ * Tables
+ */
+const std::string DB_TABLE_INCOME_CATEGORIES = APPLY_SCHEMA(MAIN_SCHEMA, "income_category");
+const std::string DB_TABLE_VEHICLE_CATEGORIES = APPLY_SCHEMA(MAIN_SCHEMA, "vehicle_category");
+const std::string DB_TABLE_LOGSUMS = APPLY_SCHEMA(DEMAND_SCHEMA, "preday_logsum");
 
-	/**
-	 * Stored procedures for long-term population database
-	 */
-	const std::string DB_SP_GET_INDIVIDUAL_IDS = APPLY_SCHEMA(MAIN_SCHEMA, "getindividualids()");
-	const std::string DB_SP_GET_INDIVIDUAL_BY_ID_FOR_PREDAY = APPLY_SCHEMA(MAIN_SCHEMA, "getindividualbyidforpreday(:_id)");
-	const std::string DB_SP_GET_ADDRESS_TAZ = APPLY_SCHEMA(MAIN_SCHEMA, "getaddresstaz()");
-	const std::string DB_SP_GET_LOGSUMS_BY_ID = APPLY_SCHEMA(PUBLIC_SCHEMA, "get_logsums_for_person(:_id)");
+/**
+ * Stored procedures for long-term population database
+ */
+const std::string DB_SP_GET_INDIVIDUAL_IDS = APPLY_SCHEMA(MAIN_SCHEMA, "getindividualids()");
+const std::string DB_SP_GET_INDIVIDUAL_BY_ID_FOR_PREDAY = APPLY_SCHEMA(MAIN_SCHEMA, "getindividualbyidforpreday(:_id)");
+const std::string DB_SP_GET_ADDRESS_TAZ = APPLY_SCHEMA(MAIN_SCHEMA, "getaddresstaz()");
+const std::string DB_SP_GET_LOGSUMS_BY_ID = APPLY_SCHEMA(PUBLIC_SCHEMA, "get_logsums_for_person(:_id)");
 
-	/**
-	 * Fields for long-term population database
-	 */
-	const std::string DB_FIELD_ID = "id";
-	const std::string DB_FIELD_PERSON_TYPE_ID = "person_type_id";
-	const std::string DB_FIELD_GENDER_ID = "gender_id";
-	const std::string DB_FIELD_STUDENT_TYPE_ID = "education_type_id";
-	const std::string DB_FIELD_VEHICLE_CATEGORY_ID = "vehicle_category_id";
-	const std::string DB_FIELD_AGE_CATEGORY_ID = "age_category_id";
-	const std::string DB_FIELD_INCOME = "income";
-	const std::string DB_FIELD_WORK_AT_HOME = "work_at_home";
-	const std::string DB_FIELD_CAR_LICENSE = "car_license";
-	const std::string DB_FIELD_MOTOR_LICENSE = "motor_license";
-	const std::string DB_FIELD_VANBUS_LICENSE = "vanbus_license";
-	const std::string DB_FIELD_WORK_TIME_FLEX = "time_restriction";
-	const std::string DB_FIELD_IS_STUDENT = "is_student";
-	const std::string DB_FIELD_HAS_FIXED_WORK_PLACE = "fixed_workplace";
-	const std::string DB_FIELD_ACTIVITY_ADDRESS_ID = "activity_address_id";
-	const std::string DB_FIELD_HOUSEHOLD_ID = "hhid";
-	const std::string DB_FIELD_HOME_ADDRESS_ID = "home_address_id";
-	const std::string DB_FIELD_HH_SIZE = "size";
-	const std::string DB_FIELD_HH_CHILDREN_UNDER_4 = "child_under4";
-	const std::string DB_FIELD_HH_CHILDREN_UNDER_15 = "child_under15";
-	const std::string DB_FIELD_HH_ADULTS = "adult";
-	const std::string DB_FIELD_HH_WORKERS = "workers";
+/**
+ * Fields for long-term population database
+ */
+const std::string DB_FIELD_ID = "id";
+const std::string DB_FIELD_PERSON_TYPE_ID = "person_type_id";
+const std::string DB_FIELD_GENDER_ID = "gender_id";
+const std::string DB_FIELD_STUDENT_TYPE_ID = "education_type_id";
+const std::string DB_FIELD_VEHICLE_CATEGORY_ID = "vehicle_category_id";
+const std::string DB_FIELD_AGE_CATEGORY_ID = "age_category_id";
+const std::string DB_FIELD_INCOME = "income";
+const std::string DB_FIELD_WORK_AT_HOME = "work_at_home";
+const std::string DB_FIELD_CAR_LICENSE = "car_license";
+const std::string DB_FIELD_MOTOR_LICENSE = "motor_license";
+const std::string DB_FIELD_VANBUS_LICENSE = "vanbus_license";
+const std::string DB_FIELD_WORK_TIME_FLEX = "time_restriction";
+const std::string DB_FIELD_IS_STUDENT = "is_student";
+const std::string DB_FIELD_HAS_FIXED_WORK_PLACE = "fixed_workplace";
+const std::string DB_FIELD_ACTIVITY_ADDRESS_ID = "activity_address_id";
+const std::string DB_FIELD_HOUSEHOLD_ID = "hhid";
+const std::string DB_FIELD_HOME_ADDRESS_ID = "home_address_id";
+const std::string DB_FIELD_HH_SIZE = "size";
+const std::string DB_FIELD_HH_CHILDREN_UNDER_4 = "child_under4";
+const std::string DB_FIELD_HH_CHILDREN_UNDER_15 = "child_under15";
+const std::string DB_FIELD_HH_ADULTS = "adult";
+const std::string DB_FIELD_HH_WORKERS = "workers";
 
-	/**
-	 * Logsum fields
-	 */
-	const std::string DB_FIELD_WORK_LOGSUM = "work";
-	const std::string DB_FIELD_EDUCATION_LOGSUM = "education";
-	const std::string DB_FIELD_SHOP_LOGSUM = "shopping";
-	const std::string DB_FIELD_OTHER_LOGSUM = "other";
-	const std::string DB_FIELD_DPT_LOGSUM = "day_pattern_tour";
-	const std::string DB_FIELD_DPS_LOGSUM = "day_pattern_stop";
+/**
+ * Logsum fields
+ */
+const std::string DB_FIELD_WORK_LOGSUM = "work";
+const std::string DB_FIELD_EDUCATION_LOGSUM = "education";
+const std::string DB_FIELD_SHOP_LOGSUM = "shopping";
+const std::string DB_FIELD_OTHER_LOGSUM = "other";
+const std::string DB_FIELD_DPT_LOGSUM = "day_pattern_tour";
+const std::string DB_FIELD_DPS_LOGSUM = "day_pattern_stop";
 
-	const std::string DB_FIELD_INCOME_CATEGORY_LOWER_LIMIT = "low_limit";
-	const std::string DB_FIELD_VEHICLE_CATEGORY_NAME = "name";
-	const std::string DB_FIELD_ADDRESS_ID = "address_id";
-	const std::string DB_FIELD_TAZ_CODE = "taz_code";
+const std::string DB_FIELD_INCOME_CATEGORY_LOWER_LIMIT = "low_limit";
+const std::string DB_FIELD_VEHICLE_CATEGORY_NAME = "name";
+const std::string DB_FIELD_ADDRESS_ID = "address_id";
+const std::string DB_FIELD_TAZ_CODE = "taz_code";
 
-	const std::string SEARCH_STRING_CAR_OWN_NORMAL = "car (normal time)";
-	const std::string SEARCH_STRING_CAR_OWN_OFF_PEAK = "car (off peak time)";
-	const std::string SEARCH_STRING_MOTORCYCLE = "motorcycle";
+const std::string SEARCH_STRING_CAR_OWN_NORMAL = "car (normal time)";
+const std::string SEARCH_STRING_CAR_OWN_OFF_PEAK = "car (off peak time)";
+const std::string SEARCH_STRING_MOTORCYCLE = "motorcycle";
 
-    /** get all individual ids from long-term population database */
-	const std::string DB_GET_ALL_PERSON_IDS = "SELECT * FROM " + DB_SP_GET_INDIVIDUAL_IDS;
+/** get all individual ids from long-term population database */
+const std::string DB_GET_ALL_PERSON_IDS = "SELECT * FROM " + DB_SP_GET_INDIVIDUAL_IDS;
 
-	/** load a specific individual by id */
-	const std::string DB_GET_PERSON_BY_ID = "SELECT * FROM " + DB_SP_GET_INDIVIDUAL_BY_ID_FOR_PREDAY; //argument to be passed
+/** load a specific individual by id */
+const std::string DB_GET_PERSON_BY_ID = "SELECT * FROM " + DB_SP_GET_INDIVIDUAL_BY_ID_FOR_PREDAY; //argument to be passed
 
-	/** load address taz mapping from LT database */
-	const std::string DB_GET_ADDRESS_TAZ = "SELECT * FROM " + DB_SP_GET_ADDRESS_TAZ;
+/** load address taz mapping from LT database */
+const std::string DB_GET_ADDRESS_TAZ = "SELECT * FROM " + DB_SP_GET_ADDRESS_TAZ;
 
-	/** load income categories */
-	const std::string DB_GET_INCOME_CATEGORIES = "SELECT * FROM " + DB_TABLE_INCOME_CATEGORIES;
+/** load income categories */
+const std::string DB_GET_INCOME_CATEGORIES = "SELECT * FROM " + DB_TABLE_INCOME_CATEGORIES;
 
-	/** load vehicle categories */
-	const std::string DB_GET_VEHICLE_CATEGORIES = "SELECT * FROM " + DB_TABLE_VEHICLE_CATEGORIES;
+/** load vehicle categories */
+const std::string DB_GET_VEHICLE_CATEGORIES = "SELECT * FROM " + DB_TABLE_VEHICLE_CATEGORIES;
 
-	/** load logsums by id */
-	const std::string DB_GET_LOGSUMS_BY_ID = "SELECT * FROM " + DB_SP_GET_LOGSUMS_BY_ID; // argument to be passed
+/** load logsums by id */
+const std::string DB_GET_LOGSUMS_BY_ID = "SELECT * FROM " + DB_SP_GET_LOGSUMS_BY_ID; // argument to be passed
 
-	/** insert logsums */
-	const std::string DB_INSERT_LOGSUMS = "INSERT INTO " + DB_TABLE_LOGSUMS + " VALUES (:v1, :v2, :v3, :v4, :v5, :v6, :v7)";
+/** insert logsums */
+const std::string DB_INSERT_LOGSUMS = "INSERT INTO " + DB_TABLE_LOGSUMS + " VALUES (:v1, :v2, :v3, :v4, :v5, :v6, :v7)";
 
-	/**
-	 * Fields for mongoDB population data
-	 */
-	const std::string MONGO_FIELD_ID = "_id";
-	const std::string MONGO_FIELD_HOUSEHOLD_ID = "hhid";
-	const std::string MONGO_FIELD_HH_FACTOR = "hhfactor";
-	const std::string MONGO_FIELD_INCOME_ID = "income_id";
-	const std::string MONGO_FIELD_PERSON_TYPE_ID = "person_type_id";
-	const std::string MONGO_FIELD_AGE_CATEGORY_ID = "age_id";
-	const std::string MONGO_FIELD_WORK_AT_HOME = "work_from_home_dummy";
-	const std::string MONGO_FIELD_DRIVER_LICENCE = "has_driving_license";
-	const std::string MONGO_FIELD_STUDENT_TYPE_ID = "student_type_id";
-	const std::string MONGO_FIELD_UNIVERSITY_STUDENT = "universitystudent";
-	const std::string MONGO_FIELD_FEMALE = "female_dummy";
-	const std::string MONGO_FIELD_HOME_MTZ = "home_mtz";
-	const std::string MONGO_FIELD_WORK_MTZ = "fix_work_location_mtz";
-	const std::string MONGO_FIELD_SCHOOL_MTZ = "school_location_mtz";
-	const std::string MONGO_FIELD_HH_ONLY_ADULTS = "only_adults";
-	const std::string MONGO_FIELD_HH_ONLY_WORKERS = "only_workers";
-	const std::string MONGO_FIELD_HH_NUM_UNDER_4 = "num_underfour";
-	const std::string MONGO_FIELD_HH_UNDER_15 = "presence_of_under15";
-	const std::string MONGO_FIELD_CAR_OWN = "car_own";
-	const std::string MONGO_FIELD_CAR_OWN_NORMAL = "car_own_normal";
-	const std::string MONGO_FIELD_CAR_OWN_OFFPEAK = "car_own_offpeak";
-	const std::string MONGO_FIELD_MOTOR_OWN = "motor_own";
-	const std::string MONGO_FIELD_MISSING_INCOME = "missingincome";
-	const std::string MONGO_FIELD_WORK_TIME_FLEX = "worktime_flex";
-	const std::string MONGO_FIELD_WORK_LOGSUM = "worklogsum";
-	const std::string MONGO_FIELD_EDU_LOGSUM = "edulogsum";
-	const std::string MONGO_FIELD_SHOP_LOGSUM = "shoplogsum";
-	const std::string MONGO_FIELD_OTHER_LOGSUM = "otherlogsum";
-	const std::string MONGO_FIELD_DPT_LOGSUM = "dptlogsum";
-	const std::string MONGO_FIELD_DPS_LOGSUM = "dpslogsum";
+/**
+ * Fields for mongoDB population data
+ */
+const std::string MONGO_FIELD_ID = "_id";
+const std::string MONGO_FIELD_HOUSEHOLD_ID = "hhid";
+const std::string MONGO_FIELD_HH_FACTOR = "hhfactor";
+const std::string MONGO_FIELD_INCOME_ID = "income_id";
+const std::string MONGO_FIELD_PERSON_TYPE_ID = "person_type_id";
+const std::string MONGO_FIELD_AGE_CATEGORY_ID = "age_id";
+const std::string MONGO_FIELD_WORK_AT_HOME = "work_from_home_dummy";
+const std::string MONGO_FIELD_DRIVER_LICENCE = "has_driving_license";
+const std::string MONGO_FIELD_STUDENT_TYPE_ID = "student_type_id";
+const std::string MONGO_FIELD_UNIVERSITY_STUDENT = "universitystudent";
+const std::string MONGO_FIELD_FEMALE = "female_dummy";
+const std::string MONGO_FIELD_HOME_MTZ = "home_mtz";
+const std::string MONGO_FIELD_WORK_MTZ = "fix_work_location_mtz";
+const std::string MONGO_FIELD_SCHOOL_MTZ = "school_location_mtz";
+const std::string MONGO_FIELD_HH_ONLY_ADULTS = "only_adults";
+const std::string MONGO_FIELD_HH_ONLY_WORKERS = "only_workers";
+const std::string MONGO_FIELD_HH_NUM_UNDER_4 = "num_underfour";
+const std::string MONGO_FIELD_HH_UNDER_15 = "presence_of_under15";
+const std::string MONGO_FIELD_CAR_OWN = "car_own";
+const std::string MONGO_FIELD_CAR_OWN_NORMAL = "car_own_normal";
+const std::string MONGO_FIELD_CAR_OWN_OFFPEAK = "car_own_offpeak";
+const std::string MONGO_FIELD_MOTOR_OWN = "motor_own";
+const std::string MONGO_FIELD_MISSING_INCOME = "missingincome";
+const std::string MONGO_FIELD_WORK_TIME_FLEX = "worktime_flex";
+const std::string MONGO_FIELD_WORK_LOGSUM = "worklogsum";
+const std::string MONGO_FIELD_EDU_LOGSUM = "edulogsum";
+const std::string MONGO_FIELD_SHOP_LOGSUM = "shoplogsum";
+const std::string MONGO_FIELD_OTHER_LOGSUM = "otherlogsum";
+const std::string MONGO_FIELD_DPT_LOGSUM = "dptlogsum";
+const std::string MONGO_FIELD_DPS_LOGSUM = "dpslogsum";
 
-	/**
-	 * Fields for mongoDB Zone data
-	 */
-	const std::string MONGO_FIELD_ZONE_ID = "zone_id";
-	const std::string MONGO_FIELD_ZONE_CODE = "zone_code";
-	const std::string MONGO_FIELD_ZONE_SHOPS = "shop";
-	const std::string MONGO_FIELD_ZONE_PARKING_RATE = "parking_rate";
-	const std::string MONGO_FIELD_ZONE_RESIDENT_WORKERS = "resident_workers";
-	const std::string MONGO_FIELD_ZONE_CENTRAL_ZONE = "central_dummy";
-	const std::string MONGO_FIELD_ZONE_EMPLOYMENT = "employment";
-	const std::string MONGO_FIELD_ZONE_POPULATION = "population";
-	const std::string MONGO_FIELD_ZONE_AREA = "area";
-	const std::string MONGO_FIELD_ZONE_TOTAL_ENROLLMENT = "total_enrollment";
-	const std::string MONGO_FIELD_ZONE_RESIDENT_STUDENTS = "resident_students";
-	const std::string MONGO_FIELD_ZONE_CBD_ZONE = "cbd_dummy";
+/**
+ * Fields for mongoDB Zone data
+ */
+const std::string MONGO_FIELD_ZONE_ID = "zone_id";
+const std::string MONGO_FIELD_ZONE_CODE = "zone_code";
+const std::string MONGO_FIELD_ZONE_SHOPS = "shop";
+const std::string MONGO_FIELD_ZONE_PARKING_RATE = "parking_rate";
+const std::string MONGO_FIELD_ZONE_RESIDENT_WORKERS = "resident_workers";
+const std::string MONGO_FIELD_ZONE_CENTRAL_ZONE = "central_dummy";
+const std::string MONGO_FIELD_ZONE_EMPLOYMENT = "employment";
+const std::string MONGO_FIELD_ZONE_POPULATION = "population";
+const std::string MONGO_FIELD_ZONE_AREA = "area";
+const std::string MONGO_FIELD_ZONE_TOTAL_ENROLLMENT = "total_enrollment";
+const std::string MONGO_FIELD_ZONE_RESIDENT_STUDENTS = "resident_students";
+const std::string MONGO_FIELD_ZONE_CBD_ZONE = "cbd_dummy";
 
-	/**
-	 * Fields for MongoDB cost data
-	 */
-	const std::string MONGO_FIELD_COST_ORIGIN = "origin";
-	const std::string MONGO_FIELD_COST_DESTINATION = "destin";
-	const std::string MONGO_FIELD_COST_PUB_WTT = "pub_wtt";
-	const std::string MONGO_FIELD_COST_CAR_IVT = "car_ivt";
-	const std::string MONGO_FIELD_COST_PUB_OUT = "pub_out";
-	const std::string MONGO_FIELD_COST_PUB_WALKT = "pub_walkt";
-	const std::string MONGO_FIELD_COST_DISTANCE = "distance";
-	const std::string MONGO_FIELD_COST_CAR_ERP = "car_cost_erp";
-	const std::string MONGO_FIELD_COST_PUB_IVT = "pub_ivt";
-	const std::string MONGO_FIELD_COST_AVG_TRANSFER = "avg_transfer";
-	const std::string MONGO_FIELD_COST_PUB_COST = "pub_cost";
+/**
+ * Fields for MongoDB cost data
+ */
+const std::string MONGO_FIELD_COST_ORIGIN = "origin";
+const std::string MONGO_FIELD_COST_DESTINATION = "destin";
+const std::string MONGO_FIELD_COST_PUB_WTT = "pub_wtt";
+const std::string MONGO_FIELD_COST_CAR_IVT = "car_ivt";
+const std::string MONGO_FIELD_COST_PUB_OUT = "pub_out";
+const std::string MONGO_FIELD_COST_PUB_WALKT = "pub_walkt";
+const std::string MONGO_FIELD_COST_DISTANCE = "distance";
+const std::string MONGO_FIELD_COST_CAR_ERP = "car_cost_erp";
+const std::string MONGO_FIELD_COST_PUB_IVT = "pub_ivt";
+const std::string MONGO_FIELD_COST_AVG_TRANSFER = "avg_transfer";
+const std::string MONGO_FIELD_COST_PUB_COST = "pub_cost";
 
-	/**
-	 * Fields from MongoDB zone_aimsunnode_mapping data
-	 */
-	const std::string MONGO_FIELD_NODE_ID = "_id";
-	const std::string MONGO_FIELD_MTZ_1092 = "MTZ_1092";
-	const std::string MONGO_FIELD_MTZ = "MTZ";
-	const std::string MONGO_FIELD_SOURCE_NODE = "source";
-	const std::string MONGO_FIELD_SINK_NODE = "sink";
+/**
+ * Fields from MongoDB zone_aimsunnode_mapping data
+ */
+const std::string MONGO_FIELD_NODE_ID = "_id";
+const std::string MONGO_FIELD_MTZ_1092 = "MTZ_1092";
+const std::string MONGO_FIELD_MTZ = "MTZ";
+const std::string MONGO_FIELD_SOURCE_NODE = "source";
+const std::string MONGO_FIELD_SINK_NODE = "sink";
 
-
-	/**
-	 * Fields for MTZ1169 to MTZ1092 mapping data
-	 */
-	const std::string MONGO_FIELD_MTZ1092 = "MTZ1092";
+/**
+ * Fields for MTZ1169 to MTZ1092 mapping data
+ */
+const std::string MONGO_FIELD_MTZ1092 = "MTZ1092";
 
 } // end namespace medium
 } // end namespace sim_mob
--- dev/Basic/medium/database/ZoneCostMongoDao.hpp
+++ dev/Basic/medium/database/ZoneCostMongoDao.hpp
@@ -2,115 +2,116 @@
 //Licensed under the terms of the MIT License, as described in the file:
 //   license.txt   (http://opensource.org/licenses/MIT)
 
-/*
- * ZoneMongoDao.hpp
- *
- *  Created on: Nov 30, 2013
- *      Author: Harish Loganathan
- */
-
 #pragma once
 #include <boost/unordered_map.hpp>
+#include <map>
+#include <string>
 #include <vector>
 #include "behavioral/params/ZoneCostParams.hpp"
 #include "database/dao/MongoDao.hpp"
 #include "database/DB_Config.hpp"
 #include "logging/Log.hpp"
 
-namespace sim_mob {
-namespace medium {
-class ZoneMongoDao : db::MongoDao {
+namespace sim_mob
+{
+namespace medium
+{
+
+/**
+ * mongodb dao for Zone collection
+ *
+ * \author Harish Loganathan
+ */
+class ZoneMongoDao: db::MongoDao
+{
 public:
 	ZoneMongoDao(db::DB_Config& dbConfig, const std::string& database, const std::string& collection);
 	virtual ~ZoneMongoDao();
 
-    /**
-     * Gets all values from the source and put them on the given list.
-     * @param outList to put the retrieved values.
-     * @return true if some values were returned, false otherwise.
-     */
-    bool getAllZones(boost::unordered_map<int, ZoneParams*>& outList) {
-        unsigned long long count = connection.getSession<mongo::DBClientConnection>().count(collectionName, mongo::BSONObj());
-    	//boost 1.49
-        outList.rehash(ceil(count / outList.max_load_factor()));
-    	//boost >= 1.50
-        //outList.reserve(count);
-        std::auto_ptr<mongo::DBClientCursor> cursor = connection.getSession<mongo::DBClientConnection>().query(collectionName, mongo::BSONObj());
-    	while(cursor->more()) {
-    		ZoneParams* zoneParams = new ZoneParams();
-    		fromRow(cursor->next(), *zoneParams);
-    		outList[zoneParams->getZoneId()] = zoneParams;
-    	}
-    	return true;
-    }
+	/**
+	 * Gets all values from the source and put them on the given list.
+	 * @param outList to put the retrieved values.
+	 * @return true if some values were returned, false otherwise.
+	 */
+	bool getAllZones(boost::unordered_map<int, ZoneParams*>& outList);
 
-    /**
-     * Converts a given row into a PersonParams type.
-     * @param result result row.
-     * @param outParam (Out parameter) to receive data from row.
-     */
-    void fromRow(mongo::BSONObj document, ZoneParams& outParam);
+	/**
+	 * Converts a given row into a PersonParams type.
+	 * @param result result row.
+	 * @param outParam (Out parameter) to receive data from row.
+	 */
+	void fromRow(mongo::BSONObj document, ZoneParams& outParam);
 };
 
-class CostMongoDao : db::MongoDao {
+/**
+ * mongodb dao for AM, PM and OP cost collections
+ *
+ * \author Harish Loganathan
+ */
+class CostMongoDao: db::MongoDao
+{
 public:
 	CostMongoDao(db::DB_Config& dbConfig, const std::string& database, const std::string& collection);
 	virtual ~CostMongoDao();
 
-    /**
-     * Gets all values from the source and put them on the given list.
-     * @param outList to put the retrieved values.
-     * @return true if some values were returned, false otherwise.
-     */
-    bool getAll(boost::unordered_map<int, boost::unordered_map<int, CostParams*> >& outList) {
-    	std::auto_ptr<mongo::DBClientCursor> cursor = connection.getSession<mongo::DBClientConnection>().query(collectionName, mongo::BSONObj());
-    	while(cursor->more()) {
-    		CostParams* costParams = new CostParams();
-    		fromRow(cursor->next(), *costParams);
-    		outList[costParams->getOriginZone()][costParams->getDestinationZone()] = costParams;
-    	}
-    	return true;
-    }
+	/**
+	 * Gets all values from the source and put them on the given list.
+	 * @param outList to put the retrieved values.
+	 * @return true if some values were returned, false otherwise.
+	 */
+	bool getAll(boost::unordered_map<int, boost::unordered_map<int, CostParams*> >& outList);
 
-    /**
-     * Converts a given row into a PersonParams type.
-     * @param result result row.
-     * @param outParam (Out parameter) to receive data from row.
-     */
-    void fromRow(mongo::BSONObj document, CostParams& outParam);
+	/**
+	 * Converts a given row into a PersonParams type.
+	 * @param result result row.
+	 * @param outParam (Out parameter) to receive data from row.
+	 */
+	void fromRow(mongo::BSONObj document, CostParams& outParam);
 };
 
-class ZoneNodeMappingDao : db::MongoDao {
+/**
+ * mongodb dao for Zone to node mapping collection
+ *
+ * \author Harish Loganathan
+ */
+class ZoneNodeMappingDao: db::MongoDao
+{
 public:
 	ZoneNodeMappingDao(db::DB_Config& dbConfig, const std::string& database, const std::string& collection);
 	virtual ~ZoneNodeMappingDao();
 
-    /**
-     * Gets all values from the source and put them on the given list.
-     * @param outList to put the retrieved values.
-     * @return true if some values were returned, false otherwise.
-     */
-    bool getAll(boost::unordered_map<int, std::vector<ZoneNodeParams*> >& outList);
+	/**
+	 * Gets all values from the source and put them on the given list.
+	 * @param outList to put the retrieved values.
+	 * @return true if some values were returned, false otherwise.
+	 */
+	bool getAll(boost::unordered_map<int, std::vector<ZoneNodeParams*> >& outList);
 
-    /**
-     * Converts a given row into a ZoneNodeParams type.
-     * @param result result row.
-     * @param outParam (Out parameter) to receive data from row.
-     */
-    void fromRow(mongo::BSONObj document, ZoneNodeParams& outParam);
+	/**
+	 * Converts a given row into a ZoneNodeParams type.
+	 * @param result result row.
+	 * @param outParam (Out parameter) to receive data from row.
+	 */
+	void fromRow(mongo::BSONObj document, ZoneNodeParams& outParam);
 };
 
-class MTZ12_MTZ08_MappingDao : db::MongoDao {
+/**
+ * mongodb dao for MTZ 2008 to MTZ 2012 mapping collection
+ *
+ * \author Harish Loganathan
+ */
+class MTZ12_MTZ08_MappingDao: db::MongoDao
+{
 public:
 	MTZ12_MTZ08_MappingDao(db::DB_Config& dbConfig, const std::string& database, const std::string& collection);
 	virtual ~MTZ12_MTZ08_MappingDao();
 
-    /**
-     * Gets all values from the source and put them on the given list.
-     * @param outList to put the retrieved values.
-     * @return true if some values were returned, false otherwise.
-     */
-    bool getAll(std::map<int,int>& outList);
+	/**
+	 * Gets all values from the source and put them on the given list.
+	 * @param outList to put the retrieved values.
+	 * @return true if some values were returned, false otherwise.
+	 */
+	bool getAll(std::map<int, int>& outList);
 };
 } // end namespace medium
 } // end namespace sim_mob

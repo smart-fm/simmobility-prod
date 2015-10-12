45b8c60fa2360135b46d24d8acc5a93017068f78
Neeraj <neeraj@smart.mit.edu>
2015-10-12 12:42:22 +0800

    Changes to shared component (Incident) file due to new road network [involved commenting out incompatible code, rather than correcting it]

---
+++ dev/Basic/shared/geospatial/Incident.cpp
@@ -0,0 +1,16 @@
+/*
+ * Incident.cpp
+ *
+ *  Created on: Sep 30, 2013
+ *      Author: zhang
+ */
+
+#include "Incident.hpp"
+
+namespace sim_mob {
+
+Incident::~Incident() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace sim_mob */

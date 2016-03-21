3083217a8db6bbdad6addfaa0f426a74496394c0
huaipeng <zhang.huaipeng@gmail.com>
2016-03-21 17:24:09 +0800

    add new role for waiting train activity

---
+++ dev/Basic/medium/entities/roles/waitTrainActivity/WaitTrainActivity.cpp
@@ -0,0 +1,104 @@
+//Copyright (c) 2013 Singapore-MIT Alliance for Research and Technology
+//Licensed under the terms of the MIT License, as described in the file:
+//   license.txt   (http://opensource.org/licenses/MIT)
+
+#include "WaitTrainActivity.hpp"
+
+#include <boost/algorithm/string.hpp>
+#include "entities/Person_MT.hpp"
+#include "entities/PT_Statistics.hpp"
+#include "entities/roles/driver/BusDriver.hpp"
+#include "geospatial/network/PT_Stop.hpp"
+#include "message/MT_Message.hpp"
+#include "WaitTrainActivityFacets.hpp"
+
+using std::vector;
+using namespace sim_mob;
+
+namespace sim_mob
+{
+
+namespace medium
+{
+
+sim_mob::medium::WaitTrainActivity::WaitTrainActivity(Person_MT* parent, sim_mob::medium::WaitTrainActivityBehavior* behavior,
+		sim_mob::medium::WaitTrainActivityMovement* movement, std::string roleName, Role<Person_MT>::Type roleType) :
+		sim_mob::Role<Person_MT>::Role(parent, behavior, movement, roleName, roleType), waitingTime(0),failedToBoardCount(0),platform(nullptr)
+{
+}
+
+sim_mob::medium::WaitTrainActivity::~WaitTrainActivity()
+{
+}
+
+Role<Person_MT>* sim_mob::medium::WaitTrainActivity::clone(Person_MT* parent) const
+{
+	WaitTrainActivityBehavior* behavior = new WaitTrainActivityBehavior();
+	WaitTrainActivityMovement* movement = new WaitTrainActivityMovement();
+	WaitTrainActivity* waitTrainActivity = new WaitTrainActivity(parent, behavior, movement);
+	behavior->setParent(waitTrainActivity);
+	movement->setParent(waitTrainActivity);
+	return waitTrainActivity;
+}
+
+void sim_mob::medium::WaitTrainActivity::increaseWaitingTime(unsigned int incrementMs)
+{
+	waitingTime += incrementMs;
+}
+
+void sim_mob::medium::WaitTrainActivity::make_frame_tick_params(timeslice now)
+{
+	getParams().reset(now);
+}
+
+void sim_mob::medium::WaitTrainActivity::collectTravelTime()
+{
+	PersonTravelTime personTravelTime;
+	std::string personId, tripStartPoint, tripEndPoint, subStartPoint, subEndPoint, subStartType, subEndType, mode, service, arrivaltime, travelTime;
+	personTravelTime.personId = parent->getId();
+	personTravelTime.tripStartPoint = (*(parent->currTripChainItem))->startLocationId;
+	personTravelTime.tripEndPoint = (*(parent->currTripChainItem))->endLocationId;
+	personTravelTime.subStartPoint = parent->currSubTrip->startLocationId;
+	personTravelTime.subEndPoint = parent->currSubTrip->endLocationId;
+	personTravelTime.subStartType = parent->currSubTrip->startLocationType;
+	personTravelTime.subEndType = parent->currSubTrip->endLocationType;
+	personTravelTime.mode = "WAITING_TRAIN";
+	personTravelTime.service = parent->currSubTrip->ptLineId;
+	personTravelTime.travelTime = ((double) parent->getRole()->getTravelTime())/1000.0; //convert to seconds
+	personTravelTime.arrivalTime = DailyTime(parent->getRole()->getArrivalTime()).getStrRepr();
+	messaging::MessageBus::PostMessage(PT_Statistics::getInstance(),
+					STORE_PERSON_TRAVEL_TIME, messaging::MessageBus::MessagePtr(new PersonTravelTimeMessage(personTravelTime)), true);
+}
+
+void sim_mob::medium::WaitTrainActivity::incrementDeniedBoardingCount()
+{
+	failedToBoardCount++;
+}
+
+unsigned int sim_mob::medium::WaitTrainActivity::getWaitingTime() const
+{
+	return waitingTime;
+}
+
+unsigned int sim_mob::medium::WaitTrainActivity::getDeniedBoardingCount() const
+{
+	return failedToBoardCount;
+}
+
+void sim_mob::medium::WaitTrainActivity::HandleParentMessage(messaging::Message::MessageType type, const messaging::Message& message)
+{}
+
+std::vector<BufferedBase*> sim_mob::medium::WaitTrainActivity::getSubscriptionParams()
+{
+	return vector<BufferedBase*>();
+}
+const Platform* sim_mob::medium::WaitTrainActivity::getStartPlatform() const
+{
+	return platform;
+}
+void sim_mob::medium::WaitTrainActivity::setStartPlatform(Platform* plat)
+{
+	platform = plat;
+}
+}
+}
---
+++ dev/Basic/medium/entities/roles/waitTrainActivity/WaitTrainActivity.hpp
@@ -0,0 +1,88 @@
+//Copyright (c) 2013 Singapore-MIT Alliance for Research and Technology
+//Licensed under the terms of the MIT License, as described in the file:
+//   license.txt   (http://opensource.org/licenses/MIT)
+
+#pragma once
+
+#include "entities/roles/Role.hpp"
+#include "geospatial/network/PT_Stop.hpp"
+#include "entities/Person_MT.hpp"
+
+namespace sim_mob
+{
+namespace medium
+{
+class TrainDriver;
+class WaitTrainActivityBehavior;
+class WaitTrainActivityMovement;
+
+/**
+ * A medium-term WaitTrainActivity.
+ * \author zhang huai peng
+ */
+class WaitTrainActivity: public sim_mob::Role<Person_MT>, public UpdateWrapper<UpdateParams>
+{
+public:
+	explicit WaitTrainActivity(Person_MT* parent, sim_mob::medium::WaitTrainActivityBehavior* behavior = nullptr,
+			sim_mob::medium::WaitTrainActivityMovement* movement = nullptr, std::string roleName = std::string("WaitTrainActivity_"),
+			Role<Person_MT>::Type roleType = Role<Person_MT>::RL_WAITTRAINACTIVITY);
+
+	virtual ~WaitTrainActivity();
+
+	virtual sim_mob::Role<Person_MT>* clone(Person_MT *parent) const;
+
+	virtual void make_frame_tick_params(timeslice now);
+
+	virtual std::vector<sim_mob::BufferedBase*> getSubscriptionParams();
+	/**
+	 * collect travel time for current role
+	 */
+	virtual void collectTravelTime();
+	/**
+	 * increase waiting time every frame tick.
+	 * @param incrementMs is accumulation time in milli-seconds
+	 */
+	void increaseWaitingTime(unsigned int incrementMs);
+	/**
+	 * increase failed boarding times
+	 */
+	void incrementDeniedBoardingCount();
+	/**
+	 * message handler which provide a chance to handle message transfered from parent agent.
+	 * @param type of the message.
+	 * @param message data received.
+	 */
+	virtual void HandleParentMessage(messaging::Message::MessageType type, const messaging::Message& message);
+	/**
+	 * get waiting time
+	 * @return waiting time in seconds
+	 */
+	unsigned int getWaitingTime() const;;
+	/**
+	 * get denied boarding count
+	 * @return denied boarding count
+	 */
+	unsigned int getDeniedBoardingCount() const;
+	/**
+	 * get starting platform
+	 * @return the starting platform
+	 */
+	const Platform* getStartPlatform() const;
+	/**
+	 * set starting platform
+	 * @param platform is a pointer to the Platform object
+	 */
+	void setStartPlatform(Platform* platform);
+private:
+	friend class WaitTrainActivityBehavior;
+	friend class WaitTrainActivityMovement;
+
+	/**record waiting time (in milliseconds) in the bus stop*/
+	unsigned int waitingTime;
+	/**pointer to waiting bus stop*/
+	const Platform* platform;
+	/**failed boarding times*/
+	unsigned int failedToBoardCount;
+};
+}
+}

ba12e530569b71db067eac380fbbe4423902c8a4
Gishara <gishara@smart.mit.edu>
2016-03-24 13:22:11 +0800

    added shared function to calculate Euclidean distance within given points

--- dev/Basic/long/util/SharedFunctions.hpp
+++ dev/Basic/long/util/SharedFunctions.hpp
@@ -54,6 +54,18 @@ namespace sim_mob
 	   return boost::shared_ptr<T>(val);
 	}
 
+	inline double distanceCalculateEuclidean(double x1, double y1, double x2, double y2)
+	{
+		double x = x1 - x2; //calculating number to square in next step
+		double y = y1 - y2;
+		double dist;
+
+		dist = pow(x, 2) + pow(y, 2);       //calculating Euclidean distance
+		dist = sqrt(dist);
+
+		return dist;
+	}
+
 	}
 
 }

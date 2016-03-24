b4def7a989c68d93f676762133e230c1fa05add0
Gishara <gishara@smart.mit.edu>
2016-03-24 13:41:33 +0800

    added data access layer classes for loading travel times from DB

---
+++ dev/Basic/long/database/dao/TravelTimeDao.cpp
@@ -0,0 +1,51 @@
+/*
+ * TravelTimeDao.cpp
+ *
+ *  Created on: 11 Mar 2016
+ *      Author: gishara
+ */
+
+#include "TravelTimeDao.hpp"
+#include "DatabaseHelper.hpp"
+#include "database/entity/TravelTime.hpp"
+
+using namespace sim_mob::db;
+using namespace sim_mob::long_term;
+
+TravelTimeDao::TravelTimeDao(DB_Connection& connection): SqlAbstractDao<TravelTime>(connection, DB_TABLE_TRAVEL_TIME, EMPTY_STR, EMPTY_STR, EMPTY_STR, EMPTY_STR, EMPTY_STR)
+{}
+
+TravelTimeDao::~TravelTimeDao() {}
+
+void TravelTimeDao::fromRow(Row& result, TravelTime& outObj)
+{
+    outObj.origin= result.get<BigSerial>("origin", INVALID_ID);
+    outObj.destination = result.get<BigSerial>("destination", INVALID_ID);
+    outObj.carTravelTime = result.get<double>("car_travel_time", 0);
+    outObj.publicTravelTime = result.get<double>("public_travel_time", 0);
+
+}
+
+void TravelTimeDao::toRow(TravelTime& data, Parameters& outParams, bool update) {}
+
+const TravelTime* TravelTimeDao::getTravelTimeByOriginDest(BigSerial origin, BigSerial destination)
+{
+	//const std::string queryStr = "SELECT * FROM " + DB_TABLE_TRAVEL_TIME + LIMIT;
+	db::Parameters params;
+	params.push_back(origin);
+	params.push_back(destination);
+	const std::string getTravelTime = "SELECT * FROM " + DB_TABLE_TRAVEL_TIME + " WHERE origin = :v1" + " AND destination = :v2;" ;
+	std::vector<TravelTime*> travelTimeList;
+	getByQueryId(getTravelTime,params,travelTimeList);
+	TravelTime *travelTime;
+	if(travelTimeList.empty())
+	{
+		travelTime = nullptr;
+	}
+	else
+	{
+		travelTime = travelTimeList.at(0);
+	}
+	return travelTime;
+}
+

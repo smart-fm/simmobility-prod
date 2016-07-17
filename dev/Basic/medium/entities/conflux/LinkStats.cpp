14c6749eea417b40012fb695283a46dce8d9afb7
Harish Loganathan <harish@smart.mit.edu>
2016-07-17 08:40:47 +0800

    WIP LinkStats

--- dev/Basic/medium/entities/conflux/LinkStats.cpp
+++ dev/Basic/medium/entities/conflux/LinkStats.cpp
@@ -2,10 +2,127 @@
 //Licensed under the terms of the MIT License, as described in the file:
 //   license.txt   (http://opensource.org/licenses/MIT)
 
+#include "LinkStats.hpp"
 
+#include <stddef.h>
+#include <cstdio>
+#include <map>
+#include <stdexcept>
+#include <vector>
+#include "entities/roles/Role.hpp"
+#include "geospatial/network/Link.hpp"
+#include "geospatial/network/RoadSegment.hpp"
+#include "entities/Vehicle.hpp"
+#include "entities/Person_MT.hpp"
 
+using namespace sim_mob;
+using namespace sim_mob::medium;
 
+void sim_mob::medium::LinkStats::addEntity(Person_MT* person)
+{
+	if(!person || !person->getRole() || !person->getRole()->getResource())
+	{
+		throw std::runtime_error("invalid person/role for link stats addition");
+	}
 
+	std::set<Person_MT*>::const_iterator personIt = onLinkEntities.find(person);
+	if(personIt == onLinkEntities.end())
+	{
+		onLinkEntities.insert(person);
+		++entryCount;
+	}
+	//else - we don't count an already counted person again
+}
 
+void sim_mob::medium::LinkStats::resetStats()
+{
+	carCount = 0;
+	busCount = 0;
+	motorcycleCount = 0;
+	taxiCount = 0;
+	otherVehiclesCount = 0;
+	entryCount = 0;
+	exitCount = 0;
+	density = 0;
+}
 
+void sim_mob::medium::LinkStats::removeEntitiy(Person_MT* person)
+{
+	if(!person)
+	{
+		throw std::runtime_error("invalid person for link stats addition");
+	}
 
+	size_t eraseCount = onLinkEntities.erase(person);
+	if(eraseCount > 0)
+	{
+		++exitCount;
+		Vehicle::VehicleType vehicleType = person->getRole()->getResource()->getVehicleType();
+		switch(vehicleType)
+		{
+		case Vehicle::CAR:
+		{
+			++carCount;
+			break;
+		}
+		case Vehicle::BUS:
+		{
+			++busCount;
+			break;
+		}
+		case Vehicle::TAXI:
+		{
+			++taxiCount;
+			break;
+		}
+		case Vehicle::BIKE:
+		{
+			++motorcycleCount;
+			break;
+		}
+		default:
+		{
+			++otherVehiclesCount;
+			break;
+		}
+		}
+	}
+	else
+	{
+		throw std::runtime_error("attempt to remove a person who was never added from LinkStats");
+	}
+}
+
+sim_mob::medium::LinkStats::LinkStats(const Link* link) : linkId(link->getLinkId()), carCount(0), busCount(0), motorcycleCount(0), taxiCount(0),
+		otherVehiclesCount(0), entryCount(0), exitCount(0), density(0), totalLinkLaneLength(0)
+{
+	const std::vector<RoadSegment*>& lnkSegments = link->getRoadSegments();
+	for(const RoadSegment* seg : lnkSegments)
+	{
+		totalLinkLaneLength = totalLinkLaneLength + (seg->getLength() * seg->getNoOfLanes());
+	}
+	totalLinkLaneLength = totalLinkLaneLength / 1000.0; //convert to KM
+}
+
+std::string sim_mob::medium::LinkStats::outputLinkStats() const
+{
+	char buf[200];
+	sprintf(buf, "%u,%.3f,%u,%u,%u,%u,%u,%u,%u",
+			linkId,
+			density,
+			entryCount,
+			exitCount,
+			carCount,
+			taxiCount,
+			motorcycleCount,
+			busCount,
+			otherVehiclesCount
+			);
+	return std::string(buf);
+}
+
+void sim_mob::medium::LinkStats::computeLinkDensity(double vehicleLength)
+{
+	double totalPCUs = vehicleLength / PASSENGER_CAR_UNIT;
+	density = totalPCUs / totalLinkLaneLength;
+}
--- dev/Basic/medium/entities/conflux/LinkStats.hpp
+++ dev/Basic/medium/entities/conflux/LinkStats.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <set>
+#include <string>
+
 namespace sim_mob
 {
 class Link;
-
 namespace medium
 {
 class Person_MT;
@@ -16,27 +18,36 @@ class Person_MT;
 class LinkStats
 {
 private:
-	const Link* link;
+	unsigned int linkId;
 	unsigned int carCount;
 	unsigned int busCount;
 	unsigned int motorcycleCount;
 	unsigned int taxiCount;
-	unsigned int totalCount;
+	unsigned int otherVehiclesCount;
 	unsigned int entryCount;
 	unsigned int exitCount;
+
+	/**
+	 * total length of all links in lane in kilometers
+	 * this is the sum of (segment length * number of lanes) for each road segment of this link represented by this linkStats
+	 */
+	double totalLinkLaneLength;
 	double density;
 
 	std::set<Person_MT*> onLinkEntities;
 
+	void resetStats();
+
 public:
-	LinkStats(const Link* link) : link(link), carCount(0), busCount(0), motorcycleCount(0), taxiCount(0), totalCount(0),
-		entryCount(0), exitCount(0), density(0) {}
+	LinkStats(const Link* link);
 
 	void addEntity(Person_MT* entitiy);
 
 	void removeEntitiy(Person_MT* entity);
 
 	std::string outputLinkStats() const;
+
+	void computeLinkDensity(double vehicleLength);
 };
 } // end medium
 } // end sim_mob

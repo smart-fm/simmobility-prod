6c69a97b6b73aba6ea8e6b9de4974f3ef00eb6ad
jabirshabbir <jabirrokx@yahoo.com>
2016-08-04 18:40:19 +0800

    changes afetr code review

--- dev/Basic/medium/entities/Person_MT.cpp
+++ dev/Basic/medium/entities/Person_MT.cpp
@@ -81,15 +81,9 @@ requestedNextSegStats(nullptr), canMoveToNextSegment(NONE), currSegStats(nullptr
 prevRole(nullptr), currRole(nullptr), nextRole(nullptr), numTicksStuck(0)
 {
 	ConfigParams& cfg = ConfigManager::GetInstanceRW().FullConfig();
-	int size=TrainController<sim_mob::medium::Person_MT>::getInstance()->getMapPlatformsSize();
-	if(size==161)
-	{
-		int r=8;
-	}
 	std::string ptPathsetStoredProcName = cfg.getDatabaseProcMappings().procedureMappings["pt_pathset"];
 	try
 	{
-		size=TrainController<sim_mob::medium::Person_MT>::getInstance()->getMapPlatformsSize();
 		convertPublicTransitODsToTrips(PT_NetworkCreater::getInstance(), ptPathsetStoredProcName);
 	}
 
@@ -97,18 +91,14 @@ prevRole(nullptr), currRole(nullptr), nextRole(nullptr), numTicksStuck(0)
 	{
 		Print()<<"[PT pathset]load pt pathset failed!"<<"["<<exception.originNode<<","<<exception.destNode<<"]"<<std::endl;
 	}
-	size=TrainController<sim_mob::medium::Person_MT>::getInstance()->getMapPlatformsSize();
+
 	insertWaitingActivityToTrip();
 	assignSubtripIds();
 	if (!tripChain.empty())
 	{
 		initTripChain();
 	}
-	int size_t=TrainController<sim_mob::medium::Person_MT>::getInstance()->getMapPlatformsSize();
-	if(size_t==161)
-	{
-		int e=78;
-	}
+
 }
 
 Person_MT::~Person_MT()

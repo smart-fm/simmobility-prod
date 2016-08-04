1065db511c66d5326bc474cf652e0672f37c3e37
adnanres <adnanres@users.noreply.github.com>
2016-08-04 16:00:25 +0800

    Update ntw.lua

--- dev/Basic/scripts/lua/mid/behavior-models/virtualcity/ntw.lua
+++ dev/Basic/scripts/lua/mid/behavior-models/virtualcity/ntw.lua
@@ -129,10 +129,7 @@ local function computeUtilities(params)
 	local income_id = params.income_id
 	local income_mid = {500,1250,1750,2250,2750,3500,4500,5500,6500,7500,8500,0,99999,99999}
 	local work_at_home_dummy = params.work_at_home_dummy
-	local car_own = params.car_own
-	local car_own_normal = params.car_own_normal
-	local car_own_offpeak = params.car_own_offpeak
-	local motor_own = params.motor_own
+	local veh_own_cat = params.vehicle_ownership_category
 	local worklogsum = params.worklogsum
 	local edulogsum = params.edulogsum
 	local shoplogsum = params.shoplogsum
@@ -208,8 +205,8 @@ local function computeUtilities(params)
 	-- other variables
 	local workathome,caravail,motoravail = 0,0,0
 	if work_at_home_dummy == 1 then workathome = 1 end
-	if car_own_normal >= 1 or car_own_offpeak >= 1 then caravail = 1 end
-	if motor_own >= 1 then motoravail = 1 end
+	if veh_own_cat == 3 or 4 or 5 or 6 then caravail = 1 end
+	if veh_own_cat == 2 or 3 or 5 or 6 then motoravail = 1 end
 	
 	utility[1] = 0
 	utility[2] = beta_cons_work_2+beta_parttime_work_2*parttime+beta_selfemployed_work_2*selfemployed+beta_universitystudent_work_2*universitystudent+beta_homemaker_work_2*homemaker+beta_retired_work_2*retired+beta_unemployed_work_2*unemployed+beta_nationalservice_work_2*nationalservice+beta_voluntary_work_2*voluntary+beta_domestic_work_2*domestic+beta_otherworker_work_2*otherworker+beta_student16_work_2*student16+beta_student515_work_2*student515+beta_child4_work_2*child4+beta_age2025_work_2*age2025+beta_age2635_work_2*age2635+beta_age5165_work_2*age5165+beta_maleage4_work_2*maleage4+beta_maleage515_work_2*maleage515+beta_femalenone_work_2*femalenone+beta_femaleage4_work_2*femaleage4+beta_femaleage515_work_2*femaleage515+beta_onlyadults_work_2*onlyadults+beta_onlyworkers_work_2*onlyworkers+beta_income_work_2*income+beta_workathome_work_2*workathome+beta_caravail_work_2*caravail+beta_motoravail_work_2*motoravail+beta_logsum_work_2*worklogsum

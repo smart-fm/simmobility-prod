92bc56846e2e5fa9ae100a9021956b4789cf75f5
huaipeng <zhang.huaipeng@gmail.com>
2016-11-14 14:39:49 +0800

    1. create taxi-stand agent 2. migrate taxi-stand agent into segment stats

---
+++ dev/Basic/shared/geospatial/network/TaxiStand.cpp
@@ -0,0 +1,72 @@
+/*
+ * TaxiStand.cpp
+ *
+ *  Created on: Oct 24, 2016
+ *      Author: fm-simmobility
+ */
+
+#include <geospatial/network/TaxiStand.hpp>
+
+namespace sim_mob
+{
+TaxiStand::TaxiStand():roadSegment(nullptr),length(0),offset(0),id(0){
+	// TODO Auto-generated constructor stub
+
+}
+
+TaxiStand::~TaxiStand() {
+	// TODO Auto-generated destructor stub
+}
+
+void TaxiStand::setLocation(const Point& pos)
+{
+	location = pos;
+}
+
+const Point& TaxiStand::getLocation() const
+{
+	return location;
+}
+
+void TaxiStand::setRoadSegment(RoadSegment* segment)
+{
+	roadSegment = segment;
+}
+
+const RoadSegment* TaxiStand::getRoadSegment() const
+{
+	return roadSegment;
+}
+
+void TaxiStand::setLength(const double len)
+{
+	length = len;
+}
+
+double TaxiStand::getLength() const
+{
+	return length;
+}
+
+void TaxiStand::setOffset(const double offset)
+{
+	this->offset = offset;
+}
+
+double TaxiStand::getOffset() const
+{
+	return offset;
+}
+
+void TaxiStand::setStandId(int id)
+{
+	this->id = id;
+}
+
+int TaxiStand::getStandId() const
+{
+	return id;
+}
+
+}
+

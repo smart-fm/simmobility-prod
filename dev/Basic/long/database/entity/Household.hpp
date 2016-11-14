50b1fb1716d158a314cf0fe9c44b16ece3df09f9
Gishara <gishara@smart.mit.edu>
2016-11-14 18:12:52 +0800

    added last awakend date and last bid status params

--- dev/Basic/long/database/entity/Household.hpp
+++ dev/Basic/long/database/entity/Household.hpp
@@ -26,7 +26,7 @@ namespace sim_mob
             Household( BigSerial id, BigSerial lifestyleId, BigSerial unitId, BigSerial ethnicityId, BigSerial vehicleCategoryId,  int size, int childUnder4, int childUnder15, int adult, double income,
             		   int housingDuration,int workers, int ageOfHead, int pendingStatusId,std::tm pendingFromDate,int unitPending,bool twoRoomHdbEligibility, bool threeRoomHdbEligibility,
 					   bool fourRoomHdbEligibility, int familyType, bool taxiAvailability, int vehicleOwnershipOptionId, double logsum, double currentUnitPrice, double householdAffordabilityAmount,
-					   int buySellInterval, std::tm moveInDate,int timeOnMarket,int timeOffMarket,int isBidder,int isSeller,int hasMoved, int tenureStatus,int awakenedDay,bool existInDB);
+					   int buySellInterval, std::tm moveInDate,int timeOnMarket,int timeOffMarket,int isBidder,int isSeller,int hasMoved, int tenureStatus,int awakenedDay,bool existInDB,std::tm lastAwakenedDate,int lastBidStatus);
 
             virtual ~Household();
 
@@ -84,6 +84,10 @@ namespace sim_mob
 			void	setCurrentUnitPrice( double value);
 			double	getCurrentUnitPrice() const;
 
+
+			std::tm getLastAwakenedDate();
+			void setLastAwakenedDate(std::tm lastAwknedDate);
+
 			int getBuySellInterval() const;
 			int getTimeOffMarket() const ;
 			int getTimeOnMarket() const;
@@ -114,6 +118,9 @@ namespace sim_mob
 			void setHasMoved(int hasMove);
 			void setAwakenedDay(int awakenDay);
 			void setExistInDB(bool exist);
+
+			int getLastBidStatus() const;
+			void setLastBidStatus(int lastBidStatus);
 
 
 			enum FAMILY_TYPE
@@ -176,6 +183,9 @@ namespace sim_mob
 			int tenureStatus;
 			int awakenedDay;
 			bool existInDB;
+
+			std::tm lastAwakenedDate;
+			int lastBidStatus;
 
         };
 

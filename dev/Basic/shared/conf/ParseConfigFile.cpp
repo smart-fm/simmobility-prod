fd0fdc99f0bd1105d2151b713a903ebe39f28b25
Neeraj <neeraj@smart.mit.edu>
2017-02-22 16:35:36 +0800

    Reading closed-loop settings only if it is enabled

--- dev/Basic/shared/conf/ParseConfigFile.cpp
+++ dev/Basic/shared/conf/ParseConfigFile.cpp
@@ -419,11 +419,15 @@ void sim_mob::ParseConfigFile::processClosedLoopPropertiesNode(xercesc::DOMEleme
 	if(node)
 	{
 		cfg.simulation.closedLoop.enabled = ParseBoolean(GetNamedAttributeValue(node, "enabled"));
-		cfg.simulation.closedLoop.guidanceFile = ParseString(GetNamedAttributeValue(GetSingleElementByName(node, "closed_loop_guidance"), "file"));
-		cfg.simulation.closedLoop.tollFile = ParseString(GetNamedAttributeValue(GetSingleElementByName(node, "closed_loop_toll"), "file"));
-		cfg.simulation.closedLoop.incentivesFile = ParseString(GetNamedAttributeValue(GetSingleElementByName(node, "closed_loop_incentives"), "file"));
-		cfg.simulation.closedLoop.sensorOutputFile = ParseString(GetNamedAttributeValue(GetSingleElementByName(node, "sensor_output"), "file"));
-		cfg.simulation.closedLoop.sensorStepSize = ParseUnsignedInt(GetNamedAttributeValue(GetSingleElementByName(node, "sensor_output"), "step_size"));
+
+		if(cfg.simulation.closedLoop.enabled)
+		{
+			cfg.simulation.closedLoop.guidanceFile = ParseString(GetNamedAttributeValue(GetSingleElementByName(node, "closed_loop_guidance"), "file"));
+			cfg.simulation.closedLoop.tollFile = ParseString(GetNamedAttributeValue(GetSingleElementByName(node, "closed_loop_toll"), "file"));
+			cfg.simulation.closedLoop.incentivesFile = ParseString(GetNamedAttributeValue(GetSingleElementByName(node, "closed_loop_incentives"), "file"));
+			cfg.simulation.closedLoop.sensorOutputFile = ParseString(GetNamedAttributeValue(GetSingleElementByName(node, "sensor_output"), "file"));
+			cfg.simulation.closedLoop.sensorStepSize = ParseUnsignedInt(GetNamedAttributeValue(GetSingleElementByName(node, "sensor_output"), "step_size"));
+		}
 	}
 }
 

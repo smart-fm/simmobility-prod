1ed08c3ac9866c1da591883666d6426096e0f4c0
Gishara <gishara@smart.mit.edu>
2017-03-29 16:13:42 +0800

    updated the code to load new vo coefficients

--- dev/Basic/long/database/dao/VehicleOwnershipCoefficientsDao.cpp
+++ dev/Basic/long/database/dao/VehicleOwnershipCoefficientsDao.cpp
@@ -17,18 +17,15 @@ VehicleOwnershipCoefficientsDao::~VehicleOwnershipCoefficientsDao() {}
 void VehicleOwnershipCoefficientsDao::fromRow(Row& result, VehicleOwnershipCoefficients& outObj)
 {
     outObj.vehicleOwnershipOptionId 		= result.get<BigSerial>("vehicle_ownership_option_id",INVALID_ID);
-    outObj.HHInc2 		= result.get<double>("hh_inc_2",0.0);
-    outObj.HHInc3 		= result.get<double>("hh_inc_3",0.0);
-    outObj.HHInc4 		= result.get<double>("hh_inc_4",0.0);
-    outObj.HHInc5 		= result.get<double>("hh_inc_5",0.0);
+    outObj.incomeAdj 		= result.get<double>("income_adj",0.0);
     outObj.malay		= result.get<double>("malay",0.0);
     outObj.indian		= result.get<double>("indian",0.0);
     outObj.otherRaces		= result.get<double>("other_races",0.0);
+    outObj.aboveSixty		= result.get<double>("above_60",0.0);
     outObj.whiteCollar		= result.get<double>("white_collar",0.0);
     outObj.worker		= result.get<double>("worker",0.0);
     outObj.HHChild1		= result.get<double>("hh_child_1",0.0);
     outObj.HHChild2Plus		= result.get<double>("hh_child_2plus",0.0);
-    outObj.elderlyHH		= result.get<double>("elderly_hh",0.0);
     outObj.taxi		= result.get<double>("taxi",0.0);
     outObj.mrt500m		= result.get<double>("mrt_500m",0.0);
     outObj.mrt1000m		= result.get<double>("mrt_1000m",0.0);

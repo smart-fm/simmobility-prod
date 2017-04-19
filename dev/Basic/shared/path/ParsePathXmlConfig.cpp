e9deb42bda3690890609fb8b3d12a54c77ac8bf1
Neeraj <neeraj@smart.mit.edu>
2017-04-19 13:50:28 +0800

    Added exception handling, refactoring

--- dev/Basic/shared/path/ParsePathXmlConfig.cpp
+++ dev/Basic/shared/path/ParsePathXmlConfig.cpp
@@ -1,28 +1,19 @@
 #include "ParsePathXmlConfig.hpp"
-#include "util/XmlParseHelper.hpp"
-#include <xercesc/dom/DOM.hpp>
+
 #include <boost/algorithm/string.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/lexical_cast.hpp>
-#include <sstream>
+#include <xercesc/dom/DOM.hpp>
+
 #include "logging/Log.hpp"
+#include "util/XmlParseHelper.hpp"
 
 using namespace xercesc;
 using namespace std;
-//Helper: turn a Xerces error message into a string.
-std::string TranscodeString(const XMLCh* str) {
-	char* raw = XMLString::transcode(str);
-	std::string res(raw);
-	XMLString::release(&raw);
-	return res;
-}
 
-sim_mob::ParsePathXmlConfig::ParsePathXmlConfig(const std::string& configFileName, PathSetConf& result) : cfg(result), ParseConfigXmlBase(configFileName)
+sim_mob::ParsePathXmlConfig::ParsePathXmlConfig(const std::string& configFileName, PathSetConf& result) :
+		cfg(result), ParseConfigXmlBase(configFileName)
 {
-	if(configFileName.size() == 0)
-	{
-		throw std::runtime_error("pathset config file not specified");
-	}
 	parseXmlAndProcess();
 }
 
@@ -30,43 +21,55 @@ void sim_mob::ParsePathXmlConfig::processXmlFile(XercesDOMParser& parser)
 {
 	//Verify that the root node is "config"
 	DOMElement* rootNode = parser.getDocument()->getDocumentElement();
-	if (TranscodeString(rootNode->getTagName()) != "pathset") {
-		throw std::runtime_error("xml parse error: root node of path set configuration file must be \"pathset\"");
+
+	if (TranscodeString(rootNode->getTagName()) != "pathset")
+	{
+		stringstream msg;
+		msg << "Error parsing file: " << inFilePath << ". Root node must be \'pathset\'";
+		throw runtime_error(msg.str());
 	}
 
-	ProcessPathSetNode(rootNode);
+	try
+	{
+		ProcessPathSetNode(rootNode);
+	}
+	catch(runtime_error &ex)
+	{
+		stringstream msg;
+		msg << "Error parsing file: " << inFilePath << ". " << ex.what();
+		throw runtime_error(msg.str());
+	}
 }
 
 
-void sim_mob::ParsePathXmlConfig::ProcessPathSetNode(xercesc::DOMElement* node){
+void sim_mob::ParsePathXmlConfig::ProcessPathSetNode(xercesc::DOMElement* node)
+{
+	if(!(cfg.enabled = ParseBoolean(GetNamedAttributeValue(node, "enabled"), false))) { return; }
 
-	if (!node)
-	{
-		std::cerr << "Pathset Configuration Not Found\n" ;
-		return;
-	}
-
-	if(!(cfg.enabled = ParseBoolean(GetNamedAttributeValue(node, "enabled"), "false"))) { return; }
-
-	//pathset generation threadpool size
+	//Path-set generation thread-pool size
 	xercesc::DOMElement* poolSize = GetSingleElementByName(node, "thread_pool");
+
 	if(!poolSize)
 	{
-		std::cerr << "Pathset generation thread pool size not specified, defaulting to 5\n";
+		Warn() << "\nWARNING! <thread_pool> node not defined in file " << inFilePath
+		       << ". Expected: <thread_pool size=\"\">. Defaulting to 4\n";
+		cfg.threadPoolSize = 4;
 	}
 	else
 	{
-		cfg.threadPoolSize = ParseInteger(GetNamedAttributeValue(poolSize, "size"), 5);
+		cfg.threadPoolSize = ParseInteger(GetNamedAttributeValue(poolSize, "size"), 4);
 	}
 
 	xercesc::DOMElement* pvtConfNode = GetSingleElementByName(node, "private_pathset");
-    if((cfg.privatePathSetEnabled = ParseBoolean(GetNamedAttributeValue(pvtConfNode, "enabled"), "false")))
+
+    if((cfg.privatePathSetEnabled = ParseBoolean(GetNamedAttributeValue(pvtConfNode, "enabled"), false)))
     {
     	processPrivatePathsetNode(pvtConfNode);
     }
 
     xercesc::DOMElement* publicConfNode = GetSingleElementByName(node, "public_pathset");
-    if((cfg.publicPathSetEnabled = ParseBoolean(GetNamedAttributeValue(publicConfNode, "enabled"), "false")))
+
+    if((cfg.publicPathSetEnabled = ParseBoolean(GetNamedAttributeValue(publicConfNode, "enabled"), false)))
     {
     	processPublicPathsetNode(publicConfNode);
     }
@@ -75,116 +78,101 @@ void sim_mob::ParsePathXmlConfig::ProcessPathSetNode(xercesc::DOMElement* node){
 
 void sim_mob::ParsePathXmlConfig::processPublicPathsetNode(xercesc::DOMElement* publicConfNode)
 {
+	cfg.publicPathSetMode = ParseString(GetNamedAttributeValue(publicConfNode, "mode", true), "");
 
-	cfg.publicPathSetMode = ParseString(GetNamedAttributeValue(publicConfNode, "mode"), "");
 	if (cfg.publicPathSetMode.empty() || !(cfg.publicPathSetMode == "normal" || cfg.publicPathSetMode == "generation"))
 	{
-		throw std::runtime_error("No pathset mode specified, \"normal\" and \"generation\" modes are supported supported\n");
+		stringstream msg;
+		msg << "Invalid value for <public_pathset mode=\""
+		    << cfg.publicPathSetMode << "\">. Expected: \"normal\" or \"generation\"";
+		throw runtime_error(msg.str());
 	}
+
 	if(cfg.publicPathSetMode == "generation" && cfg.privatePathSetMode == "generation")
 	{
-		std::string err = std::string("Error: Both Private and Public transit cannot run in generation mode together");
-		throw std::runtime_error(err);
+		stringstream msg;
+		msg << "Invalid value for <public_pathset mode=\""
+		    << cfg.publicPathSetMode << "\">. Private travel path-set and public transit path-set "
+		    << "cannot run in \"generation\" mode at the same time. Expected: \"normal\"";
+		throw runtime_error(msg.str());
 	}
+
 	if (cfg.publicPathSetMode == "generation")
 	{
-		xercesc::DOMElement* PT_odSource = GetSingleElementByName(publicConfNode, "od_source");
-		if (!PT_odSource)
-		{
-			throw std::runtime_error("public transit pathset is in \"generation\" mode but od_source is not found in pathset xml config\n");
-		}
-		else
-		{
-			cfg.publicPathSetOdSource = ParseString(GetNamedAttributeValue(PT_odSource, "table"), "");
-		}
+		xercesc::DOMElement* PT_odSource = GetSingleElementByName(publicConfNode, "od_source", true);
+		cfg.publicPathSetOdSource = ParseString(GetNamedAttributeValue(PT_odSource, "table"), "");
 
-        xercesc::DOMElement* bulk = GetSingleElementByName(publicConfNode, "bulk_generation_output_file");
-		if (!bulk)
-		{
-			throw std::runtime_error("Public transit pathset is in \"generation\" mode but bulk_generation_output_file_name is not found in pathset xml config\n");
-		}
-		else
-		{
-				cfg.publicPathSetOutputFile = ParseString(GetNamedAttributeValue(bulk, "name"), "");
-		}
+		xercesc::DOMElement* bulk = GetSingleElementByName(publicConfNode, "bulk_generation_output_file", true);
+		cfg.publicPathSetOutputFile = ParseString(GetNamedAttributeValue(bulk, "name"), "");
 	}
+
 	xercesc::DOMElement* publicPathSetAlgoConf = GetSingleElementByName(publicConfNode, "pathset_generation_algorithms");
+
 	if (publicPathSetAlgoConf)
 	{
-				cfg.publickShortestPathLevel = ParseInteger(GetNamedAttributeValue(
-								GetSingleElementByName(publicPathSetAlgoConf, "k_shortest_path"), "level"), 10);
-				cfg.simulationApproachIterations = ParseInteger(GetNamedAttributeValue(
-								GetSingleElementByName(publicPathSetAlgoConf, "simulation_approach"), "iterations"), 10);
+		cfg.publickShortestPathLevel =
+				ParseInteger(GetNamedAttributeValue(GetSingleElementByName(
+						publicPathSetAlgoConf, "k_shortest_path"), "level"), 10);
+
+		cfg.simulationApproachIterations =
+				ParseInteger(GetNamedAttributeValue(GetSingleElementByName(
+						publicPathSetAlgoConf, "simulation_approach"), "iterations"), 10);
 	}
 }
 
 void sim_mob::ParsePathXmlConfig::processPrivatePathsetNode(xercesc::DOMElement* pvtConfNode)
 {
-	cfg.privatePathSetMode = ParseString(GetNamedAttributeValue(pvtConfNode, "mode"), "");
+	cfg.privatePathSetMode = ParseString(GetNamedAttributeValue(pvtConfNode, "mode", true), "");
+
 	if (cfg.privatePathSetMode.empty() || !(cfg.privatePathSetMode == "normal" || cfg.privatePathSetMode == "generation"))
 	{
-		throw std::runtime_error("No pathset mode specified, \"normal\" and \"generation\" modes are supported supported\n");
+		stringstream msg;
+		msg << "Invalid value for <private_pathset mode=\""
+		    << cfg.privatePathSetMode << "\">. Expected: \"normal\" or \"generation\"";
+		throw runtime_error(msg.str());
 	}
 
-	//bulk pathset generation
+	//bulk path-set generation
 	if (cfg.privatePathSetMode == "generation")
 	{
-		xercesc::DOMElement* odSource = GetSingleElementByName(pvtConfNode, "od_source");
-		if (!odSource)
-		{
-			throw std::runtime_error("pathset is in \"generation\" mode but od_source is not found in pathset xml config\n");
-		}
-		else
-		{
-			cfg.odSourceTableName = ParseString(GetNamedAttributeValue(odSource, "table"), "");
-		}
+		xercesc::DOMElement* odSource = GetSingleElementByName(pvtConfNode, "od_source", true);
+		cfg.odSourceTableName = ParseString(GetNamedAttributeValue(odSource, "table"), "");
 
-		xercesc::DOMElement* bulk = GetSingleElementByName(pvtConfNode, "bulk_generation_output_file");
-		if (!bulk)
-		{
-			throw std::runtime_error("pathset is in \"generation\" mode but bulk_generation_output_file_name is not found in pathset xml config\n");
-		}
-		else
-		{
-			cfg.bulkFile = ParseString(GetNamedAttributeValue(bulk, "name"), "");
-		}
+		xercesc::DOMElement* bulk = GetSingleElementByName(pvtConfNode, "bulk_generation_output_file", true);
+		cfg.bulkFile = ParseString(GetNamedAttributeValue(bulk, "name"), "");
 	}
 
-	xercesc::DOMElement* tableNode = GetSingleElementByName(pvtConfNode, "tables");
-	if (!tableNode)
+	xercesc::DOMElement* tableNode = GetSingleElementByName(pvtConfNode, "tables", true);
+	cfg.RTTT_Conf = ParseString(GetNamedAttributeValue(tableNode, "historical_traveltime"), "");
+
+	if (cfg.RTTT_Conf.empty())
 	{
-		throw std::runtime_error("Pathset Tables specification not found");
+		stringstream msg;
+		msg << "Empty value in <tables historical_traveltime=\"\"/>. Expected: table name";
+		throw runtime_error(msg.str());
 	}
-	else
-	{
-		cfg.RTTT_Conf = ParseString(GetNamedAttributeValue(tableNode, "historical_traveltime"), "");
-		if (cfg.RTTT_Conf.empty())
-		{
-			throw std::runtime_error("historical travel time table name missing in pathset configuration");
-		}
 
-		cfg.DTT_Conf = ParseString(GetNamedAttributeValue(tableNode, "default_traveltime"), "");
-		if (cfg.DTT_Conf.empty())
-		{
-			throw std::runtime_error("default travel time table name missing in pathset configuration");
-		}
+	cfg.DTT_Conf = ParseString(GetNamedAttributeValue(tableNode, "default_traveltime"), "");
+
+	if (cfg.DTT_Conf.empty())
+	{
+		stringstream msg;
+		msg << "Empty value in <tables default_traveltime=\"\"/>. Expected: table name";
+		throw runtime_error(msg.str());
 	}
+
 	//function
-	xercesc::DOMElement* functionNode = GetSingleElementByName(pvtConfNode, "functions");
-	if (!functionNode)
-	{
-		throw std::runtime_error("Pathset Stored Procedure Not Found\n");
-	}
-	else
-	{
-		cfg.psRetrievalWithoutBannedRegion = ParseString(GetNamedAttributeValue(functionNode, "pathset_without_banned_area"), "");
-	}
+	xercesc::DOMElement* functionNode = GetSingleElementByName(pvtConfNode, "functions", true);
+	cfg.psRetrievalWithoutBannedRegion =
+			ParseString(GetNamedAttributeValue(functionNode, "pathset_without_banned_area"), "");
 
-	//recursive pathset generation
+	//recursive path-set generation
 	xercesc::DOMElement* recPS = GetSingleElementByName(pvtConfNode, "recursive_pathset_generation");
+
 	if (!recPS)
 	{
-		std::cerr << "recursive_pathset_generation Not Found, setting to false\n";
+		Warn() << "\nWARNING! <recursive_pathset_generation> node not defined in file " << inFilePath
+		       << ". Expected: <recursive_pathset_generation value=\"\"/>. Defaulting to false\n";
 		cfg.recPS = false;
 	}
 	else
@@ -194,9 +182,11 @@ void sim_mob::ParsePathXmlConfig::processPrivatePathsetNode(xercesc::DOMElement*
 
 	//reroute
 	xercesc::DOMElement* reroute = GetSingleElementByName(pvtConfNode, "reroute");
+
 	if (!reroute)
 	{
-		std::cerr << "reroute_enabled Not Found, setting to false\n";
+		Warn() << "\nWARNING! <reroute> node not defined in file " << inFilePath
+		       << ". Expected: <reroute enabled=\"\"/>. Defaulting to false\n";
 		cfg.reroute = false;
 	}
 	else
@@ -204,17 +194,24 @@ void sim_mob::ParsePathXmlConfig::processPrivatePathsetNode(xercesc::DOMElement*
 		cfg.reroute = ParseBoolean(GetNamedAttributeValue(reroute, "enabled"), false);
 	}
 
-	///	path generators configuration
+	//path generators configuration
 	xercesc::DOMElement* gen = GetSingleElementByName(pvtConfNode, "path_generators");
+
 	if (gen)
 	{
-		cfg.maxSegSpeed = ParseFloat(GetNamedAttributeValue(gen, "max_segment_speed"), 0.0);
+		cfg.maxSegSpeed = ParseFloat(GetNamedAttributeValue(gen, "max_segment_speed"));
+
 		if (cfg.maxSegSpeed <= 0.0)
 		{
-			throw std::runtime_error("Invalid or missing max_segment_speed attribute in the pathset configuration file");
+			stringstream msg;
+			msg << "Invalid value for <path_generators max_segment_speed=\"" << cfg.maxSegSpeed
+			    << "\">. Expected: Value greater than 0";
+			throw runtime_error(msg.str());
 		}
+
 		//random perturbation
 		xercesc::DOMElement* random = GetSingleElementByName(gen, "random_perturbation");
+
 		if (random)
 		{
 			cfg.perturbationIteration = ParseInteger(GetNamedAttributeValue(random, "iterations"), 0);
@@ -227,6 +224,7 @@ void sim_mob::ParsePathXmlConfig::processPrivatePathsetNode(xercesc::DOMElement*
 
 		//K-shortest Path
 		xercesc::DOMElement* ksp = GetSingleElementByName(gen, "k_shortest_path");
+
 		if (ksp)
 		{
 			cfg.kspLevel = ParseInteger(GetNamedAttributeValue(ksp, "level"), 0);
@@ -234,6 +232,7 @@ void sim_mob::ParsePathXmlConfig::processPrivatePathsetNode(xercesc::DOMElement*
 
 		//Link Elimination
 		xercesc::DOMElement* LE = GetSingleElementByName(gen, "link_elimination");
+
 		if (LE)
 		{
 			std::string types;
@@ -244,24 +243,10 @@ void sim_mob::ParsePathXmlConfig::processPrivatePathsetNode(xercesc::DOMElement*
 
 	//utility parameters
 	xercesc::DOMElement* utility = GetSingleElementByName(pvtConfNode, "utility_parameters");
+
 	if (utility)
 	{
-		cfg.params.highwayBias = ParseFloat(GetNamedAttributeValue(GetSingleElementByName(utility, "highwayBias"), "value"), 0.5);
-	}
-
-	//sanity check
-	std::stringstream out("");
-	if (cfg.RTTT_Conf == "")
-	{
-		out << "single path's realtime TT table name, ";
-	}
-	if (cfg.DTT_Conf == "")
-	{
-		out << "single path's default TT table name, ";
-	}
-	if (out.str().size())
-	{
-		std::string err = std::string("Missing:") + out.str();
-		throw std::runtime_error(err);
+		cfg.params.highwayBias =
+				ParseFloat(GetNamedAttributeValue(GetSingleElementByName(utility, "highwayBias"), "value"), 0.5);
 	}
 }

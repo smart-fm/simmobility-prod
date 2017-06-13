c6bfbe5969ddb7f09086760d68fdd1da245d262e
Neeraj <neeraj@smart.mit.edu>
2017-06-13 11:33:17 +0800

    Indentation and formatting changes

--- dev/Basic/shared/entities/controllers/MobilityServiceController.cpp
+++ dev/Basic/shared/entities/controllers/MobilityServiceController.cpp
@@ -7,43 +7,43 @@
 
 #include "MobilityServiceController.hpp"
 
-#include "entities/Person.hpp"
+#include "geospatial/network/RoadNetwork.hpp"
 #include "logging/ControllerLog.hpp"
 #include "message/MessageBus.hpp"
-#include "message/MobilityServiceControllerMessage.hpp"
+#include "util/GeomHelpers.hpp"
+#include "util/Utils.hpp"
 
-#include "geospatial/network/RoadNetwork.hpp"
+using namespace sim_mob;
+using namespace messaging;
 
-namespace sim_mob
-{
 MobilityServiceController::~MobilityServiceController()
 {
 	safe_delete_item(rebalancer);
 }
 
-void MobilityServiceController::subscribeDriver(Person* person)
+void MobilityServiceController::subscribeDriver(Person *person)
 {
 	subscribedDrivers.push_back(person);
 	availableDrivers.push_back(person);
 }
 
-void MobilityServiceController::unsubscribeDriver(Person* person)
+void MobilityServiceController::unsubscribeDriver(Person *person)
 {
 	subscribedDrivers.erase(std::remove(subscribedDrivers.begin(),
-		subscribedDrivers.end(), person), subscribedDrivers.end());
+	                                    subscribedDrivers.end(), person), subscribedDrivers.end());
 	availableDrivers.erase(std::remove(availableDrivers.begin(),
-		availableDrivers.end(), person), availableDrivers.end());
+	                                   availableDrivers.end(), person), availableDrivers.end());
 }
 
-void MobilityServiceController::driverAvailable(Person* person)
+void MobilityServiceController::driverAvailable(Person *person)
 {
 	availableDrivers.push_back(person);
 }
 
-void MobilityServiceController::driverUnavailable(Person* person)
+void MobilityServiceController::driverUnavailable(Person *person)
 {
 	availableDrivers.erase(std::remove(availableDrivers.begin(),
-		availableDrivers.end(), person), availableDrivers.end());
+	                                   availableDrivers.end(), person), availableDrivers.end());
 }
 
 Entity::UpdateStatus MobilityServiceController::frame_init(timeslice now)
@@ -54,7 +54,7 @@ Entity::UpdateStatus MobilityServiceController::frame_init(timeslice now)
 	}
 
 	currTick = now;
-	
+
 	return Entity::UpdateStatus::Continue;
 }
 
@@ -67,16 +67,19 @@ Entity::UpdateStatus MobilityServiceController::frame_tick(timeslice now)
 		localTick = 0;
 
 #ifndef NDEBUG
-		if(isComputingSchedules)
+		if (isComputingSchedules)
+		{
 			throw std::runtime_error("At this point, the controller should not be computing schedules");
+		}
 		isComputingSchedules = true;
 #endif
 		computeSchedules();
+
 #ifndef NDEBUG
 		isComputingSchedules = false;
 #endif
-		rebalancer->rebalance(availableDrivers, currTick);
 
+		rebalancer->rebalance(availableDrivers, currTick);
 	}
 	else
 	{
@@ -90,90 +93,101 @@ void MobilityServiceController::frame_output(timeslice now)
 {
 }
 
-void MobilityServiceController::HandleMessage(messaging::Message::MessageType type, const messaging::Message& message)
+void MobilityServiceController::HandleMessage(messaging::Message::MessageType type, const messaging::Message &message)
 {
 #ifndef NDEBUG
 	if (isComputingSchedules)
+	{
 		throw std::runtime_error("At this point, the controller should not be computing schedules");
+	}
 #endif
+
 	switch (type)
 	{
-		case MSG_DRIVER_SUBSCRIBE:
+	case MSG_DRIVER_SUBSCRIBE:
+	{
+		const DriverSubscribeMessage &subscribeArgs = MSG_CAST(DriverSubscribeMessage, message);
+		subscribeDriver(subscribeArgs.person);
+		break;
+	}
+
+	case MSG_DRIVER_UNSUBSCRIBE:
+	{
+		const DriverUnsubscribeMessage &unsubscribeArgs = MSG_CAST(DriverUnsubscribeMessage, message);
+		ControllerLog() << "Driver " << unsubscribeArgs.person->getDatabaseId() << " unsubscribed " << std::endl;
+		unsubscribeDriver(unsubscribeArgs.person);
+		break;
+	}
+
+	case MSG_DRIVER_AVAILABLE:
+	{
+		const DriverAvailableMessage &availableArgs = MSG_CAST(DriverAvailableMessage, message);
+		driverAvailable(availableArgs.person);
+		break;
+	}
+
+	case MSG_TRIP_REQUEST:
+	{
+		const TripRequestMessage &requestArgs = MSG_CAST(TripRequestMessage, message);
+
+		ControllerLog() << "Request received by the controller from " << requestArgs.personId << " at time "
+		                << currTick.frame() << ". Message was sent at "
+		                << requestArgs.currTick.frame() << " with startNodeId " << requestArgs.startNodeId
+		                << ", destinationNodeId "
+		                << requestArgs.destinationNodeId << ", and driverId not_yet_assigned" << std::endl;
+
+		/*
+		TripRequest r; r.currTick=requestArgs.currTick; r.userId = requestArgs.personId;
+		r.startNodeId=requestArgs.startNodeId; r.destinationNodeId=requestArgs.destinationNodeId;
+		r.extraTripTimeThreshold=requestArgs.extraTripTimeThreshold;
+		*/
+		requestQueue.push_back(requestArgs);
+
+
+		const Node *startNode = RoadNetwork::getInstance()->getMapOfIdvsNodes().at(requestArgs.startNodeId);
+		rebalancer->onRequestReceived(startNode);
+
+		break;
+	}
+
+	case MSG_SCHEDULE_PROPOSITION_REPLY:
+	{
+		const SchedulePropositionReplyMessage &replyArgs = MSG_CAST(SchedulePropositionReplyMessage, message);
+		if (!replyArgs.success)
 		{
-			const DriverSubscribeMessage &subscribeArgs = MSG_CAST(DriverSubscribeMessage, message);
-			subscribeDriver(subscribeArgs.person);
-			break;
+			ControllerLog() << "Assignment failure received from " << replyArgs.personId << " at time "
+			                << currTick.frame() << ". Message was sent at " << replyArgs.currTick.frame()
+			                << " with startNodeId "
+			                << replyArgs.startNodeId << ", destinationNodeId " << replyArgs.destinationNodeId
+			                << ", and driverId "
+			                << replyArgs.driver->getDatabaseId() << std::endl;
+
+			TripRequestMessage r;
+			r.currTick = replyArgs.currTick;
+			r.personId = replyArgs.personId;
+			r.startNodeId = replyArgs.startNodeId;
+			r.destinationNodeId = replyArgs.destinationNodeId;
+			r.extraTripTimeThreshold = replyArgs.extraTripTimeThreshold;
+			requestQueue.push_back(r);
 		}
-
-		case MSG_DRIVER_UNSUBSCRIBE:
+		else
 		{
-			const DriverUnsubscribeMessage& unsubscribeArgs = MSG_CAST(DriverUnsubscribeMessage, message);
-			ControllerLog()<<"Driver " << unsubscribeArgs.person->getDatabaseId()<<" unsubscribed "<< std::endl;
-			unsubscribeDriver(unsubscribeArgs.person);
-			break;
+
+			ControllerLog() << "Assignment success received from " << replyArgs.personId << " at time "
+			                << currTick.frame() << ". Message was sent at " << replyArgs.currTick.frame()
+			                << " with startNodeId "
+			                << replyArgs.startNodeId << ", destinationNodeId " << replyArgs.destinationNodeId
+			                << ", and driverId "
+			                << replyArgs.driver->getDatabaseId() << std::endl;
+
+			driverUnavailable(replyArgs.driver);
 		}
+		break;
+	}
 
-		case MSG_DRIVER_AVAILABLE:
-		{
-			const DriverAvailableMessage &availableArgs = MSG_CAST(DriverAvailableMessage, message);
-			driverAvailable(availableArgs.person);
-			break;
-		}
-
-		case MSG_TRIP_REQUEST:
-		{
-			const TripRequestMessage &requestArgs = MSG_CAST(TripRequestMessage, message);
-
-			ControllerLog() << "Request received by the controller from " << requestArgs.personId << " at time " << currTick.frame() << ". Message was sent at "
-				<< requestArgs.currTick.frame() << " with startNodeId " << requestArgs.startNodeId << ", destinationNodeId "
-				<< requestArgs.destinationNodeId << ", and driverId not_yet_assigned" << std::endl;
-
-			/*
-			TripRequest r; r.currTick=requestArgs.currTick; r.userId = requestArgs.personId;
-			r.startNodeId=requestArgs.startNodeId; r.destinationNodeId=requestArgs.destinationNodeId;
-			r.extraTripTimeThreshold=requestArgs.extraTripTimeThreshold;
-			*/
-			requestQueue.push_back(requestArgs);
-
-
-			const Node* startNode = RoadNetwork::getInstance()->getMapOfIdvsNodes().at(requestArgs.startNodeId);
-			rebalancer->onRequestReceived(startNode);
-
-            break;
-        }
-
-        case MSG_SCHEDULE_PROPOSITION_REPLY:
-        {
-			const SchedulePropositionReplyMessage& replyArgs = MSG_CAST(SchedulePropositionReplyMessage, message);
-			if (!replyArgs.success)
-			{
-				ControllerLog() << "Assignment failure received from " << replyArgs.personId << " at time "
-								<< currTick.frame() << ". Message was sent at " << replyArgs.currTick.frame()
-								<< " with startNodeId "
-								<< replyArgs.startNodeId << ", destinationNodeId " << replyArgs.destinationNodeId
-								<< ", and driverId "
-								<< replyArgs.driver->getDatabaseId() << std::endl;
-
-				TripRequestMessage r; r.currTick=replyArgs.currTick; r.personId= replyArgs.personId;
-				r.startNodeId=replyArgs.startNodeId; r.destinationNodeId=replyArgs.destinationNodeId;
-				r.extraTripTimeThreshold=replyArgs.extraTripTimeThreshold;
-				requestQueue.push_back(r);
-			} else {
-
-				ControllerLog() << "Assignment success received from " << replyArgs.personId << " at time "
-								<< currTick.frame() << ". Message was sent at " << replyArgs.currTick.frame()
-								<< " with startNodeId "
-								<< replyArgs.startNodeId << ", destinationNodeId " << replyArgs.destinationNodeId
-								<< ", and driverId "
-								<< replyArgs.driver->getDatabaseId() << std::endl;
-
-				driverUnavailable(replyArgs.driver);
-			}
-			break;
-        }
-
-        default: throw std::runtime_error("Unrecognized message");
-    };
+	default:
+		throw std::runtime_error("Unrecognized message");
+	};
 
 }
 
@@ -182,72 +196,70 @@ bool MobilityServiceController::isNonspatial()
 	return true;
 }
 
-void MobilityServiceController::sendScheduleProposition(const Person* driver, Schedule schedule) const
+void MobilityServiceController::sendScheduleProposition(const Person *driver, Schedule schedule) const
 {
-	messaging::MessageBus::PostMessage((messaging::MessageHandler*) driver, MSG_SCHEDULE_PROPOSITION,
-			messaging::MessageBus::MessagePtr(new SchedulePropositionMessage(currTick, schedule) ) );
+	MessageBus::PostMessage((MessageHandler *) driver, MSG_SCHEDULE_PROPOSITION, MessageBus::MessagePtr(
+			new SchedulePropositionMessage(currTick, schedule)));
 }
 
-bool MobilityServiceController::isCruising(Person* driver) const
+bool MobilityServiceController::isCruising(Person *driver) const
 {
-    MobilityServiceDriver* currDriver = driver->exportServiceDriver();
-    if (currDriver)
-    {
-        if (currDriver->getServiceStatus() == MobilityServiceDriver::SERVICE_FREE)
-        {
-            return true;
-        }
-    }
-    return false;
+	MobilityServiceDriver *currDriver = driver->exportServiceDriver();
+	if (currDriver)
+	{
+		if (currDriver->getServiceStatus() == MobilityServiceDriver::SERVICE_FREE)
+		{
+			return true;
+		}
+	}
+	return false;
 }
 
-const Node* MobilityServiceController::getCurrentNode(Person* driver) const
+const Node *MobilityServiceController::getCurrentNode(Person *driver) const
 {
-    MobilityServiceDriver* currDriver = driver->exportServiceDriver();
-    if (currDriver)
-    {
-    	const Node* currentNode =currDriver->getCurrentNode();
-        return currentNode;
-    }
-    return nullptr;
+	MobilityServiceDriver *currDriver = driver->exportServiceDriver();
+	if (currDriver)
+	{
+		const Node *currentNode = currDriver->getCurrentNode();
+		return currentNode;
+	}
+	return nullptr;
 }
 
-const Person* MobilityServiceController::findClosestDriver(const Node* node) const
+const Person *MobilityServiceController::findClosestDriver(const Node *node) const
 {
-	double bestDistanceSqr = std::numeric_limits<double>::max();
+	double bestDistance = std::numeric_limits<double>::max();
 	double bestX, bestY;
 
-	Person* bestDriver = NULL;
+	Person *bestDriver = NULL;
 	auto driver = availableDrivers.begin();
 
 	while (driver != availableDrivers.end())
 	{
-		if (isCruising(*driver) )
+		if (isCruising(*driver))
 		{
-				const Node* driverNode = getCurrentNode(*driver);
-				double currDistanceSqr =
-					(node->getPosX() - driverNode->getPosX() )*
-					(node->getPosX() - driverNode->getPosX() );
-				currDistanceSqr+=
-					(node->getPosY() - driverNode->getPosY() )*
-					(node->getPosY() - driverNode->getPosY() );
+			const Node *driverNode = getCurrentNode(*driver);
+			double currDistance = dist(node->getLocation(), driverNode->getLocation());
 
-				if (currDistanceSqr < bestDistanceSqr)
-				{
-					bestDriver = *driver;
-					bestDistanceSqr = currDistanceSqr;
-					bestX = driverNode->getPosX();
-					bestY = driverNode->getPosY();
-				}
+			if (currDistance < bestDistance)
+			{
+				bestDriver = *driver;
+				bestDistance = currDistance;
+				bestX = driverNode->getPosX();
+				bestY = driverNode->getPosY();
+			}
 		}
 		driver++;
 	}
-	if (bestDriver!=NULL)
+
+	if (bestDriver != NULL)
+	{
 		ControllerLog() << "Closest vehicle is at (" << bestX << ", " << bestY << ")" << std::endl;
+	}
 	else
-		ControllerLog()<<"No available driver" << std::endl;
+	{
+		ControllerLog() << "No available driver" << std::endl;
+	}
+
 	return bestDriver;
 }
-
-}
-

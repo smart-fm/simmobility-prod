5ee0eac0e677418f4503cfab1858acb649ec6b1b
Gishara <gishara@smart.mit.edu>
2017-09-04 15:01:49 +0800

    added code to call  job assignment calculation

--- dev/Basic/long/agent/impl/HouseholdAgent.cpp
+++ dev/Basic/long/agent/impl/HouseholdAgent.cpp
@@ -25,6 +25,7 @@
 #include "model/VehicleOwnershipModel.hpp"
 #include "model/AwakeningSubModel.hpp"
 #include "model/SchoolAssignmentSubModel.hpp"
+#include "model/JobAssignmentModel.hpp"
 #include "util/PrintLog.hpp"
 #include <random>
 
@@ -411,6 +412,26 @@ void HouseholdAgent::onWorkerEnter()
 				model->getLogsumOfVaryingHomeOrWork(hh->getId());
 		}
 	}
+
+	JobAssignmentModel jobAssignModel(model);
+	const Household *hh = this->getHousehold();
+
+	if( hh != NULL )
+	{
+		vector<BigSerial> individuals = household->getIndividuals();
+		for(int n = 0; n < individuals.size(); n++)
+		{
+			const Individual *individual = getModel()->getIndividualById(individuals[n]);
+			if(individual->getId() == 60408)
+							{
+			if(individual->getEmploymentStatusId() < 4)
+			{
+				jobAssignModel.computeJobAssignmentProbability(individual->getId());
+			}
+							}
+		}
+	}
+
 
     if (!marketSeller)
     {

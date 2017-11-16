539807d9450cabe641d84559dd0556a1e28a725b
Neeraj <neeraj@smart.mit.edu>
2017-11-16 14:15:51 +0800

    Updated on amod controller - Defined the behaviour of the controller

--- dev/Basic/shared/entities/controllers/AMOD_Controller.hpp
+++ dev/Basic/shared/entities/controllers/AMOD_Controller.hpp
@@ -11,11 +11,47 @@ namespace sim_mob
 
 class AMOD_Controller : public OnCallController
 {
+private:
+	/**Stores the single ride requests*/
+	std::list<TripRequestMessage> singleRideRequests;
+
+	/**Stores the shared ride requests*/
+	std::list<TripRequestMessage> sharedRideRequests;
+
+	/**
+	 * The set of drivers which are serving the shared requests and have capacity to serve
+	 * additional requests
+	 */
+	std::set<const Person *> driversServingSharedReq;
+
 protected:
 	/**
 	 * Performs the controller algorithm to assign vehicles to requests
 	 */
 	virtual void computeSchedules();
+
+	/**
+	 * Separates the trip requests based on the type and adds them to the respective lists
+	 */
+	void separateSharedAndSingleRequests();
+
+	/**
+	 * Tries to match unassigned requests (for shared rides) to drivers that are currently
+	 * serving other shared requests but have capacity to accommodate additional passengers
+	 */
+	void matchDriversServingSharedReq();
+
+	/**
+	 * Matches the single rider requests to the nearest available driver
+	 */
+	void matchSingleRiderReq();
+
+	/**
+	 * Iterates over all the un-assigned requests and check if they can be added to schedule. If yes it
+	 * returns the new schedule, with the added requests
+	 */
+	Schedule buildSchedule(unsigned int maxAggregatedRequests, const double maxWaitingTime, const Node *driverNode,
+	                       Schedule schedule, unsigned int *aggregatedRequests);
 
 public:
 	AMOD_Controller(const MutexStrategy &mtx, unsigned int computationPeriod, unsigned int id,

15807f16ae6d0d3c012f34e798a19fe6872edab6
Gishara <gishara@smart.mit.edu>
2018-01-10 12:52:03 +0800

    added config param for launch BTO

--- dev/Basic/long/agent/impl/DeveloperAgent.cpp
+++ dev/Basic/long/agent/impl/DeveloperAgent.cpp
@@ -681,6 +681,7 @@ bool DeveloperAgent::onFrameInit(timeslice now) {
 
 Entity::UpdateStatus DeveloperAgent::onFrameTick(timeslice now) {
 
+	ConfigParams& config = ConfigManager::GetInstanceRW().FullConfig();
     if (devModel && isActive())
     {
     	currentTick = now.ms();
@@ -729,7 +730,7 @@ Entity::UpdateStatus DeveloperAgent::onFrameTick(timeslice now) {
     		}
     	}
     	}
-    	else if(hasBTO)
+    	else if(config.ltParams.launchBTO && hasBTO)
     	{
     		launchBTOUnits(currentDate);
     	}

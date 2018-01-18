4f642f20838ad5c8398ab8525032b32137dae40d
Neeraj <neeraj@smart.mit.edu>
2018-01-18 15:42:32 +0800

    Removed loading of TAZ in withinday/supply - Not used by roaming models anymore

--- dev/Basic/medium/main_impl.cpp
+++ dev/Basic/medium/main_impl.cpp
@@ -308,11 +308,6 @@ bool performMainSupply(const std::string& configFileName, std::list<std::string>
 	const ConfigParams& config = ConfigManager::GetInstance().FullConfig();
 	const MT_Config& mtConfig = MT_Config::getInstance();
 
-	//aa{ I just run this to retrieve the TAZes. Unfortunately, at the moment only preday reads them
-	PredayManager predayManager;
-	predayManager.loadZoneNodes();
-	//aa}
-
 	PeriodicPersonLoader* periodicPersonLoader = new MT_PersonLoader(Agent::all_agents, Agent::pending_agents);
 	const ScreenLineCounter* screenLnCtr = ScreenLineCounter::getInstance(); //This line is necessary. It creates the singleton ScreenlineCounter object before any workers are created.
 	WithindayModelsHelper::loadZones(); //load zone information from db

15a2a5f3e405d087eff6873a9f3687da7e832abf
Gishara <gishara@smart.mit.edu>
2018-06-01 17:17:28 +0800

    bidder can transfer units only when the move in waiting time is 0

--- dev/Basic/long/agent/impl/HouseholdAgent.cpp
+++ dev/Basic/long/agent/impl/HouseholdAgent.cpp
@@ -242,7 +242,7 @@ Entity::UpdateStatus HouseholdAgent::onFrameTick(timeslice now)
 		bidder->setActive(false);
 
 		//transfer unit to a freelance agent if a household has done a successful bid and has not sold his house during MoveInWaitingTimeInDays.
-		if( id < model->FAKE_IDS_START && seller->sellingUnitsMap.size() > 0 && bidder->getParent()->getHousehold()->getLastBidStatus() == 1 && bidder->getMoveInWaitingTimeInDays() <= 0)
+		if( id < model->FAKE_IDS_START && seller->sellingUnitsMap.size() > 0 && bidder->getParent()->getHousehold()->getLastBidStatus() == 1 && bidder->getMoveInWaitingTimeInDays() == 0)
 			TransferUnitToFreelanceAgent();
 
 	    //The seller becomes inactive when the bidder is inactive. This is alright

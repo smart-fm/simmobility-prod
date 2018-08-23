8fb26a1243e0dc5edb42df1476da585cd120e4f8
Pankajg123 <pankaj@smart.mit.edu>
2018-08-23 17:37:47 +0800

    fixed one logical error in checkTripChain function. advanceCurrentTripChain() is only need to be call for IT_TRIP.

--- dev/Basic/medium/entities/Person_MT.cpp
+++ dev/Basic/medium/entities/Person_MT.cpp
@@ -1165,8 +1165,8 @@ Entity::UpdateStatus Person_MT::checkTripChain(unsigned int currentTime)
 
 	//advance the trip, sub-trip or activity....
 	TripChainItem *chainItem=*(tripChain.begin());
-	if(chainItem->itemType != TripChainItem::IT_ON_HAIL_TRIP || chainItem->itemType != TripChainItem::IT_ON_CALL_TRIP
-	   || chainItem->itemType != TripChainItem::IT_TAXITRIP)
+	if(chainItem->itemType != TripChainItem::IT_ON_HAIL_TRIP && chainItem->itemType != TripChainItem::IT_ON_CALL_TRIP
+	   && chainItem->itemType != TripChainItem::IT_TAXITRIP)
 	{
 		if (!isFirstTick)
 		{

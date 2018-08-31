1376ec9f3a05bc167441161eb5edb268aa780bb3
Lemuel Kumarga <register@lemuelkumarga.com>
2018-08-31 14:49:08 +0800

    Allow for path specification

--- dev/tools/controller-statistics/controller-statistics.py
+++ dev/tools/controller-statistics/controller-statistics.py
@@ -1,17 +1,46 @@
+"""
+Script to extract requests, pickups and dropoffs by time of day
+By default, the script will look for controller.log in the current directory. However,
+this may be overriden by specifying the file path as an argument.
+
+
+Usage: 
+python2.7 controller-statistics.py [optional: path of controller.log] [optional: starttime in seconds]
+
+Example:
+python2.7 controller-statistics.py /home/.../controller.log 0
+
+Author: Unknown (Last Updated by Lemuel Kumarga)
+Date: 31.08.2018
+
+Output:
+  1) request_pickup_dropoffs.png: Plot of Requests, Pickups, Dropoffs by time of day
+  2) request_pickup_dropoff.csv: # of Requests, Pickups, Dropoffs by time of day
+"""
+
 import pandas as pd
+import sys
 import os
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.markers as mkr
-import brewer2mpl as b2m # Brewer colors; view here: http://bl.ocks.org/mbostock/5577023
 import re
-from matplotlib.ticker import MaxNLocator
 
-set1H = b2m.get_map('Set1','Qualitative', 5).hex_colors # HEX values of Brewer Set1 colors
+#import brewer2mpl as b2m # Brewer colors; view here: http://bl.ocks.org/mbostock/5577023
+#set1H = b2m.get_map('Set1','Qualitative', 5).hex_colors)
 
+set1H = ['#E41A1C', '#377EB8', '#4DAF4A', '#984EA3', '#FF7F00']
 
 markers_array  = ["-","--","-.",":"]
 
+log_path = "controller.log"
+if (len(sys.argv) > 1):
+    log_path = sys.argv[1]
+
 start_time = 0 * 60 * 60
+if (len(sys.argv) > 2):
+    start_time = float(sys.argv[2])
 
 def getStats(logfile,park=True,controller=["AMOD"]):
     # shared_requests=dict.fromkeys(('time', 'user_id','origin', 'destination'),0)
@@ -50,10 +79,17 @@ def getStats(logfile,park=True,controller=["AMOD"]):
         prev_line_controller_spec = False
         for line in controllerLogLines:
             if line.startswith('Subscription received by the controller of type SERVICE_CONTROLLER_'+controller_id):
-                temp_subscriptions.append({
-                    'time':float(line.split('at time ')[1].split('.')[0]),
-                    'driver_id':line.split('Subscription from driver ')[1].split(' at time')[0]
-                    })
+                if 'time frame' in line:
+                    # For older versions where controller.log is printing time frame instead of time                
+                    temp_subscriptions.append({
+                        'time':float(line.split('at time frame ')[1].split(',')[0]),
+                        'driver_id':line.split('Subscription from driver ')[1].split(' at time')[0]
+                        })
+                else:
+                    temp_subscriptions.append({
+                        'time':float(line.split('at time ')[1].split('.')[0]),
+                        'driver_id':line.split('Subscription from driver ')[1].split(' at time')[0]
+                        })
             elif line.startswith('Request sent to controller of type SERVICE_CONTROLLER_' +controller_id):
                 temp_requests.append({
                     'time':float(line.split('at frame ')[1].split(' ')[1].split('s')[0]),
@@ -85,7 +121,7 @@ def getStats(logfile,park=True,controller=["AMOD"]):
                     'time':float(line.split('frame ')[1].split(', ')[1].split('s')[0]),
                     'driver_id':line.split('driverId ')[1].split('\n')[0], 
                     'user_id':line.split('Drop-off of user ')[1].split(' ')[0],
-                    'destination':line.split('destinationNodeId ')[1].split(',')[0]
+                    'destination':line.split('destinationNodeId ')[1].split('and')[0]
                     })
 
             elif 'Begin driving to park,' in line:
@@ -95,10 +131,10 @@ def getStats(logfile,park=True,controller=["AMOD"]):
                     'parking_node':line.split('to parking node ')[1].split('\n')[0]
                     })
             elif line.startswith('For Controller type: SERVICE_CONTROLLER_'+controller_id):
-            	prev_line_controller_spec = True
+                prev_line_controller_spec = True
             elif prev_line_controller_spec and line.startswith('Max. fleet size configured:'):
-            	fleet_size[controller_id] = (line.split(": ")[1].split("\n")[0])
-            	prev_line_controller_spec = False
+                fleet_size[controller_id] = (line.split(": ")[1].split("\n")[0])
+                prev_line_controller_spec = False
 
         temp_requests = pd.DataFrame(temp_requests)
         if temp_requests.empty==False:
@@ -128,7 +164,7 @@ def getStats(logfile,park=True,controller=["AMOD"]):
             parking.time = pd.DatetimeIndex(parking.time)
             parking.set_index('time',inplace=True)
             parking['num_parking'] = 1
-            num_parking = parking.resample('5T').apply('sum')
+            num_parking = parking.resample('5T', how='sum')['num_parking']
             #print len(requests), len(subscriptions), len(assignments), len(pickups), len(dropoffs), len(parking)
         else:
             pass
@@ -139,14 +175,15 @@ def getStats(logfile,park=True,controller=["AMOD"]):
         dropoffs.time = pd.DatetimeIndex(dropoffs.time)
         dropoffs.set_index('time',inplace=True)
         dropoffs['num_dropoffs'] = 1
-        num_dropoffs = dropoffs.resample('5T').apply('sum')
+        num_dropoffs = dropoffs.resample('5T', how='sum')['num_dropoffs']
+
 
         pickups.time = pickups.time + start_time
         pickups.time = pd.to_datetime(pickups.time, unit='s')
         pickups.time = pd.DatetimeIndex(pickups.time)
         pickups.set_index('time',inplace=True)
         pickups['num_pickups'] = 1
-        num_pickups = pickups['num_pickups'].resample('5T').apply('sum')
+        num_pickups = pickups.resample('5T', how='sum')['num_pickups']
 
 
         if temp_subscriptions.empty:
@@ -157,7 +194,7 @@ def getStats(logfile,park=True,controller=["AMOD"]):
             temp_subscriptions.time = pd.DatetimeIndex(temp_subscriptions.time)
             temp_subscriptions.set_index('time',inplace=True)
             temp_subscriptions['num_'+controller_id+'_subscriptions'] = 1
-            num_temp_subscriptions = temp_subscriptions.resample('5T').apply('sum')
+            num_temp_subscriptions = temp_subscriptions.resample('5T', how='sum')['num_'+controller_id+'_subscriptions']
 
         if temp_requests.empty:
             pass
@@ -167,7 +204,7 @@ def getStats(logfile,park=True,controller=["AMOD"]):
             temp_requests.time = pd.DatetimeIndex(temp_requests.time)
             temp_requests.set_index('time',inplace=True)
             temp_requests['num_'+controller_id+'_requests'] = 1
-            num_temp_requests = temp_requests.resample('5T').apply('sum')['num_'+controller_id+'_requests']
+            num_temp_requests = temp_requests.resample('5T', how='sum')['num_'+controller_id+'_requests']
 
         if temp_assignments.empty:
             pass
@@ -178,8 +215,8 @@ def getStats(logfile,park=True,controller=["AMOD"]):
             temp_assignments['time'] = pd.DatetimeIndex(temp_assignments['time'])
             temp_assignments.set_index('time',inplace=True)
             temp_assignments['num_'+controller_id+'_assignments'] = 1
-            num_temp_assignments = temp_assignments.resample('5T').apply('sum')['num_'+controller_id+'_assignments']
-            #num_amod_rides = amod_assignments.resample('5T').apply('sum')['num_rides']
+            num_temp_assignments = temp_assignments.resample('5T', how='sum')['num_'+controller_id+'_assignments']
+            #num_amod_rides = amod_assignments.resample('5T', how='sum')['num_rides']
 
 
         if park:
@@ -197,22 +234,19 @@ def getStats(logfile,park=True,controller=["AMOD"]):
                        # num_temp_assignments, #num_amod_rides,
                         num_pickups, num_dropoffs], axis=1,join='outer')
 
-        stats.index  = stats.index.strftime('%H:%M')
+        stats.index  = [ pd.to_datetime(i).strftime('%H:%M') for i in stats.index.values ]
         stats.columns = ['Requests', 'Pickups', 'Dropoffs']
         #stats.index = pd.DatetimeIndex(stats.index).time
         #stats.index = pd.Series([val.time() for val in stats.index])
     return { "data": stats, "fleet_size" : fleet_size } 
 
 #st0dist = getStats('controller_basecase.log',park=False)
-res = getStats('controller.log',park=False)
+res = getStats(log_path,park=False)
 st = res["data"]
 st.to_csv("request_pickup_dropoff.csv")
+
 st.plot()
 plt.title(" | ".join(k + " Fleet Size: " + v for k,v in res["fleet_size"].items()))
 plt.xlabel('Time')
 plt.ylabel('Counts')
-plt.show()
-
-##fig = figure()
-#ax = fig.add_subplot(111)
-#ax.xaxis.set_major_locator(MaxNLocator(8))
+plt.savefig("request_pickup_dropoff.png")

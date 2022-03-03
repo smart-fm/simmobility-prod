14f3528bc2805c57d0f9978881547b5453513eb6
wenhan <wenhanchong@github.com>
2022-03-03 18:24:17 +0800

    Fixed compile error in unused code

--- dev/Basic/shared/network/tcp_server.cpp
+++ dev/Basic/shared/network/tcp_server.cpp
@@ -28,8 +28,8 @@ sim_mob::tcp_server::tcp_server(boost::asio::io_service& io_service,int port, Co
 
 void sim_mob::tcp_server::start_accept()
 {
-  new_connection =
-    tcp_connection::create(acceptor_.get_io_service());
+  //new_connection =
+  //  tcp_connection::create(acceptor_.get_io_service());
 
   acceptor_.async_accept(new_connection->socket(),
       boost::bind(&tcp_server::handle_accept, this, new_connection,
